994e28f7e2d82b79cbe8c9c04a9dd9499a5e4654
stsci_jmiller <stsci_jmiller@stsci.edu>
2011-05-17 16:31:16 +0000

    Reworked validate.py to check that each reference mentioned in the closure of a mapping is known to CRDS via the project locator module.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@55 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- lib/validate.py
+++ lib/validate.py
@@ -8,23 +8,18 @@ import crds.rmap as rmap
 
 def validate_context(fname):
     log.info("Validating context:", repr(fname))
-    try:
-        header, data = rmap.load_rmap(fname, "CONTEXT")
-    except rmap.MappingError, e:
-        log.error()
-        return
-
-    filepath = os.path.dirname(fname) or "./"
-
-    for reftype, (ext, mapname) in data.items():
+    
+    mapping = rmap.load_mapping(fname)
+    
+    for reference in mapping.reference_names():
         try:
-            validate_mapping_file(filepath +"/" + mapname, reftype)
-        except rmap.MappingError, e:
-            log.error(repr(reftype))
+            where = mapping.locate.locate_reference(reference)
+        except KeyError, e:
+            sys.exc_clear()
+            log.error("Unknown reference file", str(e))
+        # if not os.path.exists(where):
+        #     log.error("Missing reference file", repr(where))
 
-def validate_mapping_file(fname, reftype=None):
-    log.info("Validating mapping file:", repr(fname))
-    header, data = rmap.load_rmap(fname, reftype)
 
 def main():
     for fname in sys.argv[1:]:
