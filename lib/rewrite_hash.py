8997a4400d1b7e0254ee6a5e67e27839e9deeca2
stsci_jmiller <stsci_jmiller@stsci.edu>
2011-06-14 18:23:05 +0000

    Added main() to rewrite_hash.py utility so that nose ignores it properly.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@79 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- lib/rewrite_hash.py
+++ lib/rewrite_hash.py
@@ -2,13 +2,16 @@ import sys
 
 import crds.rmap as rmap
 
-for file_ in sys.argv[1:]:
-    print file_
-    if file_.endswith((".pmap",".imap")):
-        m = rmap.Mapping.from_file(file_, ignore_hash=True)
-    elif file_.endswith(".rmap"):
-        m = rmap.ReferenceMapping.from_file(file_, ignore_hash=True)
-    else:
-        raise ValueError("Bad file extension in file " + repr(file_))
-    m.write(file_)
+def main():
+    for file_ in sys.argv[1:]:
+        print file_
+        if file_.endswith((".pmap",".imap")):
+            m = rmap.Mapping.from_file(file_, ignore_hash=True)
+        elif file_.endswith(".rmap"):
+            m = rmap.ReferenceMapping.from_file(file_, ignore_hash=True)
+        else:
+            raise ValueError("Bad file extension in file " + repr(file_))
+        m.write(file_)
 
+if __name__ == "__main__":
+    main()
