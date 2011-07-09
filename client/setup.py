88d2b16cdfb4f855a48512a02e5ee50e158342fe
stsci_jmiller <stsci_jmiller@stsci.edu>
2011-07-09 10:52:29 +0000

    Updated CRDS library setup.py files.  Updated eclipse project files.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@84 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- client/setup.py
+++ client/setup.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-import pytools.stsci_distutils_hack as dh
-f = open('svn_version.py', 'w' )
-f.write("__svn_version__ = '%s'\n" % dh.__get_svn_rev__('.'))
-f.write("\n__full_svn_info__ = '''\n%s'''\n\n" % dh.__get_full_info__('.'))
-f.close()
-
 setup(name="crds.client",
       version= "0.1",
       description="Python based Calibration Reference Data System,  client module for accessing web service",
