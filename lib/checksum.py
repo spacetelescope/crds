62f07667d10dc49f05c123a8a618a44d7ca88786
stsci_jmiller <stsci_jmiller@stsci.edu>
2011-09-01 14:12:52 +0000

    Factored out single-file update_checksum in crds.checksum
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@138 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- lib/checksum.py
+++ lib/checksum.py
@@ -12,16 +12,20 @@ import sys
 
 import crds.rmap as rmap
 
+def update_checksum(file_):
+    """Rewrite the checksum of a single mapping, in place."""
+    if file_.endswith((".pmap",".imap")):
+        mapping = rmap.Mapping.from_file(file_, ignore_checksum=True)
+    elif file_.endswith(".rmap"):
+        mapping = rmap.ReferenceMapping.from_file(file_, ignore_checksum=True)
+    else:
+        raise ValueError("Bad file extension in file " + repr(file_))
+    mapping.rewrite_checksum(file_)
+
 def update_checksums(files):
     """Rewrite the mapping checksums/hashes/sha1sums in all `files`."""
     for file_ in files:
-        if file_.endswith((".pmap",".imap")):
-            mapping = rmap.Mapping.from_file(file_, ignore_checksum=True)
-        elif file_.endswith(".rmap"):
-            mapping = rmap.ReferenceMapping.from_file(file_, ignore_checksum=True)
-        else:
-            raise ValueError("Bad file extension in file " + repr(file_))
-        mapping.rewrite_checksum(file_)
-
+        update_checksum(file_)
+        
 if __name__ == "__main__":
     update_checksums(sys.argv[1:])
