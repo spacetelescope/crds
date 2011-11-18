775d8176a83b6c0d0860d85301773b06ebd82d64
stsci_jmiller <stsci_jmiller@stsci.edu>
2011-11-18 15:32:22 +0000

    Simplified checksum stub.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@219 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- lib/checksum.py
+++ lib/checksum.py
@@ -14,12 +14,7 @@ import crds.rmap as rmap
 
 def update_checksum(file_):
     """Rewrite the checksum of a single mapping, in place."""
-    if file_.endswith((".pmap",".imap")):
-        mapping = rmap.Mapping.from_file(file_, ignore_checksum=True)
-    elif file_.endswith(".rmap"):
-        mapping = rmap.ReferenceMapping.from_file(file_, ignore_checksum=True)
-    else:
-        raise ValueError("Bad file extension in file " + repr(file_))
+    mapping = rmap.Mapping.from_file(file_, ignore_checksum=True)
     mapping.rewrite_checksum(file_)
 
 def update_checksums(files):
