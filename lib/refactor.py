93349409b1806f35b6369193734a69dcb31c4ecf
stsci_jmiller <stsci_jmiller@stsci.edu>
2011-12-23 21:08:47 +0000

    Added rmap refactoring module with replace_header_value().
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@248 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

---
+++ lib/refactor.py
@@ -0,0 +1,21 @@
+"""This module defines functions for modifying rmaps in various ways,  generally
+the transformations required to automate rmap maintenance on the CRDS website.
+"""
+import re
+import cStringIO
+
+KEY_RE = r"(\s*')(.*)('\s*:\s*')(.*)('\s*,.*)"
+
+def replace_header_value(filename, key, new_value):
+    print "refactoring", repr(filename), ":", key, "=", repr(new_value)
+    newfile = cStringIO.StringIO()
+    openfile = open(filename)
+    for line in openfile:
+        m = re.match(KEY_RE, line)
+        if m and m.group(2) == key:
+            line = re.sub(KEY_RE, r"\1\2\3%s\5" % new_value, line)
+        newfile.write(line)
+    openfile.close()
+    newfile.seek(0)
+    open(filename, "w+").write(newfile.read())
+    
