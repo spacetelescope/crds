c251e676f152c2125d2b166519e51a73f6daed0d
stsci_jmiller <stsci_jmiller@stsci.edu>
2012-05-13 14:17:00 +0000

    Draft checkin for tobs test observatory configuration files.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@434 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

---
+++ tobs/__init__.py
@@ -0,0 +1,10 @@
+INSTRUMENTS = ["testinstr",]
+
+EXTENSIONS = [".pmap", ".imap", ".rmap", ".fits", ]
+
+TEXT_DESCR = {
+    "testfk" : "Test File Kind",
+}
+
+FILEKINDS = sorted(TEXT_DESCR.keys())
+
