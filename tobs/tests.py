5123d9c261ce5b1344bae304c5463cb0b526afdb
stsci_jmiller <stsci_jmiller@stsci.edu>
2012-05-13 15:26:22 +0000

    Added tobs test cases for Bracket, ClosestTime, and GeometricallyNearest.   Not yet passing.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@436 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- tobs/tests.py
+++ tobs/tests.py
@@ -1,4 +1,4 @@
-"""Higher level mapping based tests.
+"""Higher level mapping based tests for selectors not covered by hst.
 """
 
 from crds import rmap
@@ -12,7 +12,7 @@ class TestSelectors(unittest.TestCase):
 
     def _selector_testcase(self, case, parameter, result):
         header = {
-            "TEST_CASE":"SELECT_VERSION",
+            "TEST_CASE": case,
             "PARAMETER": parameter,
             }
         bestref = self.rmap.get_best_ref(header)
@@ -26,6 +26,55 @@ class TestSelectors(unittest.TestCase):
         self._selector_testcase('SELECT_VERSION', '6', 'cref_flatfield_123.fits')
     def test_select_version4(self): 
         self._selector_testcase('SELECT_VERSION', '2.0', 'cref_flatfield_65.fits')
+        
+    def test_closest_time1(self):
+        self._selector_testcase('CLOSEST_TIME', '2016-05-05', 'cref_flatfield_123.fits')
+    def test_closest_time2(self):
+        self._selector_testcase('CLOSEST_TIME', '2016-04-24', 'cref_flatfield_123.fits')
+    def test_closest_time3(self):
+        self._selector_testcase('CLOSEST_TIME', '2018-02-02', 'cref_flatfield_222.fits')
+    def test_closest_time4(self):
+        self._selector_testcase('CLOSEST_TIME', '2019-03-01', 'cref_flatfield_123.fits')
+    def test_closest_time5(self):
+        self._selector_testcase('CLOSEST_TIME', '2016-04-15', 'cref_flatfield_123.fits')
+    def test_closest_time6(self):
+        self._selector_testcase('CLOSEST_TIME', '2016-04-16', 'cref_flatfield_123.fits')
+
+    def test_bracket1(self):
+        self._selector_testcase('BRACKET', '1.25',     
+            ('cref_flatfield_120.fits', 'cref_flatfield_124.fits'))
+    def test_bracket2(self):
+        self._selector_testcase('BRACKET', '1.2',     
+            ('cref_flatfield_120.fits', 'cref_flatfield_120.fits'))
+    def test_bracket3(self):
+        self._selector_testcase('BRACKET', '1.5',     
+            ('cref_flatfield_124.fits', 'cref_flatfield_124.fits'))
+    def test_bracket4(self):
+        self._selector_testcase('BRACKET', '5.0',     
+            ('cref_flatfield_137.fits', 'cref_flatfield_137.fits'))
+    def test_bracket5(self):
+        self._selector_testcase('BRACKET', '1.0',
+            ('cref_flatfield_120.fits', 'cref_flatfield_120.fits'))
+    def test_bracket6(self):
+        self._selector_testcase('BRACKET', '1.2',
+            ('cref_flatfield_137.fits', 'cref_flatfield_137.fits'))
+
+    def test_geometrically_nearest1(self):
+        self._selector_testcase("GEOMETRICALLY_NEAREST", '1.0', 'cref_flatfield_120.fits')
+    def test_geometrically_nearest2(self):
+        self._selector_testcase("GEOMETRICALLY_NEAREST", '1.2', 'cref_flatfield_120.fits')
+    def test_geometrically_nearest1(self):
+        self._selector_testcase("GEOMETRICALLY_NEAREST", '1.25', 'cref_flatfield_120.fits')
+    def test_geometrically_nearest1(self):
+        self._selector_testcase("GEOMETRICALLY_NEAREST", '1.4', 'cref_flatfield_124.fits')
+    def test_geometrically_nearest1(self):
+        self._selector_testcase("GEOMETRICALLY_NEAREST", '3.25', 'cref_flatfield_124.fits')
+    def test_geometrically_nearest1(self):
+        self._selector_testcase("GEOMETRICALLY_NEAREST", '3.26', 'cref_flatfield_137.fits')
+    def test_geometrically_nearest1(self):
+        self._selector_testcase("GEOMETRICALLY_NEAREST", '5.0', 'cref_flatfield_137.fits')
+    def test_geometrically_nearest1(self):
+        self._selector_testcase("GEOMETRICALLY_NEAREST", '5.1', 'cref_flatfield_137.fits')
 
 if __name__ == '__main__':
     unittest.main()
