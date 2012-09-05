74ec6f952c3c1abd39bd86211d630796a766a25b
stsci_jmiller <stsci_jmiller@stsci.edu>
2012-09-05 19:33:22 +0000

    Added niriss to INSTRUMENTS, dark and linearity to FILEKINDS. Changed instrument packages to instrument modules,  presently unused anyway but now like HST.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@615 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- jwst/__init__.py
+++ jwst/__init__.py
@@ -1,10 +1,12 @@
-INSTRUMENTS = ["miri","nirspec","nircam"]
+INSTRUMENTS = ["miri","nirspec","nircam","niriss"]
 
 EXTENSIONS = [".pmap", ".imap", ".rmap", ".fits", ]
 
 TEXT_DESCR = {
     "flat" : "Flat Field",
     "photom" : "Absolute Calibration",
+    "dark" : "Dark Frame",
+    "linearity" : "Detector Linearity Correction Coefficients",
 }
 
 FILEKINDS = sorted(TEXT_DESCR.keys())
