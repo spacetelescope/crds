7a02631e3dd421828800f02b036807f4a7167bca
stsci_jmiller <stsci_jmiller@stsci.edu>
2012-09-14 14:41:01 +0000

    Added required parkeys values to --provenance keyword dump of certify.py when a context is provided.   This shows the matching parameters a reference file applies to,   particularly for web feedback during file submission.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@650 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- lib/certify.py
+++ lib/certify.py
@@ -454,8 +454,10 @@ def certify_reference(fitsname, context=None,
         return
 
     if dump_provenance:
-        dump_multi_key(fitsname, ["DESCRIP", "COMMENT", "PEDIGREE", "USEAFTER",
-                                  "HISTORY",])
+        provenance_keys = ["DESCRIP", "COMMENT", "PEDIGREE", "USEAFTER",
+                                  "HISTORY",]
+        parkeys = get_rmap_parkeys(fitsname, context)
+        dump_multi_key(fitsname, parkeys + provenance_keys)
 
     mode_checker = None # Initialize mode validation
     for checker in get_validators(fitsname):
@@ -494,6 +496,21 @@ def dump_multi_key(fitsname, keys):
             for card in cards:
                 if card.key == key:
                     log.info("["+str(i)+"]", key, card.value, card.comment)
+
+def get_rmap_parkeys(refname, context):
+    """Determine required parkeys in reference path `refname` according to pipeline 
+    mapping `context`.
+    """
+    if context is None:
+        return
+    try:
+        header = data_file.get_header(refname)
+        pmap = rmap.get_cached_mapping(context)
+        instrument, filekind = pmap.locate.get_file_properties(refname)
+        return pmap.get_imap(instrument).get_rmap(filekind).get_required_parkeys()
+    except Exception, exc:
+        log.verbose_warning("Failed retrieving required parkeys:", str(exc))
+        return []
 
 def validate_file_format(fitsname):
     """ Run PyFITS verify method on file to report any FITS format problems
