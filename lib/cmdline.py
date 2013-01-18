7cd6625b3b8ad11f8011f99ac60abccb0e5e5a2c
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-01-18 22:05:36 +0000

    Converted crds.sync to cmdline base class add added --ignore-cache parameter.
    Refinements to cmdline base class,  added add_argument() method and streamlined existing subclasses newcontext and diff.
    
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@831 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- lib/cmdline.py
+++ lib/cmdline.py
@@ -7,23 +7,43 @@ import argparse
 import pdb
 import cProfile as profile
 
+from argparse import RawTextHelpFormatter
+
 from crds import log
 
+# =============================================================================
+
+def _show_version():
+    """Dump CRDS version information and exit python."""
+    import crds, crds.svn_version
+    log.info("crds version", crds.__version__, "revision", crds.svn_version.__svn_version__)
+    sys.exit(-1)
+
+# =============================================================================
+
 class Script(object):
+    
     """Base class for CRDS command line scripts with standard properties."""
     
     decription = epilog = usage = None
+    formatter_class = RawTextHelpFormatter
     
     def __init__(self, **parser_pars):
-        for key in ["description", "epilog", "usage"]: 
+        for key in ["description", "epilog", "usage", "formatter_class"]: 
             self._add_key(key, parser_pars)
         if "--version" in sys.argv:   # hack this since it's non-standard.
-            self._show_version()
+            _show_version()
         self.parser = argparse.ArgumentParser(**parser_pars)
         self.add_args()
         self.add_standard_args()
         self.args = self.parser.parse_args()
         log.set_verbose(self.args.verbosity or self.args.verbose)
+    
+    def main(self):
+        """Write a main method to perform the actions of the script using self.args."""
+        
+    def add_args(self):
+        """Add script-specific argparse add_argument calls here on self.parser"""
     
     def get_observatory(self, filename=None):
         """Determine the observatory corresponding to the differenced files."""
@@ -41,34 +61,29 @@ class Script(object):
                 observatory = "jwst"
         return observatory
             
-    def _show_version(self):
-        """Dump CRDS version information and exit python."""
-        import crds, crds.svn_version
-        log.info("crds version", crds.__version__, "revision", crds.svn_version.__svn_version__)
-        sys.exit(-1)
-
     def _add_key(self, key, parser_pars):
         """Add any defined class attribute for `key` to dict `parser_pars`."""
         inlined = getattr(self, key, parser_pars)
         if inlined is not None:  
             parser_pars[key] = inlined
         return parser_pars
-
-    def add_args(self):
-        """Add script-specific argparse add_argument calls here on self.parser"""
     
+    def add_argument(self, *args, **keys):
+        """Add a parser argument."""
+        self.parser.add_argument(*args, **keys)
+
     def add_standard_args(self):
         """Add standard CRDS command line parameters."""
-        self.parser.add_argument("-v", "--verbose", help="Set log verbosity to True,  nominal debug level.", action="store_true")
-        self.parser.add_argument("--verbosity", help="Set log verbosity to a specific level: 0..100.", type=int, default=0)
-        self.parser.add_argument("-J", "--jwst", 
+        self.add_argument("-v", "--verbose", help="Set log verbosity to True,  nominal debug level.", action="store_true")
+        self.add_argument("--verbosity", help="Set log verbosity to a specific level: 0..100.", type=int, default=0)
+        self.add_argument("-J", "--jwst", 
             help="Force observatory to JWST for determining header conventions.""",
             action="store_true")
-        self.parser.add_argument("-H", "--hst", 
+        self.add_argument("-H", "--hst", 
             help="Force observatory to HST for determining heder conventions.""",
             action="store_true")
-        self.parser.add_argument("--profile", help="Output profile stats to the specified file.", type=str, default="")
-        self.parser.add_argument("--pdb", help="Run under pdb.", action="store_true")
+        self.add_argument("--profile", help="Output profile stats to the specified file.", type=str, default="")
+        self.add_argument("--pdb", help="Run under pdb.", action="store_true")
 
     def __call__(self):
         """Run the script's main() according to command line parameters."""
@@ -78,7 +93,4 @@ class Script(object):
             pdb.runctx("self.main()", locals(), locals())
         else:
             self.main()
-
-    def main(self):
-        """Write a main method to perform the actions of the script using self.args."""
     
