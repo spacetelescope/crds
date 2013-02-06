e1c91db0806a92cf7f44ab0028f2dc3d1ed546d1
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-02-06 21:51:20 +0000

    Changes supporting initial JWST batch submissions.
    rmap.py -- Factored out rmap header dictionary case conversions into a dict subclass which converts values to lower case:  LowerCaseDict.   LowerCaseDict is then passed down as the rmap header to Selectors so that Selectors handle header case uniformly.              
    
    refactor.py -- Cleaned up USEAFTER handling in refactor.py moving towards JWST batch submissions.  pylinted.  Switched to cmdline.Script command line handling.
    
    Added reference_file, mapping, pipeline_mapping, and reference_mapping filters to cmdline.py for screening command line parameters.
    
    Incorporated current JWST reference file spec into jwst.locate.py adding reftype_to_filekind() function to interpret the new values of the REFTYPE keyword.
    
    Moved HST refactor.py USEAFTER handling into crds.hst package parkeys.reference_keys_to_dataset_keys.
    
    Changed function parameter empty list default values to empty tuples in data_file.py since mutable defaults are potentially dangerous.
    
    
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@841 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- lib/refactor.py
+++ lib/refactor.py
@@ -2,21 +2,16 @@
 the transformations required to automate rmap maintenance on the CRDS website.
 """
 import sys
-import re
-import cStringIO
 import os.path
 
-from crds import (rmap, data_file, timestamp, compat, log, selectors, 
-                  checksum, utils, diff)
-from crds.timestamp import DATETIME_RE_STR
-
+from crds import (rmap, data_file, timestamp, log, utils, diff, cmdline)
 # ============================================================================
     
 class NoUseAfterError(ValueError):
     "The specified UseAfter datetime didn't exist in the rmap."
 
 class NoMatchTupleError(ValueError):
-    "The specified UseAfter datetime didn't exist in the rmap."
+    "The specified Match tuple didn't exist in the rmap."
 
 # ============================================================================
     
@@ -25,9 +20,9 @@ def set_header_value(old_rmap, new_rmap, key, new_value):
     This is potentially lossy since rewriting the rmap may/will lose comments and 
     formatting quirks.
     """
-    map = rmap.load_mapping(old_rmap)
-    map.header[key] = new_value
-    map.write(new_rmap)
+    mapping = rmap.load_mapping(old_rmap)
+    mapping.header[key] = new_value
+    mapping.write(new_rmap)
     
 def update_derivation(new_path, old_basename=None):
     """Set the 'derived_from' and 'name' header fields of `new_path`.  
@@ -45,12 +40,12 @@ def update_derivation(new_path, old_basename=None):
     
 # ============================================================================
 
-def rmap_insert_references(old_rmap, new_rmap, inserted_references, expected=("add","replace")):
+def rmap_insert_references(old_rmap, new_rmap, inserted_references):
     """Given the full path of starting rmap `old_rmap`,  modify it by inserting 
     or replacing all files in `inserted_references` and write out the result to
     `new_rmap`.    If no actions are performed, don't write out `new_rmap`.
     
-    Return the list of RefactorAction's performed.
+    Return new ReferenceMapping named `new_rmap`
     """
     new = old = rmap.fetch_mapping(old_rmap, ignore_checksum=True)
     for reference in inserted_references:
@@ -60,7 +55,7 @@ def rmap_insert_references(old_rmap, new_rmap, inserted_references, expected=("a
     new.write(new_rmap)
     return new
 
-def rmap_check_modifications(old_rmap, new_rmap, expected=["add"]):
+def rmap_check_modifications(old_rmap, new_rmap, expected=("add",)):
     """Check the differences between `old_rmap` and `new_rmap` and make sure they're
     limited to the types listed in `expected`.
     
@@ -86,7 +81,7 @@ def _rmap_insert_reference(loaded_rmap, reffile):
     locations.  This routine assumes HST standard selector organization,  
     Match -> UseAfter.
     
-    returns new_contents, [ old_rmap_match_tuples... ],  useafter_date 
+    returns new ReferenceMapping made from `loaded_rmap` inserting `reffile`.
     """
     log.info("Inserting", repr(os.path.basename(reffile)), "into", repr(loaded_rmap.name))
     header = _get_matching_header(loaded_rmap, reffile)
@@ -97,16 +92,11 @@ def _get_matching_header(loaded_rmap, reffile):
     """Based on `loaded_rmap`,  fetch the abstract header from reffile and use 
     it to define the parkey patterns to which this reffile will apply.   Note 
     that reference files generally apply to a set of parkey values and that the
-    parkey patterns returned here can be unexploded or-globs.   The header 
-    returned actually represents a set of headers corresponding to the discrete 
-    values of exploded or-globs.
-    
-    XXX TODO possibly required:  it may be necessary to fully explode the 
-    or-globs and return a list of headers of discrete parameters for matching.
-    The current approach depends on an exact match to the or-glob.
+    parkey patterns returned here can be unexploded or-globs.
     """
-    # XXX Hack alert skip DATE-OBS, TIME-OBS
-    parkeys = loaded_rmap.get_required_parkeys()[:-2]
+    
+    # NOTE: required parkeys are in terms of *dataset* headers,  not reference headers.
+    parkeys = loaded_rmap.get_required_parkeys()
     
     # Since expansion rules may depend on keys not used in matching,  
     # get entire header  
@@ -121,16 +111,10 @@ def _get_matching_header(loaded_rmap, reffile):
     # glob patterns for matching with the reference file.
     header = loaded_rmap.locate.expand_wildcards(loaded_rmap.instrument, header)
     
-    header = { key:utils.condition_value(value) for key, value in header.items() \
-               if key in parkeys + ["USEAFTER"] }
-    
-    if "USEAFTER" in header and "DATE-OBS" not in header:
-        useafter = timestamp.reformat_date(header["USEAFTER"])
-        header["DATE-OBS"] = useafter.split()[0]
-        header["TIME-OBS"] = useafter.split()[1]
+    header = { key:utils.condition_value(value) for key, value in header.items() }
     
     # Add undefined parkeys as "UNDEFINED"
-    header = data_file.ensure_keys_defined(header, parkeys + ["DATE-OBS", "TIME-OBS"])
+    header = data_file.ensure_keys_defined(header, parkeys)
     
     # Evaluate parkey relevance rules in the context of header to map
     # mode irrelevant parameters to N/A.
@@ -141,32 +125,30 @@ def _get_matching_header(loaded_rmap, reffile):
 
 # ============================================================================
 
-def main():
-    """Command line refactoring behavior."""
-    import crds
-    crds.handle_version()
+class RefactorScript(cmdline.Script):
+    """Command line script for modifying .rmap files."""
+
+    description = """
+    Modifies a reference mapping by adding the specified reference files.
+    """
     
-    if "--verbose" in sys.argv:
-        sys.argv.remove("--verbose")
-        log.set_verbose()
+    epilog = """    
+    """
     
-    if len(sys.argv) >= 5 and sys.argv[1] == "insert":
-        old_rmap = sys.argv[2]
-        new_rmap = sys.argv[3]
-        inserted_references = sys.argv[4:]
-        rmap_insert_references(old_rmap, new_rmap, inserted_references)
-        # update_derivation(old_rmap, new_rmap)
-    elif len(sys.argv) == 6 and sys.argv[1] == "set_header":
-        old_rmap = sys.argv[2]
-        new_rmap = sys.argv[3]
-        key = sys.argv[4]
-        value = sys.argv[5]
-        set_header_value(old_rmap, new_rmap, key, value)        
-    else:
-        print "usage: python -m crds.refactor insert <old_rmap> <new_rmap> <references...>"
-        print "usage: python -m crds.refactor set_header <old_rmap> <new_rmap> <key> <value>"
-        sys.exit(-1)
+    def add_args(self):
+        self.add_argument("command", nargs=1, choices=("insert","set_header"),
+            help="Name of refactoring command to perform.")
+        self.add_argument('old_rmap', type=cmdline.reference_mapping,
+            help="Reference mapping to modify by inserting references.")
+        self.add_argument('new_rmap', type=cmdline.reference_mapping,
+            help="Name of modified reference mapping output file.")        
+        self.add_argument('references', type=cmdline.reference_file, nargs="+",
+            help="Reference files to insert into `old_rmap` to produce `new_rmap`.")
+        
+    def main(self):
+        rmap_insert_references(self.args.old_rmap, self.args.new_rmap, self.args.references)
+
 
 if __name__ == "__main__":
-    main()
+    RefactorScript()()
 
