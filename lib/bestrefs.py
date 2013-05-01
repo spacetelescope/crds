05f11c1483906052db8745cc2c3f9782bab8e3c2
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-05-01 19:48:32 +0000

    More development of catalog enabled bestrefs script.   Modified cmdline.py and Script to support an argv parameter used to instantiate scripts for testing.   Add a test_bestrefs script and relocated some test data files.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@946 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- lib/bestrefs.py
+++ lib/bestrefs.py
@@ -60,7 +60,10 @@ class HeaderGenerator(object):
         filekinds = self.pmap.get_filekinds(hdr) #  XXX only includes filekinds in .pmap
         old_bestrefs = { key.lower(): val for (key, val) in hdr.items() if key.upper() in filekinds }
         log.verbose("Old best reference recommendations from", repr(source), "=", repr(old_bestrefs))
-        return old_bestrefs
+        return hdr, old_bestrefs
+    
+    def handle_besrefs(self, updates):
+        raise NotImplementedError("This dataset access mode doesn't support updates.")
 
 class FileHeaderGenerator(HeaderGenerator):
     @utils.cached
@@ -68,12 +71,19 @@ class FileHeaderGenerator(HeaderGenerator):
         """Get the best references recommendations recorded in the header of file `dataset`."""
         return data_file.get_header(filename, observatory=self.pmap.observatory)
 
+    def handle_updates(self, all_updates):
+        for source in all_updates:
+            updates = all_updates[source]
+            if updates:
+                log.verbose("Updating data", repr(source), "==>", repr(updates), verbosity=25)
+                update_file_bestrefs(self.pmap, source, updates)
+
 class DatasetHeaderGenerator(HeaderGenerator):
     def __init__(self, context, datasets):
         super(DatasetHeaderGenerator, self).__init__(context, datasets)
-        log.verbose("Dumping datasets from CRDS server for", repr(datasets), verbosity=25)
+        log.verbose("Dumping dataset parameters from CRDS server for", repr(datasets), verbosity=25)
         self.headers = api.get_dataset_headers_by_id(context, datasets)
-        log.verbose("Dumped", len(self.headers), "of", len(datasets), "datasets from CRDS server.", verbosity=25)
+        log.verbose("Dumped", len(self.headers), "of", len(datasets), "dataset parameters from CRDS server.", verbosity=25)
     
 class InstrumentHeaderGenerator(HeaderGenerator):
     def __init__(self, context, instruments):
@@ -81,9 +91,9 @@ class InstrumentHeaderGenerator(HeaderGenerator):
         self.instruments = instruments
         sorted_sources = []
         for instrument in instruments:
-            log.verbose("Dumping datasets for", repr(instrument), "from CRDS server.", verbosity=25)
+            log.verbose("Dumping dataset parameters for", repr(instrument), "from CRDS server.", verbosity=25)
             more = api.get_dataset_headers_by_instrument(context, instrument)
-            log.verbose("Dumped", len(more), "datasets for", repr(instrument), "from CRDS server.", verbosity=25)
+            log.verbose("Dumped", len(more), "dataset parameters for", repr(instrument), "from CRDS server.", verbosity=25)
             self.headers.update(more)
             sorted_sources.extend(sorted(more.keys()))
         self.sources = sorted_sources
@@ -101,22 +111,18 @@ def update_file_bestrefs(pmap, dataset, bestrefs):
     # Here we use the dataset file because we know we have the full path, 
     # whereas the reference we'd have to locate.
     instrument = utils.file_to_instrument(dataset)
-    pmap = rmap.get_cached_mapping(new_pmap_name)
     prefix = pmap.locate.get_env_prefix(instrument)
     
-    pyfits.setval(dataset, "CRDS_CTX", value=new_pmap_name, ext=0)
-    for key, value in bestrefs.items():
+    pyfits.setval(dataset, "CRDS_CTX", value=pmap.basename, ext=0)
+    for key, value in sorted(bestrefs.items()):
 #        XXX what to do here for failed startswith("NOT FOUND") lookups?
         if value.upper().startswith("NOT FOUND"):
             if "N/A" in value.upper():
                 value = "N/A"
         else:
-            value = prefix + value
+            value = (prefix + value).lower()
+        log.verbose("Setting data", repr(dataset), "type", repr(key), "=", repr(value))
         pyfits.setval(dataset, key, value=value, ext=0)            
-
-def update_db_bestrefs(self, pmap, dataset_id):
-    """Get the best references recommendations stored in the database for `dataset_id`."""
-    raise NotImplementedError("Database best reference updates aren't implemented yet.")
 
 # ============================================================================
 
@@ -131,29 +137,59 @@ Determines best references with respect to a context.
     
     def __init__(self, *args, **keys):
         super(BestrefsScript, self).__init__(*args, **keys)
-        self.updates = []
+        self.updates = {}
         self.parameter_cache = {}
         self.old_bestrefs_cache = {}
     
         self.process_filekinds = [typ.lower() for typ in self.args.types ]
     
         # do one time startup outside profiler.
-        self.newctx = rmap.get_cached_mapping(self.args.new_context)
-        self.oldctx = None if self.args.old_context is None else rmap.get_cached_mapping(self.args.old_context)
-        self.new_headers = self.init_headers(self.args.new_context)
-        self.compare_prior, self.old_headers, self.old_bestrefs_name = self.init_comparison()
+        self.newctx, self.oldctx = \
+            self.setup_contexts()
+
+        self.new_headers = \
+            self.init_headers(self.new_context)
+        
+        self.compare_prior, self.old_headers, self.old_bestrefs_name = \
+            self.init_comparison()
+            
+    def setup_contexts(self):
+        """Determine and cache the new and comparison .pmap's for this run."""
+        if self.args.new_context is None:
+            log.verbose("Using default context", repr(self.default_context), "for computing updated best references.", verbosity=25)
+            self.new_context = self.default_context
+        else:
+            log.verbose("Using explicit context", repr(self.args.new_context), "for computing updated best references.", verbosity=25)
+            self.new_context = self.args.new_context
+        if self.args.old_context is not None:
+            log.verbose("Using explicit comparison context", repr(self.args.old_context), verbosity=25)
+        self.sync_contexts()
+        newctx = rmap.get_cached_mapping(self.new_context)
+        oldctx = rmap.get_cached_mapping(self.args.old_context)  if self.args.old_context else None
+        return newctx, oldctx
+
+    def sync_contexts(self):
+        """Recursively cache the new and comparison mappings."""
+        if self.new_context:
+            log.verbose("Syncing context", repr(self.new_context), verbosity=25)
+            api.dump_mappings(self.new_context)
+        if self.args.old_context:
+            log.verbose("Syncing context", repr(self.args.old_context), verbosity=25)
+            api.dump_mappings(self.args.old_context)
 
     def add_args(self):
         """Add bestrefs script-specific command line parameters."""
-        self.add_argument("new_context", type=cmdline.context_mapping,
-            help="New CRDS context/rules (.pmap or .imap) used to determine best references.")
+        
+        self.add_argument("-n", "--new-context", dest="new_context", 
+            help="Compute the updated best references using this context.  Uses current operational context by default.",
+            default=None, type=cmdline.context_mapping)
         
         self.add_argument("-o", "--old-context", dest="old_context",
-            help="Compare best refs recommendations from two contexts.", 
+            help="Compare bestrefs recommendations from two contexts.", 
             metavar="OLD_CONTEXT", default=None, type=cmdline.context_mapping)
         
-        self.add_argument("-c", "--compare-cdbs", dest="compare_cdbs", action="store_true",
-            help="Compare best refs recommendations from two contexts.")
+        self.add_argument("-c", "--compare-source-bestrefs", dest="compare_source_bestrefs", action="store_true",
+            help="Compare new bestrefs recommendations to recommendations from data source,  files or database.")
         
         self.add_argument("-f", "--files", nargs="+", metavar="FILES", default=None,
             help="Dataset files to compute best references for.")
@@ -164,66 +200,73 @@ Determines best references with respect to a context.
         self.add_argument("-i", "--instruments", nargs="+", metavar="INSTRUMENTS", default=None,
             help="Instruments to compute best references for, all historical datasets.")
         
+        self.add_argument("--all", action="store_true", default=None,
+            help="Compute best references for cataloged datasets for all supported instruments.")
+        
         self.add_argument("-t", "--types", nargs="+",  metavar="REFERENCE_TYPES",  default=(),
             help="A list of reference types to process,  defaulting to all types.")
         
-        self.add_argument("-u", "--update-datasets", dest="update_datasets",
+        self.add_argument("-u", "--update-bestrefs",  dest="update_bestrefs",
             help="Update dataset headers with new best reference recommendations.", 
             action="store_true")
         
-        self.add_argument("-a", "--print-affected", dest="print_affected",
+        self.add_argument("--print-affected",
             help="Print names of data sets for which the new context would assign new references.",
             action="store_true")
     
-        self.add_argument("-n", "--print-new-references", dest="print_new_references",
-            help="Prints info messages detailing each reference file change.",
+        self.add_argument("--print-new-references",
+            help="Prints messages detailing each reference file change.",
             action="store_true")
     
-        self.add_argument("-b", "--print-best-references", dest="print_best_references",
-            help="Prints info messages describing all recommended references.",
+        self.add_argument("-b", "--print-best-references",
+            help="Prints messages describing all recommended references.",
             action="store_true")
     
     def locate_file(self, filename):
         """Locate a dataset file leaving the path unchanged. Applies to self.args.files"""
         return filename
-
+    
     def init_headers(self, context):
-        assert [self.args.files, self.args.datasets, self.args.instruments].count(None) == 2, \
-            "Can only specify one of: --files, --datasets, --instruments"
+        source_modes = [self.args.files, self.args.datasets, self.args.instruments, self.args.all].count(None)
+        assert 4 - source_modes == 1, \
+            "Can only specify one of: --files, --datasets, --instruments, --all.  Specified " + repr(source_modes)
         if self.args.files:
             new_headers = FileHeaderGenerator(context, self.args.files)
         elif self.args.datasets:
             self.test_server_connection()
             new_headers = DatasetHeaderGenerator(context, [dset.upper() for dset in self.args.datasets])
-        elif self.args.instruments:
+        elif self.args.instruments or self.args.all:
             self.test_server_connection()
-            new_headers = InstrumentHeaderGenerator(context, self.args.instruments)
+            instruments = self.newctx.locate.INSTRUMENTS if self.args.all else self.args.instruments
+            new_headers = InstrumentHeaderGenerator(context, instruments)
         else:
-            raise RuntimeError("Invalid header source configuration.   Specify --files, --datasets, or --instruments.")
+            raise RuntimeError("Invalid header source configuration.   Specify --files, --datasets, --instruments, or --all.")
         return new_headers
     
     def init_comparison(self):
-        assert not (self.args.old_context and self.compare_cdbs), \
-            "Cannot specify both --old-context and --compare-cdbs."
-        compare_prior = self.args.old_context or self.args.compare_cdbs
+        assert not (self.args.old_context and self.compare_source_bestrefs), \
+            "Cannot specify both --old-context and --compare-source-bestrefs."
+        compare_prior = self.args.old_context or self.args.compare_source_bestrefs
         old_headers = old_fname = None
         if compare_prior:
             if self.args.old_context:
                 # XXX  old_headers = self.init_headers(self.args.old_context)  # ,  potentially different but slow
                 old_fname = self.args.old_context
             else:
-                old_fname = "SAVED BESTREFS"
+                old_fname = "recorded bestrefs"
             old_headers = self.new_headers
         return compare_prior, old_headers, old_fname
     
     def main(self):
         """Compute bestrefs for datasets."""
         
+        if not self.compare_prior:
+            log.info("No comparison context or source data bestrefs comparison requested;  skipping results comparisons.")
+
         for dataset in self.new_headers:
             # with log.error_on_exception("Failed processing", repr(dataset)):
             log.verbose("===> Processing", dataset, verbosity=25)
-            updates = self.process(dataset)            
-            self.updates.extend(updates)
+            self.updates[dataset] = self.process(dataset)
             
         self.handle_updates()
 
@@ -235,18 +278,19 @@ Determines best references with respect to a context.
         returns (dataset, new_context, new_bestrefs) or 
                 (dataset, new_context, new_bestrefs, old_context, old_bestrefs)
         """
-        new_bestrefs = self.get_bestrefs(self.new_headers, dataset)
+        header, new_bestrefs = self.get_bestrefs(self.new_headers, dataset)
+        instrument = self.newctx.get_instrument(header)
         if self.args.print_best_references or log.get_verbose():
-            log.info("Best references for", repr(dataset), "with respect to", repr(self.args.new_context), 
-                     "=", repr(new_bestrefs))
+            log.info("Best references for", repr(instrument), "data", repr(dataset), 
+                     "with respect to", repr(self.args.new_context), "=", repr(new_bestrefs))
         if self.compare_prior:
             if self.args.old_context:
-                old_bestrefs = self.get_bestrefs(self.old_headers, dataset)
+                old_header, old_bestrefs = self.get_bestrefs(self.old_headers, dataset)
             else:
-                old_bestrefs = self.old_headers.get_old_bestrefs(dataset)
+                old_header, old_bestrefs = self.old_headers.get_old_bestrefs(dataset)
             updates = self.compare_bestrefs(dataset, self.args.new_context, self.old_bestrefs_name, new_bestrefs, old_bestrefs)
         else:
-            updates = [(dataset, filekind, None, new) for (filekind, new) in sorted(new_bestrefs.items())]
+            updates = new_bestrefs
         return updates
     
     def get_bestrefs(self, header_gen, dataset):
@@ -254,14 +298,14 @@ Determines best references with respect to a context.
         try:
             header = header_gen.get_lookup_parameters(dataset)
             bestrefs = header_gen.pmap.get_best_references(header)
-            return bestrefs
+            return header, bestrefs
         except Exception, exc:
-            raise crds.CrdsError("Failed computing bestrefs for '{}' with respect to '{}' : {}" .format(dataset, header_gen.context, str(exc)))
+            raise crds.CrdsError("Failed computing bestrefs for data '{}' with respect to '{}' : {}" .format(dataset, header_gen.context, str(exc)))
 
     def compare_bestrefs(self, dataset, ctx1, ctx2, bestrefs1, bestrefs2):
         """Compare two sets of best references for `dataset` taken from contexts named `ctx1` and `ctx2`."""
     
-        updates = []
+        updates = {}
         
         errors = 0
         for filekind in (self.process_filekinds or bestrefs1):
@@ -272,44 +316,39 @@ Determines best references with respect to a context.
             
             old = cleanpath(bestrefs2.get(filekind, "UNDEFINED")).strip().upper()
         
-            if old.startswith(("N/A","NONE","","*")):
-                log.verbose("Old bestref marked as", repr(old), "for", repr(dataset), repr(u_filekind))
+            if old in ("N/A","NONE","","*"):
+                log.verbose("Old bestref marked as", repr(old), "for data", repr(dataset), "type", repr(u_filekind))
                 continue    
             if new.startswith("NOT FOUND N/A"):
-                log.verbose("Filetype not applicable for ", repr(dataset), repr(u_filekind))
+                log.verbose("Filetype not applicable for data", repr(dataset), "type", repr(u_filekind))
                 continue
             if new.startswith("NOT FOUND"):
                 errors += 1
-                log.error("Bestref FAILED for", repr(dataset), repr(u_filekind), new_org[len("NOT FOUND"):])
+                log.error("Bestref FAILED for data", repr(dataset), "type", repr(u_filekind), new_org[len("NOT FOUND"):])
                 continue
             if filekind not in bestrefs2:
-                log.warning("No comparison bestref for", repr(dataset), repr(u_filekind), "recommending -->", repr(new))
-                updates.append((dataset, filekind, None, new))
+                log.warning("No comparison bestref for data", repr(dataset), "type", repr(u_filekind), "recommending -->", repr(new))
+                updates[filekind] = new
                 continue
             
-            if old not in ["", "N/A","*"]:
-                if new != old:
-                    if self.args.print_new_references or log.get_verbose():
-                        log.info("New Reference for",  repr(dataset), repr(u_filekind), ":", repr(old), "-->", repr(new))
-                    updates.append((dataset, filekind, old, new))
-                else:
-                    log.verbose("Lookup MATCHES for", repr(dataset), repr(u_filekind), "=", repr(old), verbosity=30)
+            if new != old:
+                if self.args.print_new_references or log.get_verbose():
+                    log.info("New Reference for data",  repr(dataset), "type", repr(u_filekind), ":", repr(old), "-->", repr(new))
+                updates[filekind] = new
             else:
-                log.verbose("Lookup N/A for", repr(dataset), repr(u_filekind), repr(old), verbosity=30)
+                log.verbose("Lookup MATCHES for data", repr(dataset), "type", repr(u_filekind), "=", repr(old), verbosity=30)
             
         return updates
     
     def handle_updates(self):
         # (dataset, filekind, old, new)
         if self.args.print_affected:
-            self.print_affected()
-            
-    def print_affected(self):
-        seen = set()
-        for dataset, filekind, old, new in self.updates:
-            if dataset not in seen:
-                print(dataset)
-                seen.add(dataset)
+            for dataset in self.updates:
+                if self.updates[dataset]:
+                    print(dataset) 
+        if self.args.update_bestrefs:
+            log.verbose("Performing best references updates.")
+            self.new_headers.handle_updates(self.updates)
 
 # ===================================================================
 
