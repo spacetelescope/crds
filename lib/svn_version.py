2a10be10509cde895f9862cb094a08c1579ab5ef
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-09-11 14:43:58 +0000

    Improvements to pipeline_bestrefs shim script help and context specification.
    Modified bestrefs handling of dump_mappings error to issue error message and exit.
    
    Modified cmdline resolve_context to issue an info for symbolic context resolutions.   Fixed typo in unused get_conjugates
    
    Modified heavy_client get_final_context to treat -operational contexts as if they weren't specified so they still work from cache if running server-less.
    
    Added svn_version.py... with *old* version.
    
    
    
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1135 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

---
+++ lib/svn_version.py
@@ -0,0 +1,15 @@
+__svn_version__ = '1134M'
+
+__full_svn_info__ = '''
+Path: .
+URL: https://subversion.assembla.com/svn/crds/trunk
+Repository Root: https://subversion.assembla.com/svn/crds
+Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
+Revision: 1134
+Node Kind: directory
+Schedule: normal
+Last Changed Author: stsci_jmiller
+Last Changed Rev: 1131
+Last Changed Date: 2013-09-11 07:35:35 -0400 (Wed, 11 Sep 2013)
+
+'''
