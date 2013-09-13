33d3df6328945ff818dcb51331f1245ad9e0b35b
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-09-13 19:20:14 +0000

    svn_version.py updated
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1152 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- lib/svn_version.py
+++ lib/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1141:1142M'
+__svn_version__ = '1151M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1141
+Revision: 1151
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1141
-Last Changed Date: 2013-09-11 14:39:32 -0400 (Wed, 11 Sep 2013)
+Last Changed Rev: 1151
+Last Changed Date: 2013-09-13 14:05:11 -0400 (Fri, 13 Sep 2013)
 
 '''
