df0b91cb2f3fdc383b928acd807dab48136ce84f
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-09-27 14:03:37 +0000

    Improved command line error handling,  particularly for the case where the end-user sets the wrong CRDS_SERVER_URL making it impossible to contact the server.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1175 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- lib/svn_version.py
+++ lib/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1154:1162M'
+__svn_version__ = '1174M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1154
+Revision: 1174
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1153
-Last Changed Date: 2013-09-13 16:20:04 -0400 (Fri, 13 Sep 2013)
+Last Changed Rev: 1173
+Last Changed Date: 2013-09-26 15:14:14 -0400 (Thu, 26 Sep 2013)
 
 '''
