b2f23c7d52cccc9596647cb01ab8317ad641c66d
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-10-24 16:36:04 +0000

    Optimized crds.sync fetch_mappings() method to use cached mappings for much better sync --all performance.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1209 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- lib/svn_version.py
+++ lib/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1186:1191M'
+__svn_version__ = '1208M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1186
+Revision: 1208
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1185
-Last Changed Date: 2013-10-03 13:44:34 -0400 (Thu, 03 Oct 2013)
+Last Changed Rev: 1208
+Last Changed Date: 2013-10-22 17:45:42 -0400 (Tue, 22 Oct 2013)
 
 '''
