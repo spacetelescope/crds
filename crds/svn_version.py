e9020c78e013c4ec6ab376be7d93e5b99d8627af
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-11-25 15:41:28 +0000

    First pass renaming lib to crds and updating setup.py,  totally broken.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/reorg@1248 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1242M'
+__svn_version__ = '1244M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1242
+Revision: 1244
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1242
-Last Changed Date: 2013-11-22 15:07:16 -0500 (Fri, 22 Nov 2013)
+Last Changed Rev: 1244
+Last Changed Date: 2013-11-24 07:58:07 -0500 (Sun, 24 Nov 2013)
 
 '''
--- setup.py
+++ setup.py
@@ -5,46 +5,43 @@ import glob
 from distutils.core import setup
 
 subpkgs = {
-    "lib": {
+    "crds": {
         "packages" : [
             'crds',
+            'crds.client',
+            'crds.hst',
+            'crds.jwst',
+            'crds.tobs',
             'crds.tests',
+            'crds.cache',
+            'crds.cache.mappings',
+            'crds.cache.mappings.hst',
+            'crds.cache.mappings.jwst',
+            'crds.cache.mappings.tobs',
+            'crds.cache.references',
+            'crds.cache.references.hst',
+            'crds.cache.references.jwst',
+            'crds.cache.references.tobs',
             ],
         "package_dir" : {
-            'crds' : 'lib',
-            'crds.tests': 'lib/tests',
+            'crds' : 'crds',
+            'crds.client' : 'crds/client',
+            'crds.hst' : 'crds/hst',
+            'crds.jwst' : 'crds/jwst',
+            'crds.tobs' : 'crds/tobs',
+            'crds.tests' : 'crds/tests',
+            'crds.cache' : 'crds/cache',
+            'crds.cache.mappings' : 'crds/cache/mapping',
+            'crds.cache.mappings.hst' : 'crds/cache/mappings/hst',
+            'crds.cache.mappings.jwst' : 'crds/cache/mappings/jwst',
+            'crds.cache.mappings.tobs' : 'crds/cache/mappings/tobs',
+            'crds.cache.references' : 'crds/cache/references',
+            'crds.cache.references.hst' : 'crds/cache/references/hst',
+            'crds.cache.references.jwst' : 'crds/cache/references/jwst',
+            'crds.cache.references.tobs' : 'crds/cache/references/tobs',
             },
         "package_data" : {
             'crds.tests' : ['data/*.fits','data/*.*map'],
-            },
-        "scripts" : glob.glob("lib/scripts/*"),
-        },
-    "client" :{
-        "packages" : [
-            'crds.client',
-            ],
-        "package_dir" : {
-            'crds.client':'client',
-            },
-        "package_data" : {
-            },
-        },
-    "hst" : {
-        "packages" : [
-            'crds.hst',
-            'crds.mappings',
-            'crds.mappings.hst',
-            'crds.references',
-            'crds.references.hst',
-            ],
-        "package_dir" : {
-            'crds.hst':'hst',
-            'crds.mappings' : 'hst/mappings',
-            'crds.mappings.hst' : 'hst/mappings/hst',
-            'crds.references' : 'hst/references',
-            'crds.references.hst' : 'hst/references/hst',
-            },
-        "package_data" : {
             'crds.hst': [
                 'cdbs.paths.gz',
                 'cdbs/cdbs_tpns/*.tpn',
@@ -52,70 +49,39 @@ subpkgs = {
                 'cdbs/cdbs_tpns/cdbscatalog.dat',
                 '*.dat',
                 ],
-            'crds.mappings.hst' : [
-                '*.*map',
-                ],
             },
-        },
-    "jwst" : {
-        "packages" : [
-            'crds.jwst',
-            'crds.mappings',
-            'crds.mappings.jwst',
-            'crds.references',
-            'crds.references.jwst',
-            ],
-        "package_dir" : {
-            'crds.jwst':'jwst',
-            'crds.mappings' : 'jwst/mappings',
-            'crds.mappings.jwst' : 'jwst/mappings/jwst',
-            'crds.references' : 'jwst/references',
-            'crds.references.jwst' : 'jwst/references/jwst',
-            },
-        "package_data" : {
             'crds.jwst': [
                 '*.dat',
                 'tpns/*.tpn',
                 ],
-            'crds.mappings.jwst' : [
-                '*.*map',
-                ],
-            },
-        },
-    "tobs": {
-        "packages" : [
-            'crds.tobs',
-            'crds.tobs.tinstr',           
-            'crds.mappings',
-            'crds.mappings.tobs',
-            'crds.references',
-            'crds.references.tobs',
-            ],
-
-        "package_dir" : {
-            'crds.tobs':'tobs',
-            'crds.tobs.tinstr':'tobs/tinstr',
-            'crds.mappings' : 'tobs/mappings',
-            'crds.mappings.tobs' : 'tobs/mappings/tobs',
-            'crds.references' : 'tobs/references',
-            'crds.references.tobs' : 'tobs/references/tobs',
-            },
-        "package_data" : {
             'crds.tobs': [
                 '*.dat',
                 'tpns/*.tpn',
                 ],
-            'crds.mappings.tobs' : [
+            'crds.cache.mappings.hst' : [
                 '*.*map',
                 ],
-            'crds.references.tobs' : [
+            'crds.cache.mappings.jwst' : [
+                '*.*map',
+                ],
+            'crds.cache.mappings.tobs' : [
+                '*.*map',
+                ],
+            },
+        "scripts" : glob.glob("crds/scripts/*"),
+        },
+        "package_data" : {
+            'crds.cache.mappings.tobs' : [
+                '*.*map',
+                ],
+            'crds.cache.references.tobs' : [
                 '*.*fits',
                 ],
             },
-        }
+        },
     }
 
-installed_subpkgs = ["lib", "client", "tobs"]
+installed_subpkgs = ["crds", "client", "tobs"]
 if "--hst" in sys.argv:
     sys.args.remove("--hst")
     installed_subpkgs.append("hst")
