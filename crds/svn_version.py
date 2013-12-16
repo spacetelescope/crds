2209f39023dc045043d4791703d9e939545e6149
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-12-16 20:57:30 +0000

    Changed default datasets_since date from None meaning forever to 1900-01-01.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1284 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1272:1282M'
+__svn_version__ = '1283M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1272
+Revision: 1283
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1270
-Last Changed Date: 2013-12-11 16:52:37 -0500 (Wed, 11 Dec 2013)
+Last Changed Rev: 1283
+Last Changed Date: 2013-12-16 14:04:21 -0500 (Mon, 16 Dec 2013)
 
 '''
