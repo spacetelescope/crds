055e5eeefc46bbd498b8864285994565d3cd6849
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-12-17 22:14:23 +0000

    Adjusted table mode log messages in certify
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1297 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1285M'
+__svn_version__ = '1296M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1285
+Revision: 1296
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1285
-Last Changed Date: 2013-12-17 08:34:07 -0500 (Tue, 17 Dec 2013)
+Last Changed Rev: 1296
+Last Changed Date: 2013-12-17 16:04:19 -0500 (Tue, 17 Dec 2013)
 
 '''
