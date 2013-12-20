d5e39e7917ea2c92173ccd17b92923336cd3909a
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-12-20 22:49:58 +0000

    Work on HST ACS BIASFILE rmap generation and updates,  attempting to get rid of the precondition_header hook in favor of a more sophisticated rmap which captures the extra cases covered by the preconditioning.
    Ditched reference_match_fallback_header hook for an iterator scheme rmap_update_headers hook which generates a series of reference headers from a seed header.   For ACS biasfile it generates a preconditioned header and a fallback header which are both added to the rmap.
    
    
    
    
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1309 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1296M'
+__svn_version__ = '1297:1308M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1296
+Revision: 1297
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1296
-Last Changed Date: 2013-12-17 16:04:19 -0500 (Tue, 17 Dec 2013)
+Last Changed Rev: 1297
+Last Changed Date: 2013-12-17 17:14:23 -0500 (Tue, 17 Dec 2013)
 
 '''
--- crds/tests/test_config.py
+++ crds/tests/test_config.py
@@ -1,12 +1,14 @@
 import os, tempfile, shutil
 
+# must set env before crds import,  even package __init___
+os.environ["CRDS_PATH"] = os.path.join(tempfile.mkdtemp(), "test_cache")
+os.environ["CRDS_SERVER_URL"] = "https://hst-crds.stsci.edu"
+
 from crds import log
 
 def setup():
     # log.set_verbose()
     # os.environ["CRDS_PATH"] = os.path.join(os.getcwd(), "test_cache")
-    os.environ["CRDS_PATH"] = os.path.join(tempfile.mkdtemp(), "test_cache")
-    os.environ["CRDS_SERVER_URL"] = "https://hst-crds.stsci.edu"
     log.set_test_mode()
 
 def cleanup():
