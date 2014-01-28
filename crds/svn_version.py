4b04800754a01e6655222ac9ee645743dc07b622
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-01-28 18:23:33 +0000

    Made rmap hook output debug info default verbosity level.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1373 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1359:1368M'
+__svn_version__ = '1372M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1359
+Revision: 1372
 Node Kind: directory
 Schedule: normal
-Last Changed Author: stscieisenhamer
-Last Changed Rev: 1359
-Last Changed Date: 2014-01-22 12:44:51 -0500 (Wed, 22 Jan 2014)
+Last Changed Author: stsci_jmiller
+Last Changed Rev: 1372
+Last Changed Date: 2014-01-27 17:20:51 -0500 (Mon, 27 Jan 2014)
 
 '''
