375287d1dbb2fd738e0b02aefd93bc13eb10bca5
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-02-07 22:13:49 +0000

    Added get_best_references_by_ids() as baseline for MAST/Starview service.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1414 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1399:1412M'
+__svn_version__ = '1413M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1399
+Revision: 1413
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1399
-Last Changed Date: 2014-02-05 10:54:53 -0500 (Wed, 05 Feb 2014)
+Last Changed Rev: 1413
+Last Changed Date: 2014-02-07 16:32:37 -0500 (Fri, 07 Feb 2014)
 
 '''
