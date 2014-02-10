82fedc0791bf69a8b7a98d5eb2e9f71b01222ed9
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-02-10 22:02:10 +0000

    Added basedline get_best_references_by_header_map. Modified data_file FITS functions to suppress keys of ''.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1427 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1421M'
+__svn_version__ = '1413:1426M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1421
+Revision: 1413
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1420
-Last Changed Date: 2014-02-10 07:12:32 -0500 (Mon, 10 Feb 2014)
+Last Changed Rev: 1413
+Last Changed Date: 2014-02-07 16:32:37 -0500 (Fri, 07 Feb 2014)
 
 '''
