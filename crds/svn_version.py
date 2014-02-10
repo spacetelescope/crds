bc46e79efaace955049ef2a7b3947109103edf92
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-02-10 18:18:18 +0000

    Added debug output to HST WFC3 BIASFILE hook.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1424 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1416:1419M'
+__svn_version__ = '1421M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1416
+Revision: 1421
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1416
-Last Changed Date: 2014-02-08 08:11:46 -0500 (Sat, 08 Feb 2014)
+Last Changed Rev: 1420
+Last Changed Date: 2014-02-10 07:12:32 -0500 (Mon, 10 Feb 2014)
 
 '''
