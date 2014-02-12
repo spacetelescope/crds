fe814cf484de1fea654994f000c971800f5d34bd
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-02-12 22:32:11 +0000

    Fixed test_diff issues with pyfits version. Fixed uses.py doctest to run with envs/hst-crds-readonly.csh
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1431 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1429M'
+__svn_version__ = '1429:1430M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1429
+Revision: 1430
 Node Kind: directory
 Schedule: normal
-Last Changed Author: stscieisenhamer
-Last Changed Rev: 1429
-Last Changed Date: 2014-02-11 18:11:26 -0500 (Tue, 11 Feb 2014)
+Last Changed Author: stsci_jmiller
+Last Changed Rev: 1430
+Last Changed Date: 2014-02-12 15:55:08 -0500 (Wed, 12 Feb 2014)
 
 '''
