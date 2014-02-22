8458f4a0e616587e14ebd7be0dfeecd7a044a2f6
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-02-22 13:27:47 +0000

    Tweaked Mapping.todict() format to more consistently use tuples vs. lists.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1435 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1429:1430M'
+__svn_version__ = '1433:1434M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1430
+Revision: 1433
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1430
-Last Changed Date: 2014-02-12 15:55:08 -0500 (Wed, 12 Feb 2014)
+Last Changed Rev: 1433
+Last Changed Date: 2014-02-20 11:07:40 -0500 (Thu, 20 Feb 2014)
 
 '''
