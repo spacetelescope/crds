3f64e14813085a7047a47dbd2ead38c0a3cff045
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-02-24 14:35:48 +0000

    Bumped CRDS version to 1.0 for build-6
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1436 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/__init__.py
+++ crds/__init__.py
@@ -1,6 +1,6 @@
 # The "crds" __version__ defined here should also reflect the behavior of 
 # crds.client
-__version__ = "0.4.0"
+__version__ = "1.0"
 
 # ============================================================================
 
