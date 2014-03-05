e7dd161f7007fce98c670656e311ce67ec0263b3
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-03-05 14:15:19 +0000

    Overhauled calls to __init__ (used super) in rmap.py and selectors.py.
    Added consistent handling of * and N/A to all Matchers's in selector.py.
    
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1446 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1443M'
+__svn_version__ = '1445M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1443
+Revision: 1445
 Node Kind: directory
 Schedule: normal
-Last Changed Author: stscieisenhamer
-Last Changed Rev: 1443
-Last Changed Date: 2014-02-27 15:26:51 -0500 (Thu, 27 Feb 2014)
+Last Changed Author: stsci_jmiller
+Last Changed Rev: 1445
+Last Changed Date: 2014-02-28 09:52:51 -0500 (Fri, 28 Feb 2014)
 
 '''
