a4862cad495bb0388e9f913fd81fc2f0b3af36f9
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-03-11 16:27:45 +0000

    Partial fix for bad_mappings attempt to load context prior to sync.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1463 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1456:1461M'
+__svn_version__ = '1462M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1456
+Revision: 1462
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1456
-Last Changed Date: 2014-03-06 16:36:47 -0500 (Thu, 06 Mar 2014)
+Last Changed Rev: 1462
+Last Changed Date: 2014-03-11 09:34:43 -0400 (Tue, 11 Mar 2014)
 
 '''
