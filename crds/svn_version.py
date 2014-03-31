e6851e65a3fefb0957f8b3d26b48c96a05c92256
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-03-31 20:57:22 +0000

    Added transparent encoding/decoding routines to crds.client.proxy to support compression for dataset header downloads which were failing due to HTTP response limits... probably in the Apache proxy.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1485 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1478M'
+__svn_version__ = '1484M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1478
+Revision: 1484
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1478
-Last Changed Date: 2014-03-24 12:48:37 -0400 (Mon, 24 Mar 2014)
+Last Changed Rev: 1483
+Last Changed Date: 2014-03-28 12:25:22 -0400 (Fri, 28 Mar 2014)
 
 '''
