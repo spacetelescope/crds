11c079fa47a6b913eba95b81c04f2d9b1497dd1d
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-04-04 12:21:35 +0000

    crds.bestrefs dataset id database dump completeness checking,  issue warning if nothing dumped for requested id,  in particular no associated id.
    crds.sync added help comments.
    
    
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1498 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '1494M'
+__svn_version__ = ''
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1494
+Revision: 1497
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1494
-Last Changed Date: 2014-04-02 17:04:11 -0400 (Wed, 02 Apr 2014)
+Last Changed Rev: 1497
+Last Changed Date: 2014-04-03 13:02:03 -0400 (Thu, 03 Apr 2014)
 
 '''
