f3f6e2c371ba6c4da2b71e7250b9b9e9e5fc4916
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-04-09 15:06:15 +0000

    Fixed #232, WFC3 IMPHTAB table mode checking failure due to differing HDU counts and "typo" in error handling for it.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1507 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1499:1500M'
+__svn_version__ = '1506M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://subversion.assembla.com/svn/crds/trunk
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1499
+Revision: 1506
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1499
-Last Changed Date: 2014-04-04 12:53:52 -0400 (Fri, 04 Apr 2014)
+Last Changed Rev: 1504
+Last Changed Date: 2014-04-07 08:58:04 -0400 (Mon, 07 Apr 2014)
 
 '''
