ec812c7dd89b1d425cc7413cd296518da7937f59
jmiller <jmiller@stsci.edu>
2014-04-22 13:25:03 +0000

    HST remote pipeline concurrency wrappers,  take 2.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1518 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,16 @@
-__svn_version__ = ''
+__svn_version__ = '1516:1517M'
 
 __full_svn_info__ = '''
 Path: .
 Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
-URL: https://subversion.assembla.com/svn/crds/trunk
-Repository Root: https://subversion.assembla.com/svn/crds
+URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1511
+Revision: 1516
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1511
-Last Changed Date: 2014-04-14 10:10:22 -0400 (Mon, 14 Apr 2014)
+Last Changed Rev: 1516
+Last Changed Date: 2014-04-16 09:50:35 -0400 (Wed, 16 Apr 2014)
 
 '''
