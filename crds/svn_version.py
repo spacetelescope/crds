10a1722a96bf4603df6403df87c8aa133c50f079
jmiller <jmiller@stsci.edu>
2014-04-24 22:27:49 +0000

    More HST remote site parallel pipeline and sync wrapper script work, Added "cron_sync" and consolidated overall comments there.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1522 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1519M'
+__svn_version__ = '1520:1521M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1519
+Revision: 1520
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller
-Last Changed Rev: 1519
-Last Changed Date: 2014-04-22 12:46:51 -0400 (Tue, 22 Apr 2014)
+Last Changed Rev: 1520
+Last Changed Date: 2014-04-22 16:37:15 -0400 (Tue, 22 Apr 2014)
 
 '''
