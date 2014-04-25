9abf3e203c51cb118b15fb1b8c28ef10e0ad0dc5
jmiller <jmiller@stsci.edu>
2014-04-25 12:15:29 +0000

    More HST remote site script work, critical fixes and new debug output.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1524 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1520:1521M'
+__svn_version__ = '1523M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1520
+Revision: 1523
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller
-Last Changed Rev: 1520
-Last Changed Date: 2014-04-22 16:37:15 -0400 (Tue, 22 Apr 2014)
+Last Changed Rev: 1523
+Last Changed Date: 2014-04-24 18:32:03 -0400 (Thu, 24 Apr 2014)
 
 '''
