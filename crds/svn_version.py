456466069f3737a2a9fa9ea90ec37bed24903731
jmiller <jmiller@stsci.edu>
2014-04-30 15:33:48 +0000

    Modified bestrefs log messages to clarify exposure and product counts.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1528 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1524:1525M'
+__svn_version__ = '1527M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1524
+Revision: 1527
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller
-Last Changed Rev: 1524
-Last Changed Date: 2014-04-25 08:15:29 -0400 (Fri, 25 Apr 2014)
+Last Changed Rev: 1526
+Last Changed Date: 2014-04-25 14:49:18 -0400 (Fri, 25 Apr 2014)
 
 '''
