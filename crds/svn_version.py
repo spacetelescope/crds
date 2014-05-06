fb63aea703a5894e40790c049be12d019ade27c5
eisenham <eisenham@stsci.edu>
2014-05-06 21:45:45 +0000

    Updated to remove use of pyfits and replace with astropy.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1532 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,17 @@
-__svn_version__ = '1529M'
+__svn_version__ = '1531MS'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/eisenham/Documents/ssbdev/crds
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1529
+Revision: 1531
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller
-Last Changed Rev: 1528
-Last Changed Date: 2014-04-30 11:33:48 -0400 (Wed, 30 Apr 2014)
+Last Changed Rev: 1530
+Last Changed Date: 2014-05-03 12:05:07 -0400 (Sat, 03 May 2014)
 
 '''
