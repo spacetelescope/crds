e0d27933887f398a5fd40da56b028a7922048aad
jmiller <jmiller@stsci.edu>
2014-05-08 14:55:42 +0000

    Added --print-all-new-files to crds.diff as a dedicated mode which prints the names of all files added between `context1` and `context2`.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1533 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,16 @@
-__svn_version__ = '1531MS'
+__svn_version__ = ''
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/eisenham/Documents/ssbdev/crds
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
-Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1531
+Revision: 1532
 Node Kind: directory
 Schedule: normal
-Last Changed Author: jmiller
-Last Changed Rev: 1530
-Last Changed Date: 2014-05-03 12:05:07 -0400 (Sat, 03 May 2014)
+Last Changed Author: eisenham
+Last Changed Rev: 1532
+Last Changed Date: 2014-05-06 17:45:45 -0400 (Tue, 06 May 2014)
 
 '''
