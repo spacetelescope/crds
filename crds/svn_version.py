8edc0a24b05899e1f3c77086b0db9e7e08b3ae49
jmiller <jmiller@stsci.edu>
2014-05-12 14:19:45 +0000

    Added CRDS client version to crds.list --config
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1543 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '1539M'
+__svn_version__ = '1542M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1539
+Revision: 1542
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller
-Last Changed Rev: 1539
-Last Changed Date: 2014-05-10 17:49:26 -0400 (Sat, 10 May 2014)
+Last Changed Rev: 1540
+Last Changed Date: 2014-05-10 20:14:35 -0400 (Sat, 10 May 2014)
 
 '''
