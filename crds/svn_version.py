e8c06cb6331dd766b83740c802e2f3b952d24592
jmiller <jmiller@stsci.edu>
2014-05-15 18:48:53 +0000

    Consolidation of crds.bestrefs bestref value normalization code for screening and comparisons.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1555 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '1542M'
+__svn_version__ = '1554M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1542
+Revision: 1554
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller
-Last Changed Rev: 1540
-Last Changed Date: 2014-05-10 20:14:35 -0400 (Sat, 10 May 2014)
+Last Changed Rev: 1552
+Last Changed Date: 2014-05-14 15:26:23 -0400 (Wed, 14 May 2014)
 
 '''
