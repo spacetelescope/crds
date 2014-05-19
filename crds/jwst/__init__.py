f08f428eb976d8900ba4cd2a72cedf82e434423d
jmiller <jmiller@stsci.edu>
2014-05-19 18:28:27 +0000

    Added FRINGE type to JWST.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1562 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/jwst/__init__.py
+++ crds/jwst/__init__.py
@@ -2,10 +2,15 @@ INSTRUMENTS = ["miri","nirspec","nircam","niriss", "fgs"]
 
 EXTENSIONS = [".pmap", ".imap", ".rmap", ".fits", ]
 
+
+# NOTE: text descriptions for web page.
+# See also locate.py FILETYPE_TO_FILEKIND and FILEKIND_TO_FILETYPE
+
 TEXT_DESCR = {
     "flat" : "Flat Field",
     "photom" : "Absolute Calibration",
     "dark" : "Dark Frame",
+    "fringe" : "Spectral Fringing Correction Factors",
     "linearity" : "Detector Linearity Correction Coefficients",
     "mask" : "Bad Pixel Mask",
     "amplifier" : "Detector Amplifier Readout Parameters",
