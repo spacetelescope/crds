fd021c3a5a2dbb2b74965ad14668679046c63755
jmiller <jmiller@stsci.edu>
2014-06-02 14:46:06 +0000

    Modified FILE_RE to support multiple .'s in a filename but not a path.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1572 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1564:1565M'
+__svn_version__ = '1570M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1564
+Revision: 1570
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller
-Last Changed Rev: 1564
-Last Changed Date: 2014-05-21 16:45:04 -0400 (Wed, 21 May 2014)
+Last Changed Rev: 1566
+Last Changed Date: 2014-05-22 16:13:08 -0400 (Thu, 22 May 2014)
 
 '''
