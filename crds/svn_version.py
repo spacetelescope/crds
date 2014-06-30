029f72f4d7bd0e8c04b5f3b5b77707e6c54ea95d
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-06-30 16:26:54 +0000

    Fixed fatal bugs in crds.sync --datasets,  set + list,  no handling for NOT FOUND.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1622 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '1617M'
+__svn_version__ = ''
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1617
+Revision: 1619
 Node Kind: directory
 Schedule: normal
-Last Changed Author: iraf@stsci.edu
-Last Changed Rev: 1617
-Last Changed Date: 2014-06-25 15:31:12 -0400 (Wed, 25 Jun 2014)
+Last Changed Author: jmiller@stsci.edu
+Last Changed Rev: 1619
+Last Changed Date: 2014-06-26 14:01:19 -0400 (Thu, 26 Jun 2014)
 
 '''
