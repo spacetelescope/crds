df9274f1cac6a3e3bc7b76e9cd4702fcbb338fc7
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-07-09 19:08:29 +0000

    Improved error handling in cmdline.py mapping fetch.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1644 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = ''
+__svn_version__ = '1643M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1636
+Revision: 1643
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1635
-Last Changed Date: 2014-07-07 07:00:41 -0400 (Mon, 07 Jul 2014)
+Last Changed Rev: 1637
+Last Changed Date: 2014-07-07 12:20:58 -0400 (Mon, 07 Jul 2014)
 
 '''
