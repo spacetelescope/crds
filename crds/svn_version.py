96394af49241128aeff98155967f30d76624a5a3
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-07-11 16:15:32 +0000

    Fixed unit tests on trunk, whackamole.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1658 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '1653M'
+__svn_version__ = ''
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1653
+Revision: 1656
 Node Kind: directory
 Schedule: normal
-Last Changed Author: eisenham
-Last Changed Rev: 1649
-Last Changed Date: 2014-07-10 13:40:21 -0400 (Thu, 10 Jul 2014)
+Last Changed Author: jmiller@stsci.edu
+Last Changed Rev: 1654
+Last Changed Date: 2014-07-11 10:28:49 -0400 (Fri, 11 Jul 2014)
 
 '''
