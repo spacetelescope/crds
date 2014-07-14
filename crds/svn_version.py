26119ba0d514d4481313766040f07a7f8d4ebb48
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-07-14 13:38:52 +0000

    Dropped URL: and Version: from CRDS version info string recorded as CRDS_VER in FITS files since the old string was long enough to force CONTINUE records.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1662 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '1658:1660M'
+__svn_version__ = '1661M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1658
+Revision: 1661
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1658
-Last Changed Date: 2014-07-11 12:15:32 -0400 (Fri, 11 Jul 2014)
+Last Changed Rev: 1661
+Last Changed Date: 2014-07-11 17:37:09 -0400 (Fri, 11 Jul 2014)
 
 '''
