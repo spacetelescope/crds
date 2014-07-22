2698348846408eb8cd1a9d509c21f4fde6e3c6d1
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-07-22 16:16:20 +0000

    Added n/a handling to table_effects.py
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1682 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,16 @@
 __svn_version__ = ''
 
 __full_svn_info__ = '''
+Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
+URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+Repository Root: https://aeon.stsci.edu/ssb/svn/crds
+Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
+Revision: 1681
+Node Kind: directory
+Schedule: normal
+Last Changed Author: jmiller@stsci.edu
+Last Changed Rev: 1681
+Last Changed Date: 2014-07-22 12:08:04 -0400 (Tue, 22 Jul 2014)
+
 '''
