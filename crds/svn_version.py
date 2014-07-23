0bffc896f18cbe5133261a45a0c3bc908feedc5d
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-07-23 20:55:24 +0000

    Added --fetch-old-headers to crds.bestrefs to fetch dataset parameters twice, once relative to the old context, once relative to the new context, since the parameters may vary by .rmap.  This is 2x slower and ineffecient...  but conceptually the server might return something different.  It works,  if it proves required,  it can be sped up 2x.
    Modified clean to remove only crds/build, not every build under trunk.
    
    
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1685 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = ''
+__svn_version__ = '1684M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1681
+Revision: 1684
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1681
-Last Changed Date: 2014-07-22 12:08:04 -0400 (Tue, 22 Jul 2014)
+Last Changed Rev: 1684
+Last Changed Date: 2014-07-22 12:22:30 -0400 (Tue, 22 Jul 2014)
 
 '''
