82c6b4735bc4beda1008ff9be83f18b742679dab
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-08-11 14:21:18 +0000

    Modified clean script to clean only server "sources" directory when server code is installed.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1697 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1684M'
+__svn_version__ = '1696M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1684
+Revision: 1696
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1684
-Last Changed Date: 2014-07-22 12:22:30 -0400 (Tue, 22 Jul 2014)
+Last Changed Rev: 1691
+Last Changed Date: 2014-07-29 15:23:01 -0400 (Tue, 29 Jul 2014)
 
 '''
