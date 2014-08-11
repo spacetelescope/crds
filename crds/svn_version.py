456e0036df03781e80ddc8495a725a8d579c27b3
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-08-11 19:34:15 +0000

    crds.bestrefs changes:
    added --update-pickle to apply the bestrefs from the new context to the source headers prior to saving any output pickle.
    
    added .json support to --load-pickles and --save-pickle for long term storage.
    
    removed --fetch-old-headers invalid short parameter version.
    
    added sort to --print-affected-details output.
    
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1701 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '1698M'
+__svn_version__ = ''
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1698
+Revision: 1699
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1697
-Last Changed Date: 2014-08-11 10:21:18 -0400 (Mon, 11 Aug 2014)
+Last Changed Rev: 1699
+Last Changed Date: 2014-08-11 14:03:47 -0400 (Mon, 11 Aug 2014)
 
 '''
