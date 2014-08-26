b1fde77f7b602a5d7068cda229606a66b593e3e9
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-08-26 20:05:25 +0000

    Added @dataset_ids_file capability to crds.sync. Fixed case bug in --dataset-ids mode of crds.sync.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1739 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,16 @@
-__svn_version__ = '1731M'
+__svn_version__ = '1737M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS-trunk
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1731
+Revision: 1737
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1717
-Last Changed Date: 2014-08-15 11:39:45 -0400 (Fri, 15 Aug 2014)
+Last Changed Rev: 1737
+Last Changed Date: 2014-08-26 13:34:07 -0400 (Tue, 26 Aug 2014)
 
 '''
