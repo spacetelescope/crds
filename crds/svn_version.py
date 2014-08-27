50c026906c82059651996fa9691a9ccc0b4114b3
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-08-27 22:07:43 +0000

    Fixed query_affected_datasets to run on milestone-7-b which doesn't have the get_affected_datasets() service built in.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1744 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '1742M'
+__svn_version__ = '1743M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1742
+Revision: 1743
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1742
-Last Changed Date: 2014-08-26 18:05:57 -0400 (Tue, 26 Aug 2014)
+Last Changed Rev: 1743
+Last Changed Date: 2014-08-27 18:03:04 -0400 (Wed, 27 Aug 2014)
 
 '''
