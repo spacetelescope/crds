63d8f0979b0536d21bf2ec3bc5efc94e2dbcadd0
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-09-02 15:16:39 +0000

    Added file count to crds/sync --check-files verbose output.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1758 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '1752M'
+__svn_version__ = '1751:1756M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1752
+Revision: 1751
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1752
-Last Changed Date: 2014-08-29 12:17:54 -0400 (Fri, 29 Aug 2014)
+Last Changed Rev: 1750
+Last Changed Date: 2014-08-29 10:27:23 -0400 (Fri, 29 Aug 2014)
 
 '''
