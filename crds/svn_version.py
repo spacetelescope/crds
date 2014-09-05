c65fe9bbde874cc43278a4adfe26346cfcdc6184
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-09-05 22:11:24 +0000

    Tweaked logging to prevent breaking unit tests. Updated all tests in crds/tests to use dev server and /grp/crds/cache. All passing except JWST not-implemented.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1777 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '1760:1769M'
+__svn_version__ = '1776M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1760
+Revision: 1776
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1760
-Last Changed Date: 2014-09-02 14:53:34 -0400 (Tue, 02 Sep 2014)
+Last Changed Rev: 1776
+Last Changed Date: 2014-09-05 14:50:31 -0400 (Fri, 05 Sep 2014)
 
 '''
