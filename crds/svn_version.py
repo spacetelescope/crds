c7bf80d26dcd67407050e1dc3799032fe9d193d7
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-09-06 12:31:51 +0000

    Fixed bug in certify.mapping_closure input paths on mappings ignored and converted to cache paths,  fail in Parsley parsing file not found.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1779 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '1776M'
+__svn_version__ = '1778M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1776
+Revision: 1778
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1776
-Last Changed Date: 2014-09-05 14:50:31 -0400 (Fri, 05 Sep 2014)
+Last Changed Rev: 1777
+Last Changed Date: 2014-09-05 18:11:24 -0400 (Fri, 05 Sep 2014)
 
 '''
