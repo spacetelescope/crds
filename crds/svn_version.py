9339350c744cccfa3c4d3e968dbf34e1260086ce
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-09-09 14:49:21 +0000

    Added baseline acs_dfl.tpn back.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1783 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '1778M'
+__svn_version__ = '1781M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1778
+Revision: 1781
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1777
-Last Changed Date: 2014-09-05 18:11:24 -0400 (Fri, 05 Sep 2014)
+Last Changed Rev: 1781
+Last Changed Date: 2014-09-08 14:52:45 -0400 (Mon, 08 Sep 2014)
 
 '''
