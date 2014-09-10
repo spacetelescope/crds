b294a3f0edbde7ab5fb8078488652a6a51a51277
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-09-10 21:38:43 +0000

    Modified exception handling to trap KeyboardInterrupt in the client api properly,  mostly to dispose of interrupted downloads.   Added code to trap keyboard interrupt normally in cmdline Scripts to get a shorter traceback,  use --pdb to get the whole thing.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1788 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/cmdline.py
+++ crds/cmdline.py
@@ -307,18 +307,24 @@ class Script(object):
 
     def __call__(self):
         """Run the script's main() according to command line parameters."""
-        if self.args.version:
-            _show_version()
-        elif self.args.profile:
-            if self.args.profile == "console":
-                self._console_profile(self._main)
+        try:
+            if self.args.version:
+                _show_version()
+            elif self.args.profile:
+                if self.args.profile == "console":
+                    self._console_profile(self._main)
+                else:
+                    cProfile.runctx("self._main()", locals(), locals(), self.args.profile)
+            elif self.args.pdb:
+                pdb.runctx("self._main()", locals(), locals())
             else:
-                cProfile.runctx("self._main()", locals(), locals(), self.args.profile)
-        elif self.args.pdb:
-            pdb.runctx("self._main()", locals(), locals())
-        else:
-            return self._main()
-    
+                return self._main()
+        except KeyboardInterrupt:
+            if self.args.pdb:
+                raise
+            else:
+                raise KeyboardInterrupt("Interrupted... quitting.")
+
     def _console_profile(self, function, sort_by="cumulative", top_n=100):
         """Run `function` under the profiler and print results to console."""
         pr = cProfile.Profile()
