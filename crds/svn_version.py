91d1c174c5123b90eb3b6b2642fe60f53ddcd161
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-09-20 11:29:39 +0000

    Switched header dumping for reference insert to info.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1823 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '1820:1821M'
+__svn_version__ = '1822M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1820
+Revision: 1822
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1820
-Last Changed Date: 2014-09-19 07:35:37 -0400 (Fri, 19 Sep 2014)
+Last Changed Rev: 1822
+Last Changed Date: 2014-09-19 20:17:06 -0400 (Fri, 19 Sep 2014)
 
 '''
