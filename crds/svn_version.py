cd9f64500ddca93f890f829cb960e1d5cee9556c
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-09-23 21:24:27 +0000

    Merged do_not_scale_image_data=True fix from opus_2014.3 branch to trunk.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1831 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '1822M'
+__svn_version__ = '1829M'
 
 __full_svn_info__ = '''
 Path: .
-URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS-7
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.3
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1822
+Revision: 1829
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1822
-Last Changed Date: 2014-09-19 20:17:06 -0400 (Fri, 19 Sep 2014)
+Last Changed Rev: 1818
+Last Changed Date: 2014-09-18 16:14:29 -0400 (Thu, 18 Sep 2014)
 
 '''
