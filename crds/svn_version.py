5986d0e99c957ecb780969a2f57b552055a67c3c
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-09-30 16:22:47 +0000

    Quoted FILETYPE in acs_dfl.tpn
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1843 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = ''
+__svn_version__ = '1841M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1831
+Revision: 1841
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1831
-Last Changed Date: 2014-09-23 17:24:27 -0400 (Tue, 23 Sep 2014)
+Last Changed Rev: 1835
+Last Changed Date: 2014-09-25 18:24:17 -0400 (Thu, 25 Sep 2014)
 
 '''
