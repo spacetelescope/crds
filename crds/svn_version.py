33a566a5129d6d6f7c9991ed4820990d501b8658
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-09-30 18:29:23 +0000

    Simplified config message.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1849 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1841M'
+__svn_version__ = '1848M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1841
+Revision: 1848
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1835
-Last Changed Date: 2014-09-25 18:24:17 -0400 (Thu, 25 Sep 2014)
+Last Changed Rev: 1848
+Last Changed Date: 2014-09-30 14:21:44 -0400 (Tue, 30 Sep 2014)
 
 '''
