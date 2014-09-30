d04c095ef07d434eb856d872249af44dbf17c266
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-09-30 16:56:27 +0000

    Dubious changes to utils.remove() and callers to enable it to work in atypical cache configurations.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/ref_subdirs@1845 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '1840M'
+__svn_version__ = '1841:1842M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/ref_subdirs
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1840
+Revision: 1841
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1840
-Last Changed Date: 2014-09-29 14:17:58 -0400 (Mon, 29 Sep 2014)
+Last Changed Rev: 1841
+Last Changed Date: 2014-09-29 16:31:16 -0400 (Mon, 29 Sep 2014)
 
 '''
