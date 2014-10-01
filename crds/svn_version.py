342967fee55883c88c9fa626165440fa6a3ea1ff
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-01 20:19:22 +0000

    Added hst.locate.check_naming_consistency() for cross-checking file property decoding methods used to infer instrument and type from file name, file header, etc. as well as registration within the rules hierarchy, do the decoded instrument and type match the file's position in the rules?
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1856 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1853:1854M'
+__svn_version__ = '1854:1855M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1853
+Revision: 1855
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1852
-Last Changed Date: 2014-09-30 21:00:22 -0400 (Tue, 30 Sep 2014)
+Last Changed Rev: 1855
+Last Changed Date: 2014-10-01 12:15:47 -0400 (Wed, 01 Oct 2014)
 
 '''
