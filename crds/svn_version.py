b4deaec337f7659c296369ab02e358f18b7d4ff2
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-08 21:15:17 +0000

    Additional work on query_affected_datasets script to flesh it out for something closer to real-world use.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1878 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '1863:1874M'
+__svn_version__ = '1876M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1863
+Revision: 1876
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1863
-Last Changed Date: 2014-10-04 10:35:14 -0400 (Sat, 04 Oct 2014)
+Last Changed Rev: 1876
+Last Changed Date: 2014-10-08 10:44:15 -0400 (Wed, 08 Oct 2014)
 
 '''
