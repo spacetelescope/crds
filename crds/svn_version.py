b42955974f92e618b1e041b26e41f856f44e8c48
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-09 16:52:35 +0000

    Improved error handling for HST FITS references missing FILETYPE and CDBSFILE keywords rendering the filekind unidentifiable.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1882 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '1876:1878M'
+__svn_version__ = '1881M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1876
+Revision: 1881
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1876
-Last Changed Date: 2014-10-08 10:44:15 -0400 (Wed, 08 Oct 2014)
+Last Changed Rev: 1880
+Last Changed Date: 2014-10-08 17:25:20 -0400 (Wed, 08 Oct 2014)
 
 '''
