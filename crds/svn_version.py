f3780fdd11ab4dd1a125a0768578d980e0c4de1d
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-09 18:44:35 +0000

    Added reference_name_to_ld_tpn_text() to hst.locate and hst.tpn for web display.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1885 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1881:1882M'
+__svn_version__ = '1884M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1881
+Revision: 1884
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1880
-Last Changed Date: 2014-10-08 17:25:20 -0400 (Wed, 08 Oct 2014)
+Last Changed Rev: 1883
+Last Changed Date: 2014-10-09 13:52:55 -0400 (Thu, 09 Oct 2014)
 
 '''
