9b59fecf2101c6b0d60096db8fb1baa265d0a623
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-09 16:52:35 +0000

    Improved error handling for HST FITS references missing FILETYPE and CDBSFILE keywords rendering the filekind unidentifiable.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1882 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/hst/locate.py
+++ crds/hst/locate.py
@@ -18,6 +18,7 @@ import glob
 
 # import crds.pysh as pysh
 from crds import (log, rmap, pysh, data_file, config, utils, timestamp)
+from crds import CrdsError
 from crds.hst import (tpn, siname)
 
 HERE = os.path.dirname(__file__) or "./"
@@ -389,9 +390,13 @@ def ref_properties_from_header(filename):
         filetype = header["FILETYPE"].lower()
         filekind = tpn.filetype_to_filekind(instrument, filetype)
     except KeyError:
-        filetype = header["CDBSFILE"].lower()
-        filetype = TYPE_FIXERS.get((instrument, filetype), filetype)
-        filekind = tpn.filetype_to_filekind(instrument, filetype)
+        try:
+            filetype = header["CDBSFILE"].lower()
+        except KeyError:
+            raise CrdsError("File '{}' missing FILETYPE and CDBSFILE,  type not identifiable.".format(os.path.basename(filename)))
+        else:
+            filetype = TYPE_FIXERS.get((instrument, filetype), filetype)
+            filekind = tpn.filetype_to_filekind(instrument, filetype)
     return path, "hst", instrument, filekind, serial, ext
 
 # ============================================================================
