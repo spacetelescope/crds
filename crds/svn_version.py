0f8fcb06ef813a1f46f12e8dd66bbb4ec98a4b39
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-10 21:30:49 +0000

    self._profile() typo in cmdline.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/simplify_bestrefs@1898 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,15 @@
-__svn_version__ = '1893:1896M'
+__svn_version__ = '1897M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/simplify_bestrefs
-Relative URL: ^/branches/simplify_bestrefs
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1893
+Revision: 1897
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1893
-Last Changed Date: 2014-10-10 11:36:26 -0400 (Fri, 10 Oct 2014)
+Last Changed Rev: 1897
+Last Changed Date: 2014-10-10 17:27:46 -0400 (Fri, 10 Oct 2014)
 
 '''
