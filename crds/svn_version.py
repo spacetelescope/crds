dfcf15a7b0eea9e8ed7c161364ddf54c2c5d8ce7
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-10 12:40:12 +0000

    FIX: crds.bestrefs,  --diffs-only yields changes for 0 instruments fails.  Now exits with nothing affected.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1891 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1884M'
+__svn_version__ = '1890M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1884
+Revision: 1890
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1883
-Last Changed Date: 2014-10-09 13:52:55 -0400 (Thu, 09 Oct 2014)
+Last Changed Rev: 1887
+Last Changed Date: 2014-10-09 15:43:15 -0400 (Thu, 09 Oct 2014)
 
 '''
