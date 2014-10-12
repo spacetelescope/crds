f3c24eec3965766cc151e5f0b1aa38e33a7cef93
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-12 22:23:01 +0000

    Refactored cron_sync, folding in safe_sync and unsafe_sync due to locking error.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/simplify_bestrefs@1899 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1897M'
+__svn_version__ = '1898M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/simplify_bestrefs
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1897
+Revision: 1898
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1897
-Last Changed Date: 2014-10-10 17:27:46 -0400 (Fri, 10 Oct 2014)
+Last Changed Rev: 1898
+Last Changed Date: 2014-10-10 17:30:49 -0400 (Fri, 10 Oct 2014)
 
 '''
