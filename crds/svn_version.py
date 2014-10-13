74b61fbf71fbb9785fb2e31a39445054107cb689
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-13 18:07:03 +0000

    Updates to bestrefs to pass unit tests:
    1. Switched to sorted order of types in screening and comparisons.  (Long standing non-determinism?)  Updated two doctests after verifying before/after by sorting expected + got output,  identical when sorted.
    
    2. More work/bugfix on utils observatory and instrument determination functions,  added ALL_OBSERVATORIES to crds top level since it needs to be maintained whenever a new observatory package is added.
    
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/simplify_bestrefs@1906 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '1898:1901M'
+__svn_version__ = '1904M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/simplify_bestrefs
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1898
+Revision: 1904
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1898
-Last Changed Date: 2014-10-10 17:30:49 -0400 (Fri, 10 Oct 2014)
+Last Changed Rev: 1904
+Last Changed Date: 2014-10-13 12:23:10 -0400 (Mon, 13 Oct 2014)
 
 '''
