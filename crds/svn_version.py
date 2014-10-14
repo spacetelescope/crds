bebe8e3256781407d6f8ca02f8ed48195f7a8097
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-14 18:21:42 +0000

    Simplifications and generalizations in query_affected.py and api.get_context_history().
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/simplify_bestrefs@1913 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '1910:1911M'
+__svn_version__ = '1912M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/simplify_bestrefs
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1910
+Revision: 1912
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1908
-Last Changed Date: 2014-10-14 09:03:37 -0400 (Tue, 14 Oct 2014)
+Last Changed Rev: 1912
+Last Changed Date: 2014-10-14 13:07:47 -0400 (Tue, 14 Oct 2014)
 
 '''
