ed89684105debeda57ced1fdfd630067f23f3ff1
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-14 14:04:28 +0000

    Optimization to generally prevent multiple RPC calls to determine the default context for all command line scripts.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1910 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,17 @@
-__svn_version__ = '1890M'
+__svn_version__ = '1908:1909M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1890
+Revision: 1909
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1887
-Last Changed Date: 2014-10-09 15:43:15 -0400 (Thu, 09 Oct 2014)
+Last Changed Rev: 1909
+Last Changed Date: 2014-10-14 09:39:12 -0400 (Tue, 14 Oct 2014)
 
 '''
