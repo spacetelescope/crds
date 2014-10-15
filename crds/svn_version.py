f4bd567d96ae4b035ebc5c63829fc6f1e16199a0
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-15 13:06:20 +0000

    Updated svn_version.py on opus_2014.4 branch.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4@1916 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,16 @@
-__svn_version__ = '1912M'
+__svn_version__ = '1915M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
-URL: https://aeon.stsci.edu/ssb/svn/crds/branches/simplify_bestrefs
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS-opus_2014.4
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1912
+Revision: 1915
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1912
-Last Changed Date: 2014-10-14 13:07:47 -0400 (Tue, 14 Oct 2014)
+Last Changed Rev: 1915
+Last Changed Date: 2014-10-14 14:58:58 -0400 (Tue, 14 Oct 2014)
 
 '''
