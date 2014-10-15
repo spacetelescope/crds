678421068af74188d4fef6103440b041be5ab78e
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-15 16:20:41 +0000

    Added log.verbose command line (parameters) to Script.__init__(). Added reset_log=True parameter to Script.__init__(). Sorted values in certify debug output.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1917 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,16 @@
-__svn_version__ = '1912M'
+__svn_version__ = '1913:1914M'
 
 __full_svn_info__ = '''
 Path: .
 Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
-URL: https://aeon.stsci.edu/ssb/svn/crds/branches/simplify_bestrefs
+URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1912
+Revision: 1914
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1912
-Last Changed Date: 2014-10-14 13:07:47 -0400 (Tue, 14 Oct 2014)
+Last Changed Rev: 1914
+Last Changed Date: 2014-10-14 14:23:34 -0400 (Tue, 14 Oct 2014)
 
 '''
