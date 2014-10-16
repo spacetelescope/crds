c88f7baa000a3a91e40320b58389fd44826bde66
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-16 17:42:44 +0000

    Corrected the log message for query_affected datasets to make it show the end-point of the transition, i.e. the new context not the old one,  the one just made operational.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4@1925 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1921M'
+__svn_version__ = '1921:1924M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1921
+Revision: 1923
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1921
-Last Changed Date: 2014-10-15 20:18:20 -0400 (Wed, 15 Oct 2014)
+Last Changed Rev: 1923
+Last Changed Date: 2014-10-16 13:01:36 -0400 (Thu, 16 Oct 2014)
 
 '''
