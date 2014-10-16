013c290c07a96fbbf4ac4530d9f74782caab4a61
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-16 19:45:21 +0000

    Updated COS BPIXTAB filekind text string "bad Pixel" --> "Bad Pixel".
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4-server@1929 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1921:1924M'
+__svn_version__ = '1926M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1923
+Revision: 1926
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1923
-Last Changed Date: 2014-10-16 13:01:36 -0400 (Thu, 16 Oct 2014)
+Last Changed Rev: 1925
+Last Changed Date: 2014-10-16 13:42:44 -0400 (Thu, 16 Oct 2014)
 
 '''
