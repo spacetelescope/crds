5e020a74621fe76bef7699bfc82b327ccf13010d
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-22 17:37:20 +0000

    Moved header conditioning uniformly into rmap.get_best_references() and made conditioning=True the default.  That affects both the heavy_client and web server jsonapi callers to rmap.get_best_references.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/jwst_rmap_relevance@1942 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '1936:1937M'
+__svn_version__ = '1941M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/jwst_rmap_relevance
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1936
+Revision: 1941
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1936
-Last Changed Date: 2014-10-22 09:11:24 -0400 (Wed, 22 Oct 2014)
+Last Changed Rev: 1941
+Last Changed Date: 2014-10-22 11:45:18 -0400 (Wed, 22 Oct 2014)
 
 '''
