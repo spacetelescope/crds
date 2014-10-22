b5f2a437287cc0b1add2e4d6936f9ff918276c07
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-22 15:45:18 +0000

    Fix for rmap insert issue caused by parkey underscoring changes.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/jwst_rmap_relevance@1941 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,16 @@
-__svn_version__ = '1934M'
+__svn_version__ = '1936:1937M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/eisenham/Documents/ssbdev/crds-trunk
-URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
-Relative URL: ^/trunk
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/jwst_rmap_relevance
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1934
+Revision: 1936
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1934
-Last Changed Date: 2014-10-19 09:54:13 -0400 (Sun, 19 Oct 2014)
+Last Changed Rev: 1936
+Last Changed Date: 2014-10-22 09:11:24 -0400 (Wed, 22 Oct 2014)
 
 '''
