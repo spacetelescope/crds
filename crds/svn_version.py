3fbd422d54d414e6f293df876e6cfcff29fe36a5
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-25 20:19:37 +0000

    Removed cache cleanup from test_config since it uses /grp/crds/cache on the Central Store. Run on a server VM as crds,  the cleanup was deleting the /grp/crds/cache links to the project caches.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1953 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '1949:1951M'
+__svn_version__ = '1952M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1950
+Revision: 1952
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1950
-Last Changed Date: 2014-10-25 09:35:01 -0400 (Sat, 25 Oct 2014)
+Last Changed Rev: 1952
+Last Changed Date: 2014-10-25 12:23:38 -0400 (Sat, 25 Oct 2014)
 
 '''
