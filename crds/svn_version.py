bbe26dbbb4899704126e588cfc17ed63a6278d4d
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-25 15:32:45 +0000

    Modified rmap_relevance evaluation, 2nd version, to support evalable JWST header keywords,  more focused in get_best_refs leaving general functionality and parkey handling unchanged outside the scope of expression evaluation.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1951 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '1947M'
+__svn_version__ = '1949:1950M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1947
+Revision: 1950
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1946
-Last Changed Date: 2014-10-23 17:26:06 -0400 (Thu, 23 Oct 2014)
+Last Changed Rev: 1950
+Last Changed Date: 2014-10-25 09:35:01 -0400 (Sat, 25 Oct 2014)
 
 '''
