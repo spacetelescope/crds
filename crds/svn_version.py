67c267d674553cca32c57b72d11148cc80ea94d5
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-29 20:48:19 +0000

    Bumped CRDS trunk version to 1.3.0.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1955 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '1952M'
+__svn_version__ = '1954M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1952
+Revision: 1954
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1952
-Last Changed Date: 2014-10-25 12:23:38 -0400 (Sat, 25 Oct 2014)
+Last Changed Rev: 1954
+Last Changed Date: 2014-10-26 09:31:13 -0400 (Sun, 26 Oct 2014)
 
 '''
