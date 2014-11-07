f4aa505b48107bc4998c1d239924248785c36497
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-11-07 17:56:31 +0000

    More work on HST reference type config info consolidation.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1966 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/hst/reftypes.py
+++ crds/hst/reftypes.py
@@ -39,6 +39,8 @@ def _invert_instr_dict(map):
         inverted[instr] = utils.invert_dict(map[instr])
     return inverted
 
+# =============================================================================
+
 def consolidate():
     """One time generation function to consolidate piecemeal spec files."""
     from crds.hst import tpn, INSTRUMENTS, FILEKINDS
@@ -63,14 +65,14 @@ def consolidate():
             assign_field(consolidated, instr, filekind, "filetype", lambda: tpn.SUFFIX_TO_FILETYPE[instr][suffix])
             if instr == "stis" and filekind in ["lfltfile", "pfltfile"]:
                 assign_field(consolidated, instr, filekind, "tpn", lambda: tpn_cat[instr][filekind][0][1][0])
-                assign_field(consolidated, instr, filekind, "format", lambda: tpn_cat[instr][filekind][0][1][2])
+                assign_field(consolidated, instr, filekind, "reffile_format", lambda: tpn_cat[instr][filekind][0][1][2])
                 assign_field(consolidated, instr, filekind, "file_ext", lambda: tpn_cat[instr][filekind][0][1][3])
             else:
                 assign_field(consolidated, instr, filekind, "tpn", lambda: tpn_cat[instr][filekind][0])
-                assign_field(consolidated, instr, filekind, "format", lambda: tpn_cat[instr][filekind][2])
+                assign_field(consolidated, instr, filekind, "reffile_format", lambda: tpn_cat[instr][filekind][2])
                 assign_field(consolidated, instr, filekind, "file_ext", lambda: tpn_cat[instr][filekind][3])
             assign_field(consolidated, instr, filekind, "ld_tpn", lambda: ld_tpn_cat[instr][filekind][0])
-            if consolidated[instr][filekind]["format"] == "table":  # only tables have rowkeys
+            if consolidated[instr][filekind]["reffile_format"] == "table":  # only tables have rowkeys
                 assign_field(consolidated, instr, filekind, "unique_rowkeys", lambda: rowkeys[instr][filekind])
             else:
                 assign_field(consolidated, instr, filekind, "unique_rowkeys", lambda: None)
@@ -97,4 +99,91 @@ def assign_field(consolidated, instr, filekind, field, valuef):
     return value
 
 # =============================================================================
+
+UNIFIED_DEFS = _evalfile_with_fail(os.path.join(HERE, "reftypes.dat"))
+
+# .e.g. FILEKIND_TO_SUFFIX = {                 
+# 'acs': {'atodtab': 'a2d',
+#         'biasfile': 'bia',
+FILEKIND_TO_SUFFIX = { instr : { filekind : UNIFIED_DEFS[instr][filekind]["suffix" ] 
+                                 for filekind in UNIFIED_DEFS[instr] 
+                                } 
+                      for instr in UNIFIED_DEFS }
+SUFFIX_TO_FILEKIND = _invert_instr_dict(FILEKIND_TO_SUFFIX)
+
+#.e.g. FILETYPE_TO_SUFFIX = {
+# 'acs': {'analog-to-digital': 'a2d',
+#         'bad pixels': 'bpx',
+FILETYPE_TO_SUFFIX = { instr : { UNIFIED_DEFS[instr][filekind]["filetype"] : UNIFIED_DEFS[instr][filekind]["suffix" ] 
+                                 for filekind in UNIFIED_DEFS[instr] 
+                                } 
+                      for instr in UNIFIED_DEFS }
+SUFFIX_TO_FILEKIND = _invert_instr_dict(FILEKIND_TO_SUFFIX)
+
+# =============================================================================
+
+def filetype_to_filekind(instrument, filetype):
+    """Map the value of a FILETYPE keyword onto it's associated
+    keyword name,  i.e.  'dark image' --> 'darkfile'
+    """
+    instrument = instrument.lower()
+    filetype = filetype.lower()
+    if instrument == "nic":
+        instrument = "nicmos"
+    ext = FILETYPE_TO_SUFFIX[instrument][filetype]
+    return SUFFIX_TO_FILEKIND[instrument][ext]
+
+def extension_to_filekind(instrument, extension):
+    """Map the value of an instrument and TPN extension onto it's
+    associated filekind keyword name,  i.e. drk --> darkfile
+    """
+    if instrument == "nic":
+        instrument = "nicmos"
+    return SUFFIX_TO_FILEKIND[instrument][extension]
+
+# =============================================================================
+
+def mapping_validator_key(mapping):
+    """Return (_ld.tpn name, ) corresponding to CRDS ReferenceMapping `mapping` object."""
+    return (UNIFIED_DEFS[mapping.instrument][mapping.filekind]["ld_tpn"],)
+
+def reference_name_to_validator_key(filename):
+    """Given a reference filename `fitsname`,  return a dictionary key
+    suitable for caching the reference type's Validator.
+    
+    This revised version supports computing "subtype" .tpn files based
+    on the parameters of the reference.   Most references have unconditional
+    associations based on (instrument, filekind).   A select few have
+    conditional lookups which select between several .tpn's for the same
+    instrument and filetype.
+    
+    Returns (.tpn filename,)
+    """
+    header = data_file.get_header(filename)
+    instrument = header["INSTRUME"].lower()
+    filetype = header["FILETYPE"].lower()
+    filekind = filetype_to_filekind(instrument, filetype)
+    tpnfile = UNIFIED_DEFS[instrument][filekind]["tpn"]
+    if isinstance(tpnfile, basestring):
+        key = (tpnfile,)  # tpn filename
+    else: # it's a list of conditional tpns
+        for (condition, tpn) in tpnfile:
+            if eval(condition, header):
+                key = (tpn,)  # tpn filename
+                break
+        else:
+            raise ValueError("No TPN match for reference='{}' instrument='{}' reftype='{}'" % \
+                                 (os.path.basename(filename), instrument, filekind))
+    log.verbose("Validator key for", repr(filename), instrument, filekind, "=", key)
+    return key
+
+reference_name_to_tpn_key = reference_name_to_validator_key
+
+def reference_name_to_ld_tpn_key(filename):
+    """Return the _ld.tpn file key associated with reference `filename`.
+    Strictly speaking this should be driven by mapping_validator_key...  but the interface
+    for that is wrong so slave it to reference_name_to_tpn_key instead,  historically
+    one-for-one.
+    """
+    return (reference_name_to_tpn_key(filename)[0].replace(".tpn", "_ld.tpn"),)
 
--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,17 @@
-__svn_version__ = '1961:1964M'
+__svn_version__ = '1965M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1961
+Revision: 1965
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1960
-Last Changed Date: 2014-11-05 16:00:28 -0500 (Wed, 05 Nov 2014)
+Last Changed Rev: 1965
+Last Changed Date: 2014-11-06 17:14:30 -0500 (Thu, 06 Nov 2014)
 
 '''
