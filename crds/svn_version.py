067f69f83d1185ce254a349fbf104a6d76ef0e0e
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-11-09 02:00:51 +0000

    Fixed typos in STIS LFLT,PLFT assignment rules. Added support for conditional ld_tpn assigment.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1973 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,15 @@
-__svn_version__ = '1965:1971M'
+__svn_version__ = '1972M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
-Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1965
+Revision: 1972
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1965
-Last Changed Date: 2014-11-06 17:14:30 -0500 (Thu, 06 Nov 2014)
+Last Changed Rev: 1972
+Last Changed Date: 2014-11-07 17:44:25 -0500 (Fri, 07 Nov 2014)
 
 '''
