317e81d1ac6f21dd54c8739bc4cefece59ccf36d
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-11-18 22:10:45 +0000

    Modified InstrumentContext.get_rmap() to return N/A for JWST when the type is not represented in CRDS.  HST and TOBS still raise an exception.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1986 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/jwst/locate.py
+++ crds/jwst/locate.py
@@ -354,6 +354,14 @@ def locate_dir(instrument, mode=None):
     return rootdir
 
 # ============================================================================
+
+def handle_undefined_rmap(self, filekind):
+    """Customize how JWST handles undefined types for an instrument in InstrumentContext.get_rmap(): return N/A."
+    """
+    log.verbose("Type", repr(filekind), "is undefined for", repr(self.instrument), "returning N/A.")
+    return "N/A"
+
+# ============================================================================
 def load_all_type_constraints():
     """Load all the JWST type constraint files."""
     raise NotImplementedError("expected failure,  JWST type constraints not implemented yet.")
--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '1980:1981M'
+__svn_version__ = '1985M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1980
+Revision: 1985
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1980
-Last Changed Date: 2014-11-12 13:45:37 -0500 (Wed, 12 Nov 2014)
+Last Changed Rev: 1982
+Last Changed Date: 2014-11-13 09:53:19 -0500 (Thu, 13 Nov 2014)
 
 '''
