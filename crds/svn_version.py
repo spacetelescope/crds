e7f8bf1ca213f89fbdf9608f73f6c13f070b0629
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-11-20 21:17:53 +0000

    Fixed type-not-supported-for-instrument exception handling to correctly return N/A rather than raising an exception.  May revert to old exception later.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@1989 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '1985M'
+__svn_version__ = '1988M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1985
+Revision: 1988
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1982
-Last Changed Date: 2014-11-13 09:53:19 -0500 (Thu, 13 Nov 2014)
+Last Changed Rev: 1986
+Last Changed Date: 2014-11-18 17:10:45 -0500 (Tue, 18 Nov 2014)
 
 '''
