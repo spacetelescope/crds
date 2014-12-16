755610f61f6cacc1cfd4ec23d6e6edb6b50ce15c
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-12-16 22:27:13 +0000

    Fixed reftypes imports in HST locate.py as part of reftypes specs refactoring.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2032 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2022:2028M'
+__svn_version__ = '2031M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2022
+Revision: 2031
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2022
-Last Changed Date: 2014-12-15 12:57:18 -0500 (Mon, 15 Dec 2014)
+Last Changed Rev: 2031
+Last Changed Date: 2014-12-16 17:11:40 -0500 (Tue, 16 Dec 2014)
 
 '''
