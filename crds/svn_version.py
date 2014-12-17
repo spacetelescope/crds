0f25ba0e99dc15b08b3d6e1b6e68ecc1a0d733af
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-12-17 21:48:02 +0000

    Fixed date-based context resolution issue with imaps and rmaps.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2036 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2031M'
+__svn_version__ = '2032:2035M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2031
+Revision: 2032
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2031
-Last Changed Date: 2014-12-16 17:11:40 -0500 (Tue, 16 Dec 2014)
+Last Changed Rev: 2032
+Last Changed Date: 2014-12-16 17:27:13 -0500 (Tue, 16 Dec 2014)
 
 '''
