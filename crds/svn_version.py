8564c2d71f426f30f56db90f1951e813a633217f
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-12-18 17:40:13 +0000

    Changes to infrastructure so crds.bestrefs can run on a test .rmap instead of a cached .pmap.
    Modified core bestrefs logic in rmap.py so that ReferenceMapping implements a degenerate get_best_references() interface and now consolidates the error handling functionality instead of InstrumentMapping.  Updated unit tests in tobs broken by this change in exception handling.
    
    Modified several routines in client.api.py to pass the basename of the context to the server.
    
    Modified CrdsError in client.proxy.py to support multiple parameters which are space-joined akin to print().
    
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2038 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2036M'
+__svn_version__ = ''
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2036
+Revision: 2037
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2036
-Last Changed Date: 2014-12-17 16:48:02 -0500 (Wed, 17 Dec 2014)
+Last Changed Rev: 2037
+Last Changed Date: 2014-12-18 10:47:39 -0500 (Thu, 18 Dec 2014)
 
 '''
