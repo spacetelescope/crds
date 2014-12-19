9eb41d1b10dc7f90c263022b45ba9cfb8cad5f90
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-12-19 17:51:31 +0000

    Removed __main__ from HST locate.py
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2046 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,17 @@
-__svn_version__ = '2037:2039M'
+__svn_version__ = '2043:2045M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
+Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2037
+Revision: 2043
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2037
-Last Changed Date: 2014-12-18 10:47:39 -0500 (Thu, 18 Dec 2014)
+Last Changed Rev: 2040
+Last Changed Date: 2014-12-18 16:02:57 -0500 (Thu, 18 Dec 2014)
 
 '''
