36dc773a010a34c7569850d2900bb3ae5f3753ba
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-12-20 12:47:01 +0000

    More JWST .spec conversion,  modified generic reftypes to avoid explict use of INSTRUME, FILETYPE,  fixed tpn lookup parameter handling for JWST.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2053 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/reftypes.py
+++ crds/reftypes.py
@@ -190,9 +190,8 @@ class TypeParameters(object):
         Returns (.tpn filename,)
         """
         header = data_file.get_header(filename)
-        instrument = header["INSTRUME"].lower()
-        filetype = header["FILETYPE"].lower()
-        filekind = self.filetype_to_filekind(instrument, filetype)
+        observatory = data_file.get_observatory(filename)
+        instrument, filekind = utils.get_file_properties(observatory, filename)
         tpnfile = self.unified_defs[instrument][filekind][field]
         if isinstance(tpnfile, basestring):
             key = (tpnfile,)  # tpn filename
--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2050:2051M'
+__svn_version__ = '2052M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2050
+Revision: 2052
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2050
-Last Changed Date: 2014-12-19 15:58:46 -0500 (Fri, 19 Dec 2014)
+Last Changed Rev: 2052
+Last Changed Date: 2014-12-19 16:55:20 -0500 (Fri, 19 Dec 2014)
 
 '''
