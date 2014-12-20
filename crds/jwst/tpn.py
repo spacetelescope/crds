23d2eb49f031b3e6da36afe8a89ea87388c61840
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-12-20 12:47:01 +0000

    More JWST .spec conversion,  modified generic reftypes to avoid explict use of INSTRUME, FILETYPE,  fixed tpn lookup parameter handling for JWST.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2053 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/jwst/tpn.py
+++ crds/jwst/tpn.py
@@ -87,22 +87,13 @@ def _load_tpn(fname):
         tpn.append(TpnInfo(name, keytype, datatype, presence, tuple(values)))
     return tpn
 
-def _tpn_filepath(instrument, filekind, kind):
-    """Return the full path for the .tpn file corresponding to `instrument` and 
-    `filekind`,  the CRDS name for the header keyword which refers to this 
-    reference.
-    """
-    tpn_filename = "jwst_{}_{}.tpn".format(instrument, filekind)
-    path = os.path.join(HERE, "tpns", tpn_filename)
-    return path
-
 @utils.cached
 def get_tpninfos(*key):
     """Load the listof TPN info tuples corresponding to `instrument` and 
     `filekind` from it's .tpn file.
     """
     try:
-        return _load_tpn(_tpn_filepath(*key))
+        return _load_tpn(os.path.join(HERE, "tpns", key[0]))
     except IOError:
         log.verbose_warning("no TPN for", key)
         return []
