ac2e32e01724aa3c1670c5b29f5776abc2aae569
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-01-06 14:43:55 +0000

    More implementation of ACS DFLTFILE.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2073 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2069:2070M'
+__svn_version__ = '2071:2072M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2069
+Revision: 2071
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2069
-Last Changed Date: 2015-01-05 15:17:53 -0500 (Mon, 05 Jan 2015)
+Last Changed Rev: 2071
+Last Changed Date: 2015-01-05 16:55:19 -0500 (Mon, 05 Jan 2015)
 
 '''
