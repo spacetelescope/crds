3aa3063ab11bc11066168f1d5c4ceb59fa766453
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-01-08 21:13:04 +0000

    Bugfix for selectors.py considering N/A as esoteric and filtering it out of valid values list, breaking matches of UNDEFINED to that parkey discovered in JWST nightly unit tests.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2079 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2077M'
+__svn_version__ = '2078M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2077
+Revision: 2078
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2077
-Last Changed Date: 2015-01-06 16:07:22 -0500 (Tue, 06 Jan 2015)
+Last Changed Rev: 2078
+Last Changed Date: 2015-01-07 16:44:06 -0500 (Wed, 07 Jan 2015)
 
 '''
