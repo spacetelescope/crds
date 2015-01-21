0421eb3cd634e04c7490aebe6862f0090fc43535
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-01-21 21:43:30 +0000

    Added exit status to certify.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2095 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/certify.py
+++ crds/certify.py
@@ -1076,5 +1076,11 @@ Checks a CRDS reference or mapping file.
         
         return log.errors()
 
+def main():
+    """Construct and run the Certify script,  return 1 if errors occurred, 0 otherwise."""
+    errors = CertifyScript()()
+    exit_status = int(errors > 0)  # no errors = 0,  errors = 1
+    return exit_status
+
 if __name__ == "__main__":
-    CertifyScript()()
+    sys.exit(CertifyScript()())
