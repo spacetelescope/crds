43cf7f365182328f4117d5896bf6c2a2a57f5758
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-01-27 16:26:54 +0000

    Bumped CRDS version to 1.5 for OPUS > 2014.4a
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2108 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,16 @@
-__svn_version__ = '2104M'
+__svn_version__ = '2106:2107M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
-Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2104
+Revision: 2106
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2104
-Last Changed Date: 2015-01-22 16:53:46 -0500 (Thu, 22 Jan 2015)
+Last Changed Rev: 2106
+Last Changed Date: 2015-01-23 15:10:11 -0500 (Fri, 23 Jan 2015)
 
 '''
