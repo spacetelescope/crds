13e7343f18e61d4c2a4379d2e0f69e3e2d5faa97
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-01-28 17:56:15 +0000

    Optimized use of deepcopy in selectors.py loads,  skipping for no substitutions.  Factor of 2x for rmap loads.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2109 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2106:2107M'
+__svn_version__ = '2108M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2106
+Revision: 2108
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2106
-Last Changed Date: 2015-01-23 15:10:11 -0500 (Fri, 23 Jan 2015)
+Last Changed Rev: 2108
+Last Changed Date: 2015-01-27 11:26:54 -0500 (Tue, 27 Jan 2015)
 
 '''
