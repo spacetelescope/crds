ba9edcb7f17cbea952c207b7f8375233fa0dac4a
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-02-04 20:40:53 +0000

    Merged back from opus_2014.4a-server-2 to trunk to pick up parkey set arithmetic bugfix in rmap.py
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2124 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2108:2109M'
+__svn_version__ = '2122M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2108
+Revision: 2122
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2108
-Last Changed Date: 2015-01-27 11:26:54 -0500 (Tue, 27 Jan 2015)
+Last Changed Rev: 2110
+Last Changed Date: 2015-01-28 12:59:43 -0500 (Wed, 28 Jan 2015)
 
 '''
