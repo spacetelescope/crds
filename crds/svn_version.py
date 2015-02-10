22794842b8f4a465e48b94377864ae5cd92eb49b
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-02-10 15:07:46 +0000

    Renamed NIRISS FILTER type to NIRISS THROUGHPUT type.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2134 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2130M'
+__svn_version__ = '2133M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2130
+Revision: 2133
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2130
-Last Changed Date: 2015-02-09 13:18:56 -0500 (Mon, 09 Feb 2015)
+Last Changed Rev: 2131
+Last Changed Date: 2015-02-09 13:42:18 -0500 (Mon, 09 Feb 2015)
 
 '''
