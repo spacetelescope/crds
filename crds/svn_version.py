393891a82e7567d7e7772c576bea46de760a5938
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-03-10 12:53:21 +0000

    Removed dead locking comments related to an earlier implementation of safe_bestrefs.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2161 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2136:2141M'
+__svn_version__ = ''
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2136
+Revision: 2151
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2135
-Last Changed Date: 2015-02-10 10:16:17 -0500 (Tue, 10 Feb 2015)
+Last Changed Rev: 2142
+Last Changed Date: 2015-02-18 16:52:49 -0500 (Wed, 18 Feb 2015)
 
 '''
