25690fb72bef245c082e4d58330cd827c5dbab90
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-03-17 12:46:45 +0000

    Merged rmap_comments branch to trunk.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2169 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = ''
+__svn_version__ = '2168M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2151
+Revision: 2168
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2142
-Last Changed Date: 2015-02-18 16:52:49 -0500 (Wed, 18 Feb 2015)
+Last Changed Rev: 2161
+Last Changed Date: 2015-03-10 08:53:21 -0400 (Tue, 10 Mar 2015)
 
 '''
