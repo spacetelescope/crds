3e8c2ad3d1020737578c15bf47afd16c143b90cf
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-03-18 20:04:47 +0000

    Ongoing ASDF/certify changes,  untested snapshot.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/asdf_support@2177 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/tests/test_certify.py
+++ crds/tests/test_certify.py
@@ -7,16 +7,17 @@ import warnings
 import numpy as np
 
 import crds
-from crds import certify, utils, log
+from crds import certify, utils, log, certify
 
 from crds.tests import CRDSTestCase
 #from pyfits.tests.util import CaptureStdout, catch_warnings
 
 from nose.tools import assert_equal, assert_raises, assert_true, assert_false
 
-class TestHSTTpninfoClass(CRDSTestCase):
-    def setup(self):
-        CRDSTestCase.setup(self)
+class TestHSTTpnInfoClass(CRDSTestCase):
+
+    def setUp(self, *args, **keys):
+        super(TestHSTTpnInfoClass, self).setUp(*args, **keys)
         hstlocator = utils.get_locator_module("hst")
         self.tpninfos = hstlocator.get_tpninfos('acs_idc.tpn')
         self.validators = [certify.validator(info) for info in self.tpninfos]
@@ -31,15 +32,15 @@ class TestHSTTpninfoClass(CRDSTestCase):
     def test_column_validator(self):
         assert self.validators[-2].check(self.data('acs_new_idc.fits'))
 
-class TestValidatorClasses(CRDSTestCase):
+class TestCertifyClasses(CRDSTestCase):
 
-    def setup(self, *args, **keys):
+    def setUp(self, *args, **keys):
+        super(TestCertifyClasses, self).setUp(*args, **keys)
         self._old_debug = log.set_debug(True)
-        super(TestValidatorClasses, self).setup(*args, **keys)
 
-    def teardown(self, *args, **keys):
+    def tearDown(self, *args, **keys):
+        super(TestCertifyClasses, self).tearDown(*args, **keys)
         log.set_debug(self._old_debug)
-        super(TestValidatorClasses, self).teardown(*args, **keys)
         
     def test_character_validator(self):
         """Test the constructor with default argument values."""
@@ -55,24 +56,20 @@ class TestValidatorClasses(CRDSTestCase):
         cval.check(self.data('acs_new_idc.fits'))
 
     def test_check_dduplicates(self):
-        from crds import certify
         certify.certify_files([self.data("hst.pmap")], observatory="hst")
         certify.certify_files([self.data("hst_acs.imap")], observatory="hst")
         certify.certify_files([self.data("hst_acs_darkfile.rmap")], observatory="hst")
         
     def test_check_comment(self):
-        from crds import certify
         certify.certify_files([self.data("hst.pmap")], observatory="hst")
         certify.certify_files([self.data("hst_acs.imap")], observatory="hst")
         certify.certify_files([self.data("hst_acs_darkfile_comment.rmap")], observatory="hst")
         
     def test_table_mode_checks_identical(self):
-        from crds import certify
         certify.certify_files([self.data("v8q14451j_idc.fits")], observatory="hst", 
                               context="hst.pmap", compare_old_reference=True)
 
     def test_table_mode_checks_missing_modes(self):
-        from crds import certify
         certify.certify_files([self.data("v8q1445xx_idc.fits")], observatory="hst", 
                               context="hst.pmap", compare_old_reference=True)
         
@@ -87,12 +84,12 @@ class TestValidatorClasses(CRDSTestCase):
         locate.load_all_type_constraints()
 
     def test_JsonCertifier_valid(self):
-        cert = certify.JsonCertifier(self.data("core.schema.json"), 
+        cert = certify.JsonCertifier(self.data("valid.json"), 
                                      observatory="jwst",context="jwst.pmap")
         cert.certify()
             
     def test_JsonCertifier_invalid(self):
-        cert = certify.JsonCertifier(self.data("core.invalid.json"),
+        cert = certify.JsonCertifier(self.data("invalid.json"),
                                      observatory="jwst",context="jwst.pmap")
         assert_raises(certify.InvalidFormatError, cert.certify)
         
@@ -106,13 +103,23 @@ class TestValidatorClasses(CRDSTestCase):
                                      observatory="jwst",context="jwst.pmap")
         assert_raises(certify.InvalidFormatError, cert.certify)
         
-    def test_TextCertifier_valid(self):
-        cert = certify.TextCertifier(self.data("valid.text"), 
+    def test_AsdfCertifier_valid(self):
+        cert = certify.AsdfCertifier(self.data("valid.asdf"), 
                                      observatory="jwst",context="jwst.pmap")
         cert.certify()
             
-    def test_TextCertifier_missing(self):
-        cert = certify.TextCertifier(self.data("non-existent-file.txt"), 
+    def test_AsdfCertifier_invalid(self):
+        cert = certify.YamlCertifier(self.data("invalid.asdf"),
+                                     observatory="jwst",context="jwst.pmap")
+        assert_raises(certify.InvalidFormatError, cert.certify)
+        
+    def test_UnknownCertifier_valid(self):
+        cert = certify.UnknownCertifier(self.data("valid.text"), 
+                                     observatory="jwst",context="jwst.pmap")
+        cert.certify()
+            
+    def test_UnknownCertifier_missing(self):
+        cert = certify.UnknownCertifier(self.data("non-existent-file.txt"), 
                                      observatory="jwst",context="jwst.pmap")
         assert_raises(IOError, cert.certify)
         
