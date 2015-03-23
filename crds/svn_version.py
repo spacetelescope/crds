4c1cc7a8193713d32b09212992bda335e4ef3eac
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-03-23 18:10:18 +0000

    Added extension based file date inferencing to assist with name-based reversion checking across type changes.   Assumes no regression back from ASDF -> YMAL -> JSON -> FITS -> GEIS.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2187 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2181M'
+__svn_version__ = '2185M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2181
+Revision: 2185
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2181
-Last Changed Date: 2015-03-20 09:27:57 -0400 (Fri, 20 Mar 2015)
+Last Changed Rev: 2183
+Last Changed Date: 2015-03-20 14:11:01 -0400 (Fri, 20 Mar 2015)
 
 '''
