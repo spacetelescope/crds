31c596242522104752b82ef40c7ad35c0bfa84c3
jmiller <jmiller@stsci.edu>
2015-04-20 23:11:10 +0000

    Modified crds.bestrefs to include failed bestrefs in affected datasets since the DADSOPS catalog sometimes records flawed parameter sets which cannot be used to determine reprocessing.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2211 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,17 @@
-__svn_version__ = '2209M'
+__svn_version__ = '2210M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2209
+Revision: 2210
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2205
-Last Changed Date: 2015-04-15 10:13:05 -0400 (Wed, 15 Apr 2015)
+Last Changed Rev: 2210
+Last Changed Date: 2015-04-20 11:14:10 -0400 (Mon, 20 Apr 2015)
 
 '''
