d3946c5ea779376831584df11e15f8bce6f4ba9b
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-04-21 14:14:29 +0000

    Added failures in old-context bestrefs to reprocessing recommendations, not just new-context.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2212 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,15 @@
-__svn_version__ = '2210M'
+__svn_version__ = '2211M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
-Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2210
+Revision: 2211
 Node Kind: directory
 Schedule: normal
-Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2210
-Last Changed Date: 2015-04-20 11:14:10 -0400 (Mon, 20 Apr 2015)
+Last Changed Author: jmiller
+Last Changed Rev: 2211
+Last Changed Date: 2015-04-20 19:11:10 -0400 (Mon, 20 Apr 2015)
 
 '''
