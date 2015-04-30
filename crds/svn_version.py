d944706f4593c725340f6a2e6df72c188f3e6ed9
jmiller <jmiller@stsci.edu>
2015-04-30 19:32:31 +0000

    Additional tests for core rmap library identified by "coverage".
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2251 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,17 @@
-__svn_version__ = '2246:2248M'
+__svn_version__ = '2250M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
+Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2246
+Revision: 2250
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2246
-Last Changed Date: 2015-04-29 13:53:42 -0400 (Wed, 29 Apr 2015)
+Last Changed Rev: 2250
+Last Changed Date: 2015-04-30 13:53:16 -0400 (Thu, 30 Apr 2015)
 
 '''
