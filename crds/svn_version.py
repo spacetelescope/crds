ff422fca49dfb8db7bbf3509ac287bfd5cdfe08f
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-05-01 20:06:39 +0000

    Work on CRDS config state get and set for testing tweaks with restoration later. More rmap tests and test fixes.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2257 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,16 @@
-__svn_version__ = '2253M'
+__svn_version__ = '2254:2256M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
-Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2253
+Revision: 2254
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller
-Last Changed Rev: 2253
-Last Changed Date: 2015-04-30 17:58:09 -0400 (Thu, 30 Apr 2015)
+Last Changed Rev: 2254
+Last Changed Date: 2015-05-01 11:19:56 -0400 (Fri, 01 May 2015)
 
 '''
