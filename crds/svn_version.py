798560216ffa37df7d1ec6a8cb8ad4eaf7646913
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-05-13 20:06:45 +0000

    Merged opus_2014.4a-server-3 to trunk, COS FLATFILE prototype.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2283 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2273M'
+__svn_version__ = '2278M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
-URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4a-server-3
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2273
+Revision: 2278
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2273
-Last Changed Date: 2015-05-06 16:56:44 -0400 (Wed, 06 May 2015)
+Last Changed Rev: 2278
+Last Changed Date: 2015-05-13 12:14:20 -0400 (Wed, 13 May 2015)
 
 '''
