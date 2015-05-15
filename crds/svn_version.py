526ef975d51eaae6f75fe109c0e8ae744722fba7
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-05-15 17:22:00 +0000

    Fixed dict_values issue in newcontext by adding list().
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/python3@2293 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,16 @@
-__svn_version__ = '2277:2289M'
+__svn_version__ = '2290:2292M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
-URL: https://aeon.stsci.edu/ssb/svn/crds/branches/decoupled_tests
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS_python3
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/python3
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2277
+Revision: 2291
 Node Kind: directory
 Schedule: normal
-Last Changed Author: jmiller
-Last Changed Rev: 2277
-Last Changed Date: 2015-05-08 16:32:44 -0400 (Fri, 08 May 2015)
+Last Changed Author: jmiller@stsci.edu
+Last Changed Rev: 2291
+Last Changed Date: 2015-05-15 09:28:38 -0400 (Fri, 15 May 2015)
 
 '''
