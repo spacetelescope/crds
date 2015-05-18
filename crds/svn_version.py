bfbf8b911bbd7b2a176884dd57ff49a58076d560
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-05-18 15:12:32 +0000

    Fixed test_diff unit test and improved diff file syncing for --print-all-new-files.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/python3@2299 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,16 @@
-__svn_version__ = '2296M'
+__svn_version__ = '2298M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS_python3
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/python3
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2296
+Revision: 2298
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2296
-Last Changed Date: 2015-05-15 17:45:29 -0400 (Fri, 15 May 2015)
+Last Changed Rev: 2298
+Last Changed Date: 2015-05-18 09:27:51 -0400 (Mon, 18 May 2015)
 
 '''
