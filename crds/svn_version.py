094affbc927fa49189aa8944cb98bcdcd0440e3b
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-05-18 13:26:29 +0000

    newcontext doctest determinism changes. Added crds/cache to ./clean to remove test artifacts.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/python3@2297 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2290:2295M'
+__svn_version__ = '2296M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS_python3
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/python3
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2291
+Revision: 2296
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2291
-Last Changed Date: 2015-05-15 09:28:38 -0400 (Fri, 15 May 2015)
+Last Changed Rev: 2296
+Last Changed Date: 2015-05-15 17:45:29 -0400 (Fri, 15 May 2015)
 
 '''
