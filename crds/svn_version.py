abe0ee4586e1e4778166b815f2b3713232f600fe
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-05-27 13:41:06 +0000

    Added missing test data file ot python3 branch.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/python3@2336 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,16 @@
-__svn_version__ = '2331M'
+__svn_version__ = '2335M'
 
 __full_svn_info__ = '''
 Path: .
 Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
-URL: https://aeon.stsci.edu/ssb/svn/crds/branches/python3
+URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2331
+Revision: 2335
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller
-Last Changed Rev: 2331
-Last Changed Date: 2015-05-26 13:22:09 -0400 (Tue, 26 May 2015)
+Last Changed Rev: 2334
+Last Changed Date: 2015-05-26 16:47:06 -0400 (Tue, 26 May 2015)
 
 '''
