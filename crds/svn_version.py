5a26b16e68b3293279920271aa37d4d1ca1ba399
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-05-29 15:51:09 +0000

    Updated unit tests for certify file truncation to support warnings changes.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2345 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2342M'
+__svn_version__ = '2344M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2342
+Revision: 2344
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2342
-Last Changed Date: 2015-05-28 16:11:32 -0400 (Thu, 28 May 2015)
+Last Changed Rev: 2344
+Last Changed Date: 2015-05-29 11:00:41 -0400 (Fri, 29 May 2015)
 
 '''
