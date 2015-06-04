0270567eea4590dd4ceb3f171059ce6d14dde62d
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-06-04 12:51:03 +0000

    Updated HST builtin cache to latest mappings.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2353 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2347:2348M'
+__svn_version__ = '2350M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2347
+Revision: 2350
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2345
-Last Changed Date: 2015-05-29 11:51:09 -0400 (Fri, 29 May 2015)
+Last Changed Rev: 2350
+Last Changed Date: 2015-06-03 16:51:51 -0400 (Wed, 03 Jun 2015)
 
 '''
