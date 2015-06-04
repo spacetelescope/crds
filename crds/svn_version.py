47a5f353183f290c33f6e63004ebf23a3fd4a04f
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-06-04 14:14:51 +0000

    Added test cases for JWST getreferences() N/A and OMIT cases,  started test_heavy_client.py to add to as needed.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/na_omit@2358 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,16 @@
-__svn_version__ = '2336:2337M'
+__svn_version__ = '2357M'
 
 __full_svn_info__ = '''
 Path: .
 Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
-URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/na_omit
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2336
+Revision: 2357
 Node Kind: directory
 Schedule: normal
-Last Changed Author: jmiller
-Last Changed Rev: 2334
-Last Changed Date: 2015-05-26 16:47:06 -0400 (Tue, 26 May 2015)
+Last Changed Author: jmiller@stsci.edu
+Last Changed Rev: 2355
+Last Changed Date: 2015-06-04 09:09:29 -0400 (Thu, 04 Jun 2015)
 
 '''
