e471af94a7344ecbdd433b956da0fbb2b8a387c3
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-06-12 13:10:04 +0000

    Finished baseline na_omit diff using FileSelectionsDict approach.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/na_omit@2374 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2369M'
+__svn_version__ = '2373M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/na_omit
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2369
+Revision: 2373
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2369
-Last Changed Date: 2015-06-09 16:41:23 -0400 (Tue, 09 Jun 2015)
+Last Changed Rev: 2373
+Last Changed Date: 2015-06-11 13:16:46 -0400 (Thu, 11 Jun 2015)
 
 '''
