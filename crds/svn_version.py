7f0bab6e695c7d342e3ac13acac0e3af4fdb504b
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-06-15 11:34:54 +0000

    Updated version info on irafx.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2383 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,16 @@
-__svn_version__ = '2377M'
+__svn_version__ = '2380M'
 
 __full_svn_info__ = '''
 Path: .
 Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
-URL: https://aeon.stsci.edu/ssb/svn/crds/six_removal
+URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2377
+Revision: 2380
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2371
-Last Changed Date: 2015-06-10 11:27:30 -0400 (Wed, 10 Jun 2015)
+Last Changed Rev: 2379
+Last Changed Date: 2015-06-12 14:24:40 -0400 (Fri, 12 Jun 2015)
 
 '''
