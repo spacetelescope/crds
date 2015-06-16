e870ef3f91af012e1f39ff2b2f0e89fa1660f242
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-06-16 20:59:00 +0000

    New unit tests for Mapping.tojson() and .todict(),  corresponding bugfix pending.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2390 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2387M'
+__svn_version__ = '2389M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2387
+Revision: 2389
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2387
-Last Changed Date: 2015-06-16 13:20:19 -0400 (Tue, 16 Jun 2015)
+Last Changed Rev: 2389
+Last Changed Date: 2015-06-16 14:53:05 -0400 (Tue, 16 Jun 2015)
 
 '''
