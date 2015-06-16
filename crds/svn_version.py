9b25269ee420468d661bfeaf246b6b54643a31e3
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-06-16 18:53:05 +0000

    Added N/A and OMIT as special cases of file nqme re,  / normally restricted from names to avoids legal paths.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2389 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2380:2385M'
+__svn_version__ = '2387M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2380
+Revision: 2387
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2379
-Last Changed Date: 2015-06-12 14:24:40 -0400 (Fri, 12 Jun 2015)
+Last Changed Rev: 2387
+Last Changed Date: 2015-06-16 13:20:19 -0400 (Tue, 16 Jun 2015)
 
 '''
