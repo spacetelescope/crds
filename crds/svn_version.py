bdf20eceea7a01f33f11c79de45a0c175a3560d3
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-06-18 19:00:08 +0000

    Removed special values from ReferenceMapping.reference_names(),  only real files are returned.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2400 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2397M'
+__svn_version__ = '2398M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2397
+Revision: 2398
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2397
-Last Changed Date: 2015-06-18 09:43:34 -0400 (Thu, 18 Jun 2015)
+Last Changed Rev: 2398
+Last Changed Date: 2015-06-18 14:19:05 -0400 (Thu, 18 Jun 2015)
 
 '''
