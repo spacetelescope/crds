bf16aaebbad5f35a8f9c7ac7e517e6f2d851ff28
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-06-24 20:14:54 +0000

    Bugfix for leading substring match in crds.certify.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2423 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2414:2415M'
+__svn_version__ = '2421M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2414
+Revision: 2421
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2413
-Last Changed Date: 2015-06-23 12:04:03 -0400 (Tue, 23 Jun 2015)
+Last Changed Rev: 2416
+Last Changed Date: 2015-06-24 09:07:22 -0400 (Wed, 24 Jun 2015)
 
 '''
