ee0765d4075dff4cc0d040a27ee0cdb7e0a83f3a
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-07-13 23:49:04 +0000

    Added baseline niriss_area type spec.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2475 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2465M'
+__svn_version__ = '2471:2473M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2465
+Revision: 2471
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2465
-Last Changed Date: 2015-07-06 15:14:02 -0400 (Mon, 06 Jul 2015)
+Last Changed Rev: 2470
+Last Changed Date: 2015-07-12 18:13:41 -0400 (Sun, 12 Jul 2015)
 
 '''
