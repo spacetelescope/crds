d8351f4918b151bac403d34b396731117b8469c5
jmiller <jmiller@stsci.edu>
2015-07-24 22:16:22 +0000

    Started adding push_context() on cache_sync branch.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/cache_sync@2488 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,17 @@
-__svn_version__ = '2484M'
+__svn_version__ = '2487M'
 
 __full_svn_info__ = '''
 Path: .
 Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
-URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
-Relative URL: ^/trunk
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/cache_sync
+Relative URL: ^/branches/cache_sync
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2484
+Revision: 2487
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller
-Last Changed Rev: 2483
-Last Changed Date: 2015-07-17 12:45:29 -0400 (Fri, 17 Jul 2015)
+Last Changed Rev: 2487
+Last Changed Date: 2015-07-24 13:56:42 -0400 (Fri, 24 Jul 2015)
 
 '''
