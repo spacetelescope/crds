3bb3771e1a7f18ef34c489363d0b1a08742f5acd
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-07-26 19:23:19 +0000

    Fixed --push-context type decl and client.push_context parameter order on cache_sync branch.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/cache_sync@2489 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,15 @@
-__svn_version__ = '2487M'
+__svn_version__ = '2488M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/cache_sync
-Relative URL: ^/branches/cache_sync
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2487
+Revision: 2488
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller
-Last Changed Rev: 2487
-Last Changed Date: 2015-07-24 13:56:42 -0400 (Fri, 24 Jul 2015)
+Last Changed Rev: 2488
+Last Changed Date: 2015-07-24 18:16:22 -0400 (Fri, 24 Jul 2015)
 
 '''
