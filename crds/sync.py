8b13c918aa499b83f16e475a22a40b91c284324e
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-07-27 20:33:33 +0000

    Message formatting in sync log message.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/cache_sync@2492 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/sync.py
+++ crds/sync.py
@@ -304,7 +304,7 @@ class SyncScript(cmdline.ContextsScript):
         if old_context == new_context:
             log.error("Expected operational context switch but starting and post-sync contexts are both", repr(old_context))
         else:
-            log.info("Operational context  updated from", repr(old_context), "to",  repr(new_context))
+            log.info("Operational context updated from", repr(old_context), "to",  repr(new_context))
             
     def push_context(self):
         """Push the final context recorded in the local cache to the CRDS server so it can be displayed
