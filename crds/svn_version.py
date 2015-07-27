96cb95c299ab74a417b3ab825d98255083ea9cc2
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-07-27 20:33:33 +0000

    Message formatting in sync log message.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/cache_sync@2492 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2488:2489M'
+__svn_version__ = '2490M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/cache_sync
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2488
+Revision: 2490
 Node Kind: directory
 Schedule: normal
-Last Changed Author: jmiller
-Last Changed Rev: 2488
-Last Changed Date: 2015-07-24 18:16:22 -0400 (Fri, 24 Jul 2015)
+Last Changed Author: jmiller@stsci.edu
+Last Changed Rev: 2490
+Last Changed Date: 2015-07-26 17:08:53 -0400 (Sun, 26 Jul 2015)
 
 '''
