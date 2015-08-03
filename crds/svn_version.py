3c0ce126f7cd44d5c9d4482d69ab978787667255
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-03 20:35:07 +0000

    Reviewed cron_sync output and behavior for --verify-context-change and --push-context.
    Reworked cmdline._list_mappings() machinery (server api call) to eliminate
    _all_mappings member variable and leverage enhanced server_info call which
    includes list of all current mappings for use in client side globbing.  This
    helps eliminate list_mappings server API calls during cache syncs.
    
    Removed --verbose from cron_sync,  it can still be added as extra parameter, to
    reduce output clutter for pipeline.
    
    Fixed _CRDS_READONLY_CACHE global save/restore for table_effects unit test
    discrepancies under nose.
    
    Increased required verbosity for utils.clear_function_caches() status to 80.
    
    Renamed all_references and all_mappings cmdline.Script methods to
    are_all_references and are_all_mappings.
    
    
    
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2495 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2491M'
+__svn_version__ = '2494M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2491
+Revision: 2494
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2491
-Last Changed Date: 2015-07-27 14:52:14 -0400 (Mon, 27 Jul 2015)
+Last Changed Rev: 2493
+Last Changed Date: 2015-07-27 17:01:37 -0400 (Mon, 27 Jul 2015)
 
 '''
