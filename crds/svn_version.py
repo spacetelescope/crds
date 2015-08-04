3f426464c7a95b93fef4fc822e274037ca47d863
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-04 12:47:45 +0000

    Fixed lock error management in scripts/cron_sync
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2499 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2494M'
+__svn_version__ = '2495M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2494
+Revision: 2495
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2493
-Last Changed Date: 2015-07-27 17:01:37 -0400 (Mon, 27 Jul 2015)
+Last Changed Rev: 2495
+Last Changed Date: 2015-08-03 16:35:07 -0400 (Mon, 03 Aug 2015)
 
 '''
