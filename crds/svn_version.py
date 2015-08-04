67fe53b2fe553e0d07bbc70b89b016e9299fca75
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-04 13:41:59 +0000

    Modified setup.py and runtests to make installation of test data optional so it can be omitted from source disitributions by default.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2503 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2495M'
+__svn_version__ = '2501M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2495
+Revision: 2501
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2495
-Last Changed Date: 2015-08-03 16:35:07 -0400 (Mon, 03 Aug 2015)
+Last Changed Rev: 2499
+Last Changed Date: 2015-08-04 08:47:45 -0400 (Tue, 04 Aug 2015)
 
 '''
