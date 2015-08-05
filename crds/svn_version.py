584b25e8a9c370ec41d5d63d39a057b46a9c8b11
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-05 18:31:29 +0000

    Bugfix for list_mappings optimizations which broke crds.sync --all.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2508 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2501M'
+__svn_version__ = '2507M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2501
+Revision: 2507
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2499
-Last Changed Date: 2015-08-04 08:47:45 -0400 (Tue, 04 Aug 2015)
+Last Changed Rev: 2507
+Last Changed Date: 2015-08-04 13:23:17 -0400 (Tue, 04 Aug 2015)
 
 '''
