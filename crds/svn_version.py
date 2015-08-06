ed29f876e6844b67677e647b259d7343c01b225b
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-06 13:26:45 +0000

    Removed ./clean from ./install for source distributions since it depends on subversion checkout.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2515 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2509M'
+__svn_version__ = '2514M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2509
+Revision: 2514
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2508
-Last Changed Date: 2015-08-05 14:31:29 -0400 (Wed, 05 Aug 2015)
+Last Changed Rev: 2513
+Last Changed Date: 2015-08-05 17:01:50 -0400 (Wed, 05 Aug 2015)
 
 '''
