09b0096a5edfbf30102d021d84e77eed5cee9518
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-07 17:37:56 +0000

    Added "NOT expressions" to selector matchers to match the negation of the original match expressions.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2519 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2517M'
+__svn_version__ = '2518M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2517
+Revision: 2518
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2515
-Last Changed Date: 2015-08-06 09:26:45 -0400 (Thu, 06 Aug 2015)
+Last Changed Rev: 2518
+Last Changed Date: 2015-08-06 13:17:13 -0400 (Thu, 06 Aug 2015)
 
 '''
