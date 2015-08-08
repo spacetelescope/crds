1a253717d0de58f306bc50691540f66b419ee9d8
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-08 12:45:22 +0000

    Modified MANIFEST.in to include TESTING readme and drop ./runtests since testing has been relegated to a development activity due to large storage requirements for test files.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2521 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2518M'
+__svn_version__ = '2520M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2518
+Revision: 2520
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2518
-Last Changed Date: 2015-08-06 13:17:13 -0400 (Thu, 06 Aug 2015)
+Last Changed Rev: 2520
+Last Changed Date: 2015-08-07 16:17:51 -0400 (Fri, 07 Aug 2015)
 
 '''
