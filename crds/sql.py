2833644b16cea7007fa57400a1b55193be69e82c
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-13 20:20:16 +0000

    Improvements to sql.py tool.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2534 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/sql.py
+++ crds/sql.py
@@ -3,6 +3,8 @@ the CRDS catalog database.  It is a wrapper around a sqlite3 database download s
 sqlite3 command line program which can be used to query  it.
 """
 
+# ===================================================================
+
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
@@ -10,41 +12,59 @@ from __future__ import absolute_import
 import sys
 import os
 import os.path
-from collections import defaultdict
-from pprint import pprint as pp
 import sqlite3
 
-from crds import rmap, log, cmdline, utils, config, pysh
+from crds import log, cmdline, config
 from crds.client import api
-from crds import python23
 
 # ===================================================================
 
-def test():
-    """Run any doctests."""
-    import doctest, crds.sql
-    return doctest.testmod(crds.sql)
-
 class CrdsSqlQueryScript(cmdline.Script):
-    """Command line script for querying a downloaded copy of the CRDS catalog using a SQLite3 subprocess."""
+    """Command line script for querying a downloaded copy of the CRDS catalog using sqlite3."""
 
     description = """crds.sql is a thin wrapper around sqlite3 operating on a downloaded copy of
     the crds catalog or context history.   It is capable of doing basic select queries.    Fully general
-    queries can be run under the external sqlite3 program operating on the 
+    queries can be run under the external sqlite3 program operating on the dowloaded CRDS catalog
+    sqlite3 database file.
 """
 
     epilog = """
+* Dumping the table names supported can be done as follows:
+
+% python -m crds.sql --list-tables
+crds_hst_catalog
+crds_hst_context_history
+
+* Dumping the columns of a table can be done as follows:
+
+% python -m crds.sql --list-columns
+id, name, blob, state, blacklisted, rejected, observatory, instrument, filekind, type, derived_from, sha1sum, delivery_date, activation_date, useafter_date, change_level, pedigree, reference_file_type, size, uploaded_as, creator_name, deliverer_user, deliverer_email, description, catalog_link, replaced_by_filename, comment, aperture
+
+* Executing a SQL query can be done as follows:
+
+% python -m crds.sql "select name, state, pedigree from crds_catalog"
+('h230851po_pfl.fits', 'operational', 'GROUND')
+('h230851qo_pfl.fits', 'operational', 'GROUND')
+('h230851so_pfl.fits', 'operational', 'GROUND')
+('h2310096o_pfl.fits', 'operational', 'GROUND')
+('h241320gn_ilm.fits', 'operational', 'DUMMY 04/02/1997')
+('h241320in_ilm.fits', 'operational', 'DUMMY 04/02/1997')
+('h241320jn_ilm.fits', 'operational', 'DUMMY 04/02/1997')
+('h241320ln_ilm.fits', 'operational', 'DUMMY 04/02/1997')
+...
 """
     
     def add_args(self):
         super(CrdsSqlQueryScript, self).add_args()
         self.add_argument("sql_query", nargs="*", help="sqlite3 query to run on cached database.")
         self.add_argument("-u", "--update-db", action="store_true",
-            help="Updates local cached copy of CRDS catalog and context history from CRDS server.")
+                          help="Updates local cached copy of CRDS catalog and context history from CRDS server.")
         self.add_argument("-t", "--list-tables", action="store_true",
-            help="Print out the table names contained in the database")
-        self.add_argument("-c", "--list-columns", metavar="TABLE", nargs="*",
-            help="Print out the column names contained in the specified table of the database")
+                          help="Print out the table names contained in the database")
+        self.add_argument("-c", "--list-columns", metavar="TABLE", nargs="+",
+                          help="Print out the column names contained in the specified table of the database")
+        self.add_argument("-p", "--list-database-path", action="store_true",
+                          help="Print out the path of the local sqlite3 database file,  useable by sqlite3.")
 
     def main(self):
         """Process command line parameters in to a context and list of
@@ -60,6 +80,9 @@ class CrdsSqlQueryScript(cmdline.Script):
         if self.args.list_columns:
             self.list_columns(self.args.list_columns[0])
 
+        if self.args.list_database_path:
+            print(self.sqlite_db_path)
+
         if self.args.sql_query:
             self.run_query(" ".join(self.args.sql_query))
 
@@ -73,17 +96,21 @@ class CrdsSqlQueryScript(cmdline.Script):
     def fetch_sqlite_db(self):
         """Download a SQLite version of the CRDS catalog from the server."""
         log.info("Downloading CRDS catalog database file.")
-        self.db_path = api.get_sqlite_db(self.observatory)
-        log.info("Sqlite3 database file downloaded to:", self.db_path)
+        with log.warn_on_exception("Failed updating local CRDS database"):
+            db_path = api.get_sqlite_db(self.observatory)
+            log.info("Sqlite3 database file downloaded to:", db_path)
 
     def list_tables(self):
+        """Print out the names of the database tables,  one per line."""
         for table in self.get_tables():
             print(table)
 
     def list_columns(self, table):
+        """Print out an ordered comma separated list of database table column names"""
         print(", ".join(self.get_columns(table)))
 
     def get_tables(self):
+        """Return the list of database table names."""
         connection = sqlite3.connect(self.sqlite_db_path)
         cursor = connection.cursor()
         query = 'select name from sqlite_master where type=\'table\''
@@ -93,6 +120,7 @@ class CrdsSqlQueryScript(cmdline.Script):
         return tables
 
     def get_columns(self, table):
+        """Return the list of database column names for `table`."""
         connection = sqlite3.connect(self.sqlite_db_path)
         connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
@@ -114,17 +142,23 @@ class CrdsSqlQueryScript(cmdline.Script):
         connection.close()
 
     def format_row(self, row):
-        row = self.squash_unicode(row)
+        """Return a query row formatted for output."""
+        row = squash_unicode(row)
         return row
+    
+# ===================================================================
 
-    def squash_unicode(self, row):
-        row2 = []
-        for field in row:
-            if isinstance(field, (str, unicode)):
-                row2.append(str(field))
-            else:
-                row2.append(field)
-        return tuple(row2)
+def squash_unicode(row):
+    """Convert unicode strings in row to ordinary strings."""
+    row2 = []
+    for field in row:
+        if isinstance(field, (str, unicode)):
+            row2.append(str(field))
+        else:
+            row2.append(field)
+    return tuple(row2)
         
+# ===================================================================
+
 if __name__ == "__main__":
-   sys.exit(CrdsSqlQueryScript()())
+    sys.exit(CrdsSqlQueryScript()())
