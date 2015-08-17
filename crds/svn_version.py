ce10ebb3d1c934b547d175d67a90e6ccfddecd11
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-17 15:01:53 +0000

    Added include_headers parameter to JSONRPC get_best_references_by_ids().
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2535 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,15 @@
-__svn_version__ = '2528:2531M'
+__svn_version__ = '2534M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
-Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2528
+Revision: 2534
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2527
-Last Changed Date: 2015-08-11 09:21:42 -0400 (Tue, 11 Aug 2015)
+Last Changed Rev: 2534
+Last Changed Date: 2015-08-13 16:20:16 -0400 (Thu, 13 Aug 2015)
 
 '''
