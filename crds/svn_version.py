0eb687053500b9d5c2d7a43be9c901d1f620b31d
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-18 13:27:57 +0000

    Added support for ACS BIASFILE CCDGAIN substitution for DETECTOR=HRC.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2536 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2534M'
+__svn_version__ = '2535M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2534
+Revision: 2535
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2534
-Last Changed Date: 2015-08-13 16:20:16 -0400 (Thu, 13 Aug 2015)
+Last Changed Rev: 2535
+Last Changed Date: 2015-08-17 11:01:53 -0400 (Mon, 17 Aug 2015)
 
 '''
