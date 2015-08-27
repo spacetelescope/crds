c6b2cf1e394b57dc6a8b361b798cb4965965cd3f
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-27 16:19:11 +0000

    Added author as provenance keyword.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/jwst_param_web_serv@2542 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2540M'
+__svn_version__ = '2541M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/jwst_param_web_serv
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2540
+Revision: 2541
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2540
-Last Changed Date: 2015-08-19 16:57:04 -0400 (Wed, 19 Aug 2015)
+Last Changed Rev: 2541
+Last Changed Date: 2015-08-24 10:10:39 -0400 (Mon, 24 Aug 2015)
 
 '''
