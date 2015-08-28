f92caf71b32b3b8eeca020d11daa488718fd73e7
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-28 21:00:42 +0000

    Improved error handling for crds.list --datasets used to dump parameter web service values for dataset ids and specified contexts.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/jwst_param_web_serv@2543 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2541M'
+__svn_version__ = '2542M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/jwst_param_web_serv
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2541
+Revision: 2542
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2541
-Last Changed Date: 2015-08-24 10:10:39 -0400 (Mon, 24 Aug 2015)
+Last Changed Rev: 2542
+Last Changed Date: 2015-08-27 12:19:11 -0400 (Thu, 27 Aug 2015)
 
 '''
