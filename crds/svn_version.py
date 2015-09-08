79c8d8c3de521b11ff12e5aaa32f0f07f4878da6
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-09-08 17:35:40 +0000

    Added checking exception for NOT expressions.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2565 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2560M'
+__svn_version__ = '2564M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2560
+Revision: 2564
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2560
-Last Changed Date: 2015-09-02 16:40:52 -0400 (Wed, 02 Sep 2015)
+Last Changed Rev: 2564
+Last Changed Date: 2015-09-08 13:20:01 -0400 (Tue, 08 Sep 2015)
 
 '''
