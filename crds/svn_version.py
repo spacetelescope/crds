6ee7bdc408511b026665ab3e1cba1d300e7e6a68
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-09-10 15:02:26 +0000

    Comment in crds.list.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2569 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2567M'
+__svn_version__ = '2568M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2567
+Revision: 2568
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2567
-Last Changed Date: 2015-09-08 16:22:22 -0400 (Tue, 08 Sep 2015)
+Last Changed Rev: 2568
+Last Changed Date: 2015-09-08 17:17:05 -0400 (Tue, 08 Sep 2015)
 
 '''
