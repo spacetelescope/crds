956a184dd2ad442e781c7b29ab7c5f4f7cb15236
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-09-20 11:51:38 +0000

    Added conversion parameter dict conversion to strings for get_best_references jsonrpc call for remote mode.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2582 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2578:2579M'
+__svn_version__ = '2581M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2578
+Revision: 2581
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2578
-Last Changed Date: 2015-09-15 09:51:04 -0400 (Tue, 15 Sep 2015)
+Last Changed Rev: 2581
+Last Changed Date: 2015-09-18 12:59:38 -0400 (Fri, 18 Sep 2015)
 
 '''
