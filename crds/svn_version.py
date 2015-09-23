9f1257436f64d9217573b814b25253db79b90ab3
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-09-23 14:10:14 +0000

    On schema_merge: fixed bad merge of crds/jwst/tpn.py renamed setup_test_data.py so it isn't discovered for testing. fixed output changes in test_certify.py
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/schema_merge@2600 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2596:2597M'
+__svn_version__ = '2598:2599M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/schema_merge
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2596
+Revision: 2599
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2596
-Last Changed Date: 2015-09-23 08:44:41 -0400 (Wed, 23 Sep 2015)
+Last Changed Rev: 2599
+Last Changed Date: 2015-09-23 09:33:05 -0400 (Wed, 23 Sep 2015)
 
 '''
