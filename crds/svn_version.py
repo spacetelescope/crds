9009bcd34c2311df969756320a9f187f4f32d0c7
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-09-24 00:53:22 +0000

    Fixed runtests on trunk.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2607 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2593M'
+__svn_version__ = '2606M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2593
+Revision: 2606
 Node Kind: directory
 Schedule: normal
-Last Changed Author: jmiller
-Last Changed Rev: 2588
-Last Changed Date: 2015-09-21 14:57:33 -0400 (Mon, 21 Sep 2015)
+Last Changed Author: jmiller@stsci.edu
+Last Changed Rev: 2595
+Last Changed Date: 2015-09-23 08:42:32 -0400 (Wed, 23 Sep 2015)
 
 '''
