09e3a0f9ccad6ed19ab900e744b4082e52277b3f
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-10-25 12:54:19 +0000

    Updated valid.asdf test file with more recent ASDF file.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2667 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2665M'
+__svn_version__ = '2666M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2665
+Revision: 2666
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2665
-Last Changed Date: 2015-10-23 09:46:54 -0400 (Fri, 23 Oct 2015)
+Last Changed Rev: 2666
+Last Changed Date: 2015-10-24 11:46:30 -0400 (Sat, 24 Oct 2015)
 
 '''
