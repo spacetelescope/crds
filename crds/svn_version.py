5d4c24b35dd84375edc488cf2ac0ff6e70dcba8e
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-10-26 15:23:54 +0000

    Added spec nircam_distortion.rmap,  removed original placeholder .spec file.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2672 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2666:2669M'
+__svn_version__ = '2671M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2666
+Revision: 2671
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2666
-Last Changed Date: 2015-10-24 11:46:30 -0400 (Sat, 24 Oct 2015)
+Last Changed Rev: 2671
+Last Changed Date: 2015-10-25 10:42:40 -0400 (Sun, 25 Oct 2015)
 
 '''
