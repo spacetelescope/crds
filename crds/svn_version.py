99474259932d0462f8a73fed42ca9b38f6f6164f
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-10-27 21:27:00 +0000

    Added CRDS_ALLOW_SCHEMA_VIOLATIONS to run crds.bestrefs on bad datasets.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2678 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2671:2674M'
+__svn_version__ = '2676:2677M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2671
+Revision: 2676
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2671
-Last Changed Date: 2015-10-25 10:42:40 -0400 (Sun, 25 Oct 2015)
+Last Changed Rev: 2676
+Last Changed Date: 2015-10-26 17:28:51 -0400 (Mon, 26 Oct 2015)
 
 '''
