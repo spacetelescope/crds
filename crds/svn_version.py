34bf9ece5dbda1299fd02ad57e453153609a4e14
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-10-29 21:39:04 +0000

    Modified HST file locator to depend only on instrument content of name by creating instrument_from_refname().  This avoids inability to locate references with CDBS-style names of undefined types.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2684 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,16 @@
-__svn_version__ = '2676:2677M'
+__svn_version__ = '2683M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS.opus_2015.2
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2676
+Revision: 2683
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2676
-Last Changed Date: 2015-10-26 17:28:51 -0400 (Mon, 26 Oct 2015)
+Last Changed Rev: 2683
+Last Changed Date: 2015-10-28 13:38:07 -0400 (Wed, 28 Oct 2015)
 
 '''
