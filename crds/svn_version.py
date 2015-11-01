11860045863df424e1456ed2ca20a5bcdb26dfa8
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-11-01 19:11:49 +0000

    Improvements to certify validators for pedigree and time formats. Made instrument and telescope parameters optional in JWST all_all.tpn. Added existing path exclusion to config.relocate_reference()
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2697 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2692:2693M'
+__svn_version__ = '2695:2696M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2692
+Revision: 2695
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2692
-Last Changed Date: 2015-10-31 10:50:25 -0400 (Sat, 31 Oct 2015)
+Last Changed Rev: 2695
+Last Changed Date: 2015-11-01 10:02:15 -0500 (Sun, 01 Nov 2015)
 
 '''
