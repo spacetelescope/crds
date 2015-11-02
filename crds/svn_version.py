cc9df98587089b2ed880f886d79ac15167a3e2d0
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-11-02 22:07:23 +0000

    Reverted JWST provenance capture for .json and .yaml to something more FITS-like.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2708 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2706M'
+__svn_version__ = '2707M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2706
+Revision: 2707
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2706
-Last Changed Date: 2015-11-02 14:25:07 -0500 (Mon, 02 Nov 2015)
+Last Changed Rev: 2707
+Last Changed Date: 2015-11-02 15:46:57 -0500 (Mon, 02 Nov 2015)
 
 '''
