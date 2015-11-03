183077569750ae7b732746ef0ead27823b9c5766
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-11-03 19:28:34 +0000

    Moved selector validation DEBUG messages to verbosity=60 to avoid cluttering bestrefs debug output.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2710 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2707M'
+__svn_version__ = '2709M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2707
+Revision: 2709
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2707
-Last Changed Date: 2015-11-02 15:46:57 -0500 (Mon, 02 Nov 2015)
+Last Changed Rev: 2708
+Last Changed Date: 2015-11-02 17:07:23 -0500 (Mon, 02 Nov 2015)
 
 '''
