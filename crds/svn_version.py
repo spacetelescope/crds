b695fac5a662a2ca2a907f66f3755430ae728b01
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-11-04 20:28:32 +0000

    Fixed test_certify unit test case for revised warnings trapping.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2718 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2712:2713M'
+__svn_version__ = '2715M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2712
+Revision: 2715
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2712
-Last Changed Date: 2015-11-04 11:22:40 -0500 (Wed, 04 Nov 2015)
+Last Changed Rev: 2715
+Last Changed Date: 2015-11-04 13:13:22 -0500 (Wed, 04 Nov 2015)
 
 '''
