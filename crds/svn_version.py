edc54b7d0500fe37a638d32eff429edfb73bb5b3
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-11-04 15:13:53 +0000

    Updated name time ordering for PR# 81379.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2711 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2709M'
+__svn_version__ = '2710M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2709
+Revision: 2710
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2708
-Last Changed Date: 2015-11-02 17:07:23 -0500 (Mon, 02 Nov 2015)
+Last Changed Rev: 2710
+Last Changed Date: 2015-11-03 14:28:34 -0500 (Tue, 03 Nov 2015)
 
 '''
