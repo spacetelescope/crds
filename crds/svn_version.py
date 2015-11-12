e1772c5d46833ac92dd13130b25e5707aaf115e3
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-11-12 18:55:23 +0000

    Added code to check conditioned and unconditioned values for membership in valid_list when certifying rmaps.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2729 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2726M'
+__svn_version__ = '2727M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2726
+Revision: 2727
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2726
-Last Changed Date: 2015-11-10 10:33:14 -0500 (Tue, 10 Nov 2015)
+Last Changed Rev: 2727
+Last Changed Date: 2015-11-10 13:55:36 -0500 (Tue, 10 Nov 2015)
 
 '''
