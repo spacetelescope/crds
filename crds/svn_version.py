e3eb23ff821f9cb2a3eb16dfbb1d4241ae73113f
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-11-14 12:33:41 +0000

    Modified FGS .tpn's to make READPATT optional.   With the currrent design only the most fundamental parameters can be declared as *required* at the instrument level.  At the instrument level currrently in common use,  parameters are screened for correct values if they are supplied, but otherwise must be considered optional since not all types match on them and their references may not supply them.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2734 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2727M'
+__svn_version__ = '2733M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2727
+Revision: 2733
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2727
-Last Changed Date: 2015-11-10 13:55:36 -0500 (Tue, 10 Nov 2015)
+Last Changed Rev: 2733
+Last Changed Date: 2015-11-13 13:27:20 -0500 (Fri, 13 Nov 2015)
 
 '''
