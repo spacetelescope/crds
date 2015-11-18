8204031a3797c18a11067303e247738db0e154d5
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-11-18 16:01:23 +0000

    Modified WFC3 BIASFILE APERTURE rule for UVIS2-C1K1C-CTE, UVIS2-C512C-CTE
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2739 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2733M'
+__svn_version__ = '2736:2737M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2733
+Revision: 2736
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2733
-Last Changed Date: 2015-11-13 13:27:20 -0500 (Fri, 13 Nov 2015)
+Last Changed Rev: 2736
+Last Changed Date: 2015-11-16 16:32:15 -0500 (Mon, 16 Nov 2015)
 
 '''
