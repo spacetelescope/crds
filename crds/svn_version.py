921b6ddb432e9f900b3fb247b889caf3d1f90073
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-11-20 14:41:28 +0000

    Heavily re-worked crds.uniqname, 2nd draft dealing with FITS checksums and output path. Modified naming.generate_unique_name() params to optinally include observatory.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2745 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2736:2737M'
+__svn_version__ = '2743M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2736
+Revision: 2743
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2736
-Last Changed Date: 2015-11-16 16:32:15 -0500 (Mon, 16 Nov 2015)
+Last Changed Rev: 2741
+Last Changed Date: 2015-11-18 18:05:47 -0500 (Wed, 18 Nov 2015)
 
 '''
