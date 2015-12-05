aac9633541eedacc9477302c39e3f2495f2bf0c0
jmiller <jmiller@stsci.edu>
2015-12-05 23:16:47 +0000

    Added ROOTNAME and HISTORY to crds.uniqname.  Modified switches some.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2751 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/uniqname.py
+++ crds/uniqname.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import
 
 import os.path
 import sys
+import datetime
 
 from astropy.io import fits
 
@@ -32,13 +33,13 @@ class UniqnameScript(cmdline.Script):
         self.add_argument('--dry-run', action='store_true',
                           help='Print how a file would be renamed without modifying it.')
         self.add_argument('-a', '--add-checksum', action='store_true',
-                           help='Add FITS checksum.  Without, checksums *removed* if header modified.')
-        self.add_argument('-f', '--set-filename-keyword', action='store_true',
-                          help='When renaming, set the FILENAME keyword of the file to the generated name.')
+                          help='Add FITS checksum.  Without, checksums *removed* if header modified.')
+        self.add_argument('-f', '--add-filename-keywords', action='store_true',
+                          help='When renaming, add FILENAME, ROOTNAME, HISTORY keywords for the generated name.')
         self.add_argument('-e', '--verify-file', action='store_true', 
                           help='Verify FITS compliance and any checksums before changing each file.')
         self.add_argument('-s', '--standard', action='store_true', 
-                          help='Same as --set-filename-keyword --verify-file,  does not add checksums (add -a).')
+                          help='Same as --add-filename-keywords --verify-file,  does not add checksums (add -a).')
         self.add_argument('-r', '--remove-original', action='store_true',
                           help='After renaming,  remove the orginal file.')
         self.add_argument('-o', '--output-path',
@@ -52,10 +53,8 @@ class UniqnameScript(cmdline.Script):
     def main(self):
         """Generate names corrsponding to files listed on the command line."""
         if self.args.standard:
-            self.args.set_filename_keyword = True
+            self.args.add_filename_keywords = True
             self.args.verify_file = True
-            self.args.remove_original = True
-
         for filename in self.files:
             assert config.is_reference(filename), \
                 "File " + repr(filename) + " does not appear to be a reference file.  Only references can be renamed."
@@ -73,15 +72,24 @@ class UniqnameScript(cmdline.Script):
         if self.args.verify_file:
             hdus.verify("fix+warn")
         basename = os.path.basename(uniqname)
-        if self.args.set_filename_keyword:
+        if self.args.add_filename_keywords:
+            now = datetime.datetime.now()
             hdus[0].header["FILENAME"] = basename
+            hdus[0].header["ROOTNAME"] = os.path.splitext(basename)[0].upper()
+            hdus[0].header["HISTORY"] = "{0} renamed to {1} on {2} {3} {4}".format(
+                os.path.basename(filename), basename, MONTHS[now.month - 1], now.day, now.year)
         if self.args.output_path:
             uniqname = os.path.join(self.args.outpath, basename)
         log.info("Rewriting", self.format_file(filename), "-->", self.format_file(uniqname))
         hdus.writeto(uniqname, output_verify="fix+warn", checksum=self.args.add_checksum)
 
     def format_file(self, filename):
+        """Print absolute path or basename of `filename` depending on command line --brief"""
         return repr(os.path.basename(filename) if self.args.brief else filename)
-        
+
+MONTHS = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+]
+    
 if __name__ == "__main__":
     sys.exit(UniqnameScript()())
