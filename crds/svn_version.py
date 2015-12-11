e0db6dbf7590881e899782ddfec7b152de3d927d
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-12-11 18:53:28 +0000

    Updated baseline wfc3 drkcfile spec to document initial parkey selection from ticket #364.  Added CCDGAIN to wfc3 dkc .tpn files.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2758 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,16 @@
-__svn_version__ = '2750:2752M'
+__svn_version__ = '2756M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
-Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2750
+Revision: 2756
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2750
-Last Changed Date: 2015-11-30 12:27:49 -0500 (Mon, 30 Nov 2015)
+Last Changed Rev: 2756
+Last Changed Date: 2015-12-07 16:56:48 -0500 (Mon, 07 Dec 2015)
 
 '''
