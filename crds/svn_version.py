b20f9840d6a6a9fa559b0d8f6f07456e5802abc4
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-12-17 16:32:58 +0000

    Reverted r2750,  complex fix for cmdline.Script backstop error handler.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2762 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2759M'
+__svn_version__ = '2743:2748M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2759
+Revision: 2743
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2759
-Last Changed Date: 2015-12-14 10:11:58 -0500 (Mon, 14 Dec 2015)
+Last Changed Rev: 2741
+Last Changed Date: 2015-11-18 18:05:47 -0500 (Wed, 18 Nov 2015)
 
 '''
