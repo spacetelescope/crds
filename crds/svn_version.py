74ed3c16dd55c83c22286712c335489a090613bb
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-12-17 21:10:57 +0000

    Saved svn info on opus_2015_3dms branch.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015_3dms@2768 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2761:2762M'
+__svn_version__ = '2767M'
 
 __full_svn_info__ = '''
 Path: .
-URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015_3dms
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2761
+Revision: 2767
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2761
-Last Changed Date: 2015-12-16 13:02:07 -0500 (Wed, 16 Dec 2015)
+Last Changed Rev: 2765
+Last Changed Date: 2015-12-17 16:07:57 -0500 (Thu, 17 Dec 2015)
 
 '''
