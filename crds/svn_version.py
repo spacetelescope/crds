e7bd679cd134cb24b9f65f034df7e38ffd395015
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-12-21 13:49:16 +0000

    Added "return log.errors() to query_affected main() to give program exit status.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2770 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2761:2762M'
+__svn_version__ = '2768:2769M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2761
+Revision: 2768
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2761
-Last Changed Date: 2015-12-16 13:02:07 -0500 (Wed, 16 Dec 2015)
+Last Changed Rev: 2764
+Last Changed Date: 2015-12-17 11:57:57 -0500 (Thu, 17 Dec 2015)
 
 '''
