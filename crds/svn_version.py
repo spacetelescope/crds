8db4b1591ff9099712b759befd44e5744673e9e4
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-12-23 02:25:15 +0000

    Updated regex for CDBS references to accept digits as part of suffix as-in "d2i".
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2774 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2768:2769M'
+__svn_version__ = '2772M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2768
+Revision: 2772
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2764
-Last Changed Date: 2015-12-17 11:57:57 -0500 (Thu, 17 Dec 2015)
+Last Changed Rev: 2772
+Last Changed Date: 2015-12-21 13:46:19 -0500 (Mon, 21 Dec 2015)
 
 '''
