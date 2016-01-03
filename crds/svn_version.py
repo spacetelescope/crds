75688765fb88487927349de4c589b8c42a7a14ff
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2016-01-03 14:29:41 +0000

    Added revised new_type_form.rst to docs.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2778 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2772M'
+__svn_version__ = '2776M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2772
+Revision: 2776
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2772
-Last Changed Date: 2015-12-21 13:46:19 -0500 (Mon, 21 Dec 2015)
+Last Changed Rev: 2774
+Last Changed Date: 2015-12-22 21:25:15 -0500 (Tue, 22 Dec 2015)
 
 '''
