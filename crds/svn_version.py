e60a071483d21fa3a64cea299f28e24b17c88077
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2016-01-13 18:48:39 +0000

    Changes to crds.uniqname command line program to support integration with the web servers with automatic preservation of FITS checksums.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2779 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2776M'
+__svn_version__ = '2778M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2776
+Revision: 2778
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2774
-Last Changed Date: 2015-12-22 21:25:15 -0500 (Tue, 22 Dec 2015)
+Last Changed Rev: 2778
+Last Changed Date: 2016-01-03 09:29:41 -0500 (Sun, 03 Jan 2016)
 
 '''
