21fbb399dd104dbf00aaf0747c3784e81c22db14
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2016-01-14 17:31:36 +0000

    Added "probable file truncation" exception handling to crds.uniqname as well as general mapping of exceptions to CrdsError.   Added uniqname file removal on exception.    Motivated by handling web submission of truncated files and doing appropriate cleanup.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2785 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,15 @@
-__svn_version__ = '2783M'
+__svn_version__ = '2784M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
-Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2783
+Revision: 2784
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2783
-Last Changed Date: 2016-01-13 15:55:25 -0500 (Wed, 13 Jan 2016)
+Last Changed Rev: 2784
+Last Changed Date: 2016-01-13 17:03:01 -0500 (Wed, 13 Jan 2016)
 
 '''
