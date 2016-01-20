e51b65fbcc73e80108f9fefac88959d83bf9b6e4
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2016-01-20 15:06:00 +0000

    Updated version on opus_2015_3dms for push to pypi.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2792 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,17 @@
-__svn_version__ = '2784M'
+__svn_version__ = '2785:2791M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS-opus-2015.3
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2784
+Revision: 2785
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2784
-Last Changed Date: 2016-01-13 17:03:01 -0500 (Wed, 13 Jan 2016)
+Last Changed Rev: 2785
+Last Changed Date: 2016-01-14 12:31:36 -0500 (Thu, 14 Jan 2016)
 
 '''
