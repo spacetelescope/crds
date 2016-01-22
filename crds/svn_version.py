2737664e9ccfac52ef080342ed0ea6466a6e2eb6
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2016-01-22 01:56:43 +0000

    Reversed accidental changes to opus_2015_3dms branch.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015_3dms@2798 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,16 @@
-__svn_version__ = '2789:2794M'
+__svn_version__ = '2767M'
 
 __full_svn_info__ = '''
 Path: .
 Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015_3dms
-Relative URL: ^/branches/opus_2015_3dms
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2793
+Revision: 2767
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2793
-Last Changed Date: 2016-01-20 10:12:21 -0500 (Wed, 20 Jan 2016)
+Last Changed Rev: 2765
+Last Changed Date: 2015-12-17 16:07:57 -0500 (Thu, 17 Dec 2015)
 
 '''
