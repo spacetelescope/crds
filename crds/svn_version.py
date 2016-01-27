32eb0be8a03bcfe414630cb29b2dfa33842d20fc
jmiller <jmiller@stsci.edu>
2016-01-27 03:37:09 +0000

    Start of set_parkey function for refactoring tool.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2801 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,17 @@
-__svn_version__ = '2785:2791M'
+__svn_version__ = '2800M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS-opus-2015.3
+Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2785
+Revision: 2800
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2785
-Last Changed Date: 2016-01-14 12:31:36 -0500 (Thu, 14 Jan 2016)
+Last Changed Rev: 2800
+Last Changed Date: 2016-01-22 13:27:47 -0500 (Fri, 22 Jan 2016)
 
 '''
