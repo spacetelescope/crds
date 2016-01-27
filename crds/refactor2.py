9c11a53d7dc099b82a901b3e11de43a679f4c99a
jmiller <jmiller@stsci.edu>
2016-01-27 03:37:09 +0000

    Start of set_parkey function for refactoring tool.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2801 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/refactor2.py
+++ crds/refactor2.py
@@ -129,29 +129,20 @@ def rmap_check_modifications(old_rmap, new_rmap, old_ref, new_ref, expected=("ad
 
 # ============================================================================
 
-def del_rmap_parameter(context, instruments, types, parameter_name, modify_inplace=False):
+def del_rmap_parameter(rmapping, parameter, modify_inplace, *args, **keys):
     """Delete `parameter_name` from the parkey item of the `types` of the specified
     `instruments` in `context`.
     """
-    pmap = rmap.get_cached_mapping(context)
-    for instr in instruments:
-        with log.error_on_exception("Failed loading imap for", repr(instr), "from", repr(context)):
-            imap = pmap.get_imap(instr)
-            for filekind in types:
-                with log.error_on_exception("Failed loading rmap for", repr(filekind), "from", 
-                                            repr(imap.basename), "of", repr(context)):
-                    r = imap.get_rmap(filekind).copy()
-                    log.info("Deleting parameter", repr(parameter_name), "from",repr(r.basename))
-                    parkey = r.parkey
-                    i, j = get_parameter_index(parkey, parameter_name)
-                    
-                    del_parkey = parkey[:i] +  ((parkey[i][:j] + parkey[i][j+1:]),)  + parkey[i+1:]
-                    log.verbose("Replacing", repr(parkey), "with", repr(del_parkey), "in", repr(r.basename))
-                    new_filename  = r.filename if modify_inplace else os.path.join(".", r.basename)
-                    r.header["parkey"] = del_parkey
-                    # set_header_value(r.filename, new_filename, "parkey", del_parkey)
-                    r.selector.delete_match_param(parameter_name)
-                    r.write(new_filename)
+    log.info("Deleting parameter", repr(parameter), "from",repr(rmapping.basename))
+    parkey = rmapping.parkey
+    i, j = get_parameter_index(parkey, parameter)
+    del_parkey = parkey[:i] +  ((parkey[i][:j] + parkey[i][j+1:]),)  + parkey[i+1:]
+    log.verbose("Replacing", repr(parkey), "with", repr(del_parkey), "in", repr(rmapping.basename))
+    new_filename  = rmapping.filename if modify_inplace else os.path.join(".", rmapping.basename)
+    rmapping.header["parkey"] = del_parkey
+    # set_header_value(r.filename, new_filename, "parkey", del_parkey)
+    rmapping.selector.delete_match_param(parameter)
+    rmapping.write(new_filename)
                 
 def get_parameter_index(parkey, parameter_name):
     """Return the 2D parkey index for `parameter_name`:  (which_sub_tuple,  index_in_subtuple)."""
@@ -159,8 +150,23 @@ def get_parameter_index(parkey, parameter_name):
         for j, par in enumerate(pars):
             if par.lower() == parameter_name.lower():
                 return i, j
-    raise crexc.CrdsError("Can't find index for", repr(parameter_name), "in parkey", repr(parkey))
+    raise crexc.CrdsError("Can't find index for", repr(parameter_name), "in parkey", repr(parkey))    
     
+# ============================================================================
+
+def set_rmap_parkey(rmapping, parkey, modify_inplace, *args, **keys):
+    """Delete `parameter_name` from the parkey item of the `types` of the specified
+    `instruments` in `context`.
+    """
+    new_filename  = rmapping.filename if modify_inplace else os.path.join(".", rmapping.basename)
+    log.info("Setting parkey, removing all references from", rmapping.basename)
+    references = rmapping.reference_names()
+    rmapping = rmap_delete_references(rmapping.filename, new_filename, references)
+    log.info("Setting parkey", repr(parkey), "in",repr(rmapping.basename))
+    rmapping.header["parkey"] = eval(parkey)
+    rmapping.write(new_filename)
+    rmapping = rmap_insert_references(new_filename, new_filename, references)
+
 # ============================================================================
 
 class RefactorScript(cmdline.Script):
@@ -174,7 +180,8 @@ class RefactorScript(cmdline.Script):
     """
     
     def add_args(self):
-        self.add_argument("command", choices=("insert_reference", "delete_reference", "set_header", "del_header", "delete_parameter"),
+        self.add_argument("command", choices=("insert_reference", "delete_reference", "set_header", 
+                                              "del_header", "delete_parameter", "set_parkey"),
             help="Name of refactoring command to perform.")
         self.add_argument('--old-rmap', type=cmdline.reference_mapping, default=None,
             help="Reference mapping to modify by inserting references.")
@@ -186,6 +193,8 @@ class RefactorScript(cmdline.Script):
             help="Source context from which to retrieve affected mappings.")
         self.add_argument('--instruments', type=str, nargs="*",
             help="Instruments to which to apply this operation.")
+        self.add_argument('--parkey', type=str, default=None,
+            help="New parkey value for set_parkey.")
         self.add_argument('--types', type=str, nargs="*",
             help="Reference types to which to apply this operation.")
         self.add_argument('--parameter-name', type=str, default=None,
@@ -205,12 +214,46 @@ class RefactorScript(cmdline.Script):
             elif self.args.command == "del_header":
                 del_header_value(self.args.old_rmap, self.args.new_rmap, self.args.references[0])
             elif self.args.command == "delete_parameter":
-                source_context = self.resolve_context(self.args.source_context)
-                del_rmap_parameter(source_context, self.args.instruments, self.args.types, self.args.parameter_name, self.args.inplace)
+                self.del_parameter()
+            elif self.args.command == "set_parkey":
+                self.set_parkey()
             else:
                 raise ValueError("Unknown refactoring command: " + repr(self.args.command))
         return log.errors()
+    
+    def rmap_apply(self, func, *args, **keys):
+        """Apply `func()` to *args and **keys,  adding the pmap, imap, and rmap values
+        associated with the elaboration of args.source_context, args.instruments, args.types.
+        """
+        keywords = dict(keys)
+        source_context = self.resolve_context(self.args.source_context)
+        pmapping = rmap.get_cached_mapping(source_context)
+        for instr in self.args.instruments:
+            with log.error_on_exception("Failed loading imap for", repr(instr), "from", 
+                                        repr(source_context)):
+                imapping = pmapping.get_imap(instr)
+                for filekind in self.args.types:
+                    with log.error_on_exception("Failed loading rmap for", repr(filekind), "from", 
+                                                repr(imapping.basename), "of", 
+                                                repr(source_context)):
+                        rmapping = imapping.get_rmap(filekind).copy()
+                        keywords.update(locals())
+                        func(*args, **keywords)
+                        
+    def del_parameter(self):
+        """Delete args.parameter_name from the parkey of each rmapping in the rmap
+        elaboration of the command line parameters.
+        """
+        self.rmap_apply(del_rmap_parameter, parameter=self.args.parameter_name, 
+                        modify_inplace=self.args.inplace)
 
+        
+    def set_parkey(self):
+        """Change the parkey in rmaps elaborated from command line and automatically 
+        delete and re-insert each reference to construct appropriate matching tuples.
+        """
+        self.rmap_apply(set_rmap_parkey, parkey=self.args.parkey, modify_inplace=self.args.inplace)
+        
 if __name__ == "__main__":
     sys.exit(RefactorScript()())
 
