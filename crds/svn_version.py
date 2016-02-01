f460c4189c41f84e5e3c69e9b39ac3126f9d3470
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2016-02-01 00:19:03 +0000

    Refactored refactor2.py
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2806 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,17 @@
-__svn_version__ = ''
+__svn_version__ = '2801:2805M'
 
 __full_svn_info__ = '''
+Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
+URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+Relative URL: ^/trunk
+Repository Root: https://aeon.stsci.edu/ssb/svn/crds
+Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
+Revision: 2801
+Node Kind: directory
+Schedule: normal
+Last Changed Author: jmiller
+Last Changed Rev: 2801
+Last Changed Date: 2016-01-26 22:37:09 -0500 (Tue, 26 Jan 2016)
+
 '''
