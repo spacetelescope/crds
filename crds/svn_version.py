9a503cdbbe803e914c4fdd1c0a51e537044ee2a9
jmiller <jmiller@stsci.edu>
2016-02-06 14:55:04 +0000

    Added HISTORY concatenation to data_file.py to support web capture of history.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2817 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,17 @@
-__svn_version__ = '2813:2815M'
+__svn_version__ = '2816M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
+Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2813
+Revision: 2816
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2813
-Last Changed Date: 2016-02-04 11:01:22 -0500 (Thu, 04 Feb 2016)
+Last Changed Rev: 2816
+Last Changed Date: 2016-02-05 16:52:24 -0500 (Fri, 05 Feb 2016)
 
 '''
