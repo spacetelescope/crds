2ffd77b282591b1a2d7f4837a93af2b7578163c7
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2016-02-08 19:37:32 +0000

    Work on HISTORY for data_model, ASDF, and GEIS.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2819 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,17 +1,15 @@
-__svn_version__ = '2816:2817M'
+__svn_version__ = '2818M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /home/jmiller/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
-Relative URL: ^/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2816
+Revision: 2818
 Node Kind: directory
 Schedule: normal
-Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2816
-Last Changed Date: 2016-02-05 16:52:24 -0500 (Fri, 05 Feb 2016)
+Last Changed Author: jmiller
+Last Changed Rev: 2818
+Last Changed Date: 2016-02-06 10:55:58 -0500 (Sat, 06 Feb 2016)
 
 '''
