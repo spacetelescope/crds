dac485070ff8f0e8697025006c9decbe71644fd1
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2016-02-20 19:14:21 +0000

    Fixed crds.diff handling of transition from filename to nested UseAfter needed for JWST UseAfter rmap updates.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2840 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2835:2838M'
+__svn_version__ = '2839M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2835
+Revision: 2839
 Node Kind: directory
 Schedule: normal
-Last Changed Author: rij
-Last Changed Rev: 2835
-Last Changed Date: 2016-02-19 14:24:32 -0500 (Fri, 19 Feb 2016)
+Last Changed Author: jmiller@stsci.edu
+Last Changed Rev: 2839
+Last Changed Date: 2016-02-20 12:29:42 -0500 (Sat, 20 Feb 2016)
 
 '''
