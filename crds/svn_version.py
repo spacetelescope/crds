de4233db463f42909a5651fc0b9242884cd8f560
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2016-02-20 15:57:46 +0000

    UseAfter substitutions bug-fix in selectors.py,  Selection class constructor.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2837 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2826M'
+__svn_version__ = '2835:2836M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2826
+Revision: 2835
 Node Kind: directory
 Schedule: normal
-Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2825
-Last Changed Date: 2016-02-15 16:24:09 -0500 (Mon, 15 Feb 2016)
+Last Changed Author: rij
+Last Changed Rev: 2835
+Last Changed Date: 2016-02-19 14:24:32 -0500 (Fri, 19 Feb 2016)
 
 '''
