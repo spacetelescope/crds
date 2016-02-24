68d880f0ed0c602d4c2232a2a3706f85bb5b8175
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2016-02-24 18:22:24 +0000

    Added --undefined-differences-matter and na-differences-matter to affected-datasets bundle switch in crds.bestrefs which re-tunes those parameter interpretations from CDBS regression testing to reprocessing.   In regression testing the difference from CDBS UNDEFINED to CRDS N/A was ignorable,  but now it corresponds to a new type result.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2847 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2840:2841M'
+__svn_version__ = '2846M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2840
+Revision: 2846
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2840
-Last Changed Date: 2016-02-20 14:14:21 -0500 (Sat, 20 Feb 2016)
+Last Changed Rev: 2846
+Last Changed Date: 2016-02-23 15:20:00 -0500 (Tue, 23 Feb 2016)
 
 '''
