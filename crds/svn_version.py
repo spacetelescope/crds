49ab91afc21286a9fa8b0b613f13e6f716adc201
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2016-02-25 14:20:56 +0000

    Fixed diff.diff_replace_old_new,  must run on quoted diff message as originally done.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2851 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2846:2847M'
+__svn_version__ = '2850M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2846
+Revision: 2850
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2846
-Last Changed Date: 2016-02-23 15:20:00 -0500 (Tue, 23 Feb 2016)
+Last Changed Rev: 2848
+Last Changed Date: 2016-02-24 16:47:38 -0500 (Wed, 24 Feb 2016)
 
 '''
