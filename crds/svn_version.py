18427002b3e1875db74b431436da8f697f274412
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2016-03-05 22:01:06 +0000

    Work on USEAFTER refactoring tool, added optional diffing and certification to refactored rmaps. Added print_status parameter to cmdline.Script() to suppress embedded status.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/trunk@2867 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2856:2857M'
+__svn_version__ = '2861:2864M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2856
+Revision: 2861
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2856
-Last Changed Date: 2016-03-02 16:58:47 -0500 (Wed, 02 Mar 2016)
+Last Changed Rev: 2859
+Last Changed Date: 2016-03-03 17:18:36 -0500 (Thu, 03 Mar 2016)
 
 '''
