f9feb9b6770dce4dac04ddb3d82504e10033ec46
Todd Miller <jmiller@stsci.edu>
2016-03-13 14:28:31 -0400

    Removed CRDS_FAKE_HEADER_KEYWORDS. Modified data_file data model history treatment to join history lines with "\n". Adjusted "import asdf" exception trap from ImportError to Exception due to alternate failure modes.

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,4 +1,4 @@
-__svn_version__ = '2861:2864M'
+__svn_version__ = '2873M'
 
 __full_svn_info__ = '''
 Path: .
@@ -6,11 +6,11 @@ Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2861
+Revision: 2873
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2859
-Last Changed Date: 2016-03-03 17:18:36 -0500 (Thu, 03 Mar 2016)
+Last Changed Rev: 2873
+Last Changed Date: 2016-03-11 13:07:26 -0500 (Fri, 11 Mar 2016)
 
 '''
