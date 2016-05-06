e3242b6035c5b28d64ac36b290482424cf1b4f5f
Todd Miller <jmiller@stsci.edu>
2016-05-06 11:12:59 -0400

    Overhauled username/password handling to simplify and defer calling fallback function when not defined in environment or ini file. Cleaned up web.py considerably,  including many superficial pylint issues.

--- crds/web.py
+++ crds/web.py
@@ -9,67 +9,95 @@ import socket
 from lxml import html
 import requests
 
-import crds
 from crds import config, log, utils, pysh
 from crds.client import api
-from crds.python23 import *
+
+# ================================================================================
+
+def combine_dicts(*post_dicts, **post_vars):
+    """Combine positional parameters (dictionaries) and individual
+    variables specified by keyword into a single parameter dict.
+    """
+    combined_vars = dict()
+    for pars in post_dicts:
+        combined_vars.update(pars)
+    combined_vars.update(post_vars)
+    return combined_vars
+
+def divider(name="", char="-", repeat=75, func=log.verbose):
+    """Create a log divider line consisting of `char` repeated `n` times
+    possibly with `name` injected into the center of the divider.
+    Output it as a string to logging function `func`.
+    """
+    if name:
+        repeat2 = (repeat - len(name) - 2) // 2
+        func(char*repeat2, name, char*repeat2)
+    else:
+        func(char*repeat)
+
+def start_stats(self, filepaths, name=None, char="="):
+    """Helper method to initialize stats keeping for ingest."""
+    total_bytes = 0
+    for name in filepaths:
+        total_bytes += os.stat(name).st_size
+    stats = utils.TimingStats(output=log.verbose)
+    stats.start()
+    divider(name=name, char=char, func=log.info)
+    log.info("Copying", len(filepaths), "file(s) totalling", total_bytes, "bytes")
+    divider()
+    return stats
 
 # ================================================================================
 
 class CrdsDjangoConnection(object):
-    def __init__(self, locked_instrument="none", username=None, password=None, 
-                 observatory=None, ingest_destination=None):
+    """This class provides the basis for authenticated GETS and POSTS to a CRDS server."""
+    def __init__(self, username=None, password=None, locked_instrument=None, observatory=None, ingest_destination=None):
         self.locked_instrument = locked_instrument
-        self.username = username or config.USERNAME.get()
-        self.password = password or config.PASSWORD.get()
+        self.username = config.get_username(username)
+        self.password = config.get_password(password)
         self.observatory = observatory or api.get_default_observatory()
-        self.ingest_destination = ingest_destination or api.get_submission_info(self.observatory, self.username).ingest_dir
         self.base_url = config.get_server_url(self.observatory)
+        self.ingest_destination = ingest_destination or \
+            api.get_submission_info(self.observatory, self.username).ingest_dir
         self.session = requests.session()
         self.session.headers.update({'referer': self.base_url})
+        self.jpoll_key = None
 
     def abs_url(self, relative_url):
         """Return the absolute server URL constructed from the given `relative_url`."""
         return self.base_url + relative_url
 
-    def divider(self, name="", char="-", n=75, func=log.verbose):
-        """Create a log divider line consisting of `char` repeated `n` times
-        possibly with `name` injected into the center of the divider.
-        Output it as a string to logging function `func`.
-        """
-        if name:
-            n2 = (n - len(name) - 2) // 2
-            func(char*n2, name, char*n2)
-        else:
-            func(char*n)
-
     def dump_response(self, name, response):
         """Print out verbose output related to web `response` from activity `name`."""
-        self.divider(name=name)
+        divider(name=name)
         log.verbose("headers:", response.headers)
-        self.divider()
+        divider()
         log.verbose("text:", response.text, verbosity=60)
-        self.divider()
+        divider()
         try:
             log.verbose("json:", response.json()) 
         except:
             pass
-        self.divider()
+        divider()
 
     def get(self, relative_url):
+        """Get from `relative_url` which should include any necessary encoded parameters."""
         url = self.abs_url(relative_url)
-        self.divider(name="GET")
+        divider(name="GET")
         log.verbose("GET:", url)
         response = self.session.get(url)
         self.dump_response("GET response:", response)
         return response
 
     def post(self, relative_url, *post_dicts, **post_vars):
+        """Post to `relative_url` the combination of variables defined using the combined 
+        sequence of *post_dicts overridden by **post_vars.
+        """
         url = self.abs_url(relative_url)
-        vars = self.combine_dicts(*post_dicts, **post_vars)
-        self.divider(name="POST " + url)
-        log.verbose("POST:", vars)
-        response = self.session.post(url, vars)
+        combined_vars = combine_dicts(*post_dicts, **post_vars)
+        divider(name="POST " + url)
+        log.verbose("POST:", combined_vars)
+        response = self.session.post(url, combined_vars)
         self.dump_response("POST response: ", response)
         return response
 
@@ -89,18 +117,9 @@ class CrdsDjangoConnection(object):
         response = self.post(relative_url, *post_dicts, **post_vars)
         return response
     
-    def combine_dicts(self, *post_dicts, **post_vars):
-        """Combine positional parameters (dictionaries) and individual
-        variables specified by keyword into a single parameter dict.
-        """
-        vars = dict()
-        for pars in post_dicts:
-            vars.update(pars)
-        vars.update(post_vars)
-        return vars
-
     def login(self, next="/"):
-        self.repost("/login/", 
+        self.repost(
+            "/login/", 
             username = self.username,
             password = self.password, 
             instrument = self.locked_instrument,
@@ -109,7 +128,7 @@ class CrdsDjangoConnection(object):
     
     def ingest_files(self, filepaths):
         """Copy `filepaths` into the user's ingest directory on the CRDS server."""
-        stats = self._start_stats(filepaths)
+        stats = start_stats(filepaths)
 
         destination = self.ingest_destination
 
@@ -125,23 +144,11 @@ class CrdsDjangoConnection(object):
             stats.increment("bytes", os.stat(name).st_size)
             stats.increment("files", 1)
 
-        self.divider()
+        divider()
         stats.report()
-        self.divider(char="=", func=log.info)
+        divider(char="=", func=log.info)
 
-    def _start_stats(self, filepaths):
-        """Helper method to initialize stats keeping for ingest."""
-        total_bytes = 0
-        for name in filepaths:
-            total_bytes += os.stat(name).st_size
-        stats = utils.TimingStats(output=log.verbose)
-        stats.start()
-        self.divider(name="ingest files", char="=", func=log.info)
-        log.info("Copying", len(filepaths), "file(s) totalling", total_bytes, "bytes")
-        self.divider()
-        return stats
-
-    def get_pmap_selection_parameters(self, pmap_mode="pmap_edit", pmap_name=None, **keys):
+    def get_pmap_selection_parameters(self, pmap_mode="pmap_edit", pmap_name=None):
         """Return a dictionary of parameters used so satisfy the pmap selection (Derived From Context)
         form variables on the CRDS web server.
         """
@@ -167,7 +174,7 @@ class CrdsDjangoConnection(object):
         self.ingest_files(filepaths)
         self.jpoll_open()
         pmap_pars = self.get_pmap_selection_parameters(pmap_mode, pmap_name)
-        self.repost("/certify/", pmap_pars, compare_old_reference="checked")
+        self.repost("/certify/", pmap_pars, compare_old_reference=compare_old_reference)
 
 def main(files):
     session = CrdsDjangoConnection()
