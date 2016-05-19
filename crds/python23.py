c9313bc8fdb054de6f16c31e5f35d3a8274cfa51
Todd Miller <jmiller@stsci.edu>
2016-05-19 11:33:37 -0400

    Copied python23.py from cmdline-submission to master to pick up urlopen() for client.api

--- crds/python23.py
+++ crds/python23.py
@@ -5,17 +5,27 @@ if sys.version_info >= (3,0,0):
     long = int
     string_types = (str,)
 
+    from urllib.request import urlopen
+    from html import unescape
     import configparser
+
 else:
     long = long
     string_types = (basestring,)
+
+    import HTMLParser as _parser_mod
+    from urllib2 import urlopen
+    unescape = _parser_mod.HTMLParser().unescape
 
     import ConfigParser as configparser
 
 __all__ = [
     "long",
     "string_types",
-    
+
+    "urlopen",
+    "unescape",
     "configparser",
+
     ]
 
