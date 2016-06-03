30124841c87f213c4c0891cc3393bf614189d397
Todd Miller <jmiller@stsci.edu>
2016-06-03 17:06:58 -0400

    Updated version to 7.0.0

--- crds/__init__.py
+++ crds/__init__.py
@@ -3,7 +3,7 @@ from __future__ import division
 from __future__ import absolute_import
 # The "crds" __version__ defined here should also reflect the behavior of 
 # crds.client
-__version__ = "6.0.1"   #  jwst_build6,  also change ../setup.cfg
+__version__ = "7.0.0"   #  jwst_build7,  also change ../setup.cfg
 
 # ============================================================================
 
