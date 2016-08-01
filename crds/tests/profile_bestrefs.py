2396e987a8fdf0a574f89aa73a5a76534ac81a75
Todd Miller <jmiller@stsci.edu>
2016-08-01 16:34:34 -0400

    Added profiling code for rmap pickling.  Refactored bestrefs profiling code.

--- crds/tests/profile_bestrefs.py
+++ crds/tests/profile_bestrefs.py
@@ -1,8 +1,7 @@
 """This module is used to profile getrecommendations() and bestreds.BestrefsScript()."""
 
-import cProfile, pstats, os
-
 from crds import heavy_client, bestrefs, utils, log
+from crds.tests.test_config import run_and_profile
 
 HST_HEADER = {
     'INSTRUME' : 'ACS',
@@ -28,29 +27,9 @@ JWST_HEADER = {
     "meta.exposture.readpatt" : "FAST",
     }
 
-def run_and_profile(name, case):
-    """Using `name` for a banner and divider,  execute code string `case` in the
-    global namespace,  both evaled printing result and under the profiler.
-    """
-    utils.clear_function_caches()
-    log.divider()
-    log.divider(name + " example")
-    log.divider()
-    print(eval(case, locals(), globals()))
-    utils.clear_function_caches()
-    log.divider()
-    log.divider(name + " profile")
-    log.divider()
-    cProfile.run(case, "profile.stats")
-    stats = pstats.Stats('profile.stats')
-    stats.strip_dirs()
-    stats.sort_stats('cumulative')
-    stats.print_stats(100)
-    os.remove('profile.stats')
-
 if __name__ == "__main__":
-    run_and_profile("JWST getrecommendations()", 'heavy_client.getrecommendations(JWST_HEADER, observatory="jwst", ignore_cache=False)')
-    run_and_profile("HST getrecommendations()", 'heavy_client.getrecommendations(HST_HEADER, observatory="hst", ignore_cache=False)')
-    run_and_profile("HST bestrefs file", "bestrefs.BestrefsScript('crds.bestrefs --files data/j8bt09jcq_raw.fits --log-time --stats')()")
+    run_and_profile("JWST getrecommendations()", 'heavy_client.getrecommendations(JWST_HEADER, observatory="jwst", ignore_cache=False)', globals())
+    run_and_profile("HST getrecommendations()", 'heavy_client.getrecommendations(HST_HEADER, observatory="hst", ignore_cache=False)', globals())
+    run_and_profile("HST bestrefs file", "bestrefs.BestrefsScript('crds.bestrefs --files data/j8bt09jcq_raw.fits --log-time --stats')()", globals())
 
 # run_and_profile("JWST bestrefs file", "bestrefs.BestrefsScript('crds.bestrefs --files data/ --log-time --stats')()")
