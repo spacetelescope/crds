8999834bdccd6068d134a4533f7ea11217752e76
Todd Miller <jmiller@stsci.edu>
2016-09-16 18:22:22 -0400

    Modified combined types .json loader to squash unicode to str on Python-2.7.x

--- crds/reftypes.py
+++ crds/reftypes.py
@@ -84,6 +84,7 @@ def load_specs(spec_path):
     combined_specs_path = os.path.join(spec_path, "combined_specs.json")
     if os.path.exists(combined_specs_path):
         headers = load_json_specs(combined_specs_path)
+        headers = python23.unicode_to_str(headers)
     else:
         headers = load_raw_specs(spec_path)
         with log.warn_on_exception("Failed to save type specs .json at", repr(combined_specs_path)):
