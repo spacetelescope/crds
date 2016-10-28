d78fe9c321804cd6b27fbb1787268f2c838d6322
Todd Miller <jmiller@stsci.edu>
2016-10-28 15:21:25 -0400

    Added test case generation to test_special.py

--- crds/tests/test_special.py
+++ crds/tests/test_special.py
@@ -16,6 +16,25 @@ BESTREFS = {
         "TRACETAB" : "NOT FOUND some other error",
         }
 
+BESTREFS_DEFINED = {
+        "FLATFILE": 'x5v1944hl_flat.fits',
+        }
+
+BESTREFS_NA = {
+        "BADTTAB" : "NOT FOUND n/a",
+        }
+
+BESTREFS_UNDEFINED = {
+        "BRFTAB" : "UNDEFINED",
+        }
+
+BESTREFS_OMITTED= {
+        }
+
+BESTREFS_ERROR = {
+    "TRACETAB" : "NOT FOUND some other error",
+    }
+
 """Special case tests verify behavior in various configurations:
 
 --regression
@@ -152,7 +171,7 @@ def dt_bestrefs_na_undefined_single_ctx_na_matters():
     >>> script.handle_na_and_not_found("New:", BESTREFS, "LA9K03C3Q:LA9K03C3Q", "COS", "XTRACTAB")
     (True, 'N/A')
 
-    An explicit error response from CRDS,  not OK
+    An explicit error response from CRDS, not OK
 
     >>> script.handle_na_and_not_found("New:", BESTREFS, "LA9K03C3Q:LA9K03C3Q", "COS", "TRACETAB")
     CRDS - ERROR - instrument='COS' type='TRACETAB' data='LA9K03C3Q:LA9K03C3Q' ::  New: Bestref FAILED:  some other error
@@ -161,6 +180,39 @@ def dt_bestrefs_na_undefined_single_ctx_na_matters():
     >>> test_config.cleanup(old_state)
     
     """
+
+kinds = ["DEFINED", "NA", "UNDEFINED", "OMITTED", "ERROR"]
+
+def generate_comparisons(undef_matters, na_matters):
+    for kind1 in kinds:
+        for kind2 in kinds:
+            bestrefs1 = globals()["BESTREFS_" + kind1]
+            bestrefs2 = globals()["BESTREFS_" + kind2]
+            print("""
+
+def dt_compare_bestrefs_%s_%s_%s_%s():
+    '''
+    >>> script = BestrefsScript(argv="crds.bestrefs --load-pickles data/bestrefs.special.json --new-context hst_0315.pmap %s %s")
+    >>> script.complex_init()
+    CRDS - INFO - Loading file 'data/bestrefs.special.json'
+    CRDS - INFO - Loaded 1 datasets from file 'data/bestrefs.special.json' completely replacing existing headers.
+    CRDS - INFO - No comparison context or source comparison requested.
+    True
+    >>> script.compare_bestrefs(%s, %s, %s, %s)
+   ''' """ % ("undefmatters" if undef_matters else "undefok",
+       "namatters" if na_matters else "naok",
+       kind1.lower(),
+       kind2.lower(),
+       "--undefined-differences-matter" if undef_matters else "",
+       "--na-differences-matter" if na_matters else "",
+       "COS",
+       "LA9K03C3Q:LA9K03C3Q",
+       bestrefs1, bestrefs2))
+
+def generate_all():
+    for undef_matters in [False, True]:
+        for na_matters in [False, True]:
+            generate_comparisons(undef_matters, na_matters)
 
 # ==================================================================================
 
