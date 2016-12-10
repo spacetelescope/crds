ca3869b07bd959ccb9c33e498017fcd964485d00
Todd Miller <jmiller@stsci.edu>
2016-12-10 10:05:49 -0500

    Refactored unit tests to set and preserve project context better,  fixing matches results. Added .sh env setup scripts for D-string and CRDS unit test development.

---
+++ envs/crds-tests.sh
@@ -0,0 +1,15 @@
+# This environment is used for running unit tests as stand alone modules
+# rather than under the control of runtests.
+
+export CRDS_SERVER_URL=https://hst-crds.stsci.edu
+
+export CRDS_TEST_ROOT="$HOME"
+
+export CRDS_PATH="${CRDS_TEST_ROOT}/crds-cache-default-test"
+
+export CRDS_CACHE_DEFAULT="$CRDS_PATH"
+
+export CRDS_TESTING_CACHE="${CRDS_TEST_ROOT}/crds-cache-test"
+
+export CRDS_GROUP_CACHE="$HOME/crds_cache_ops"
+
