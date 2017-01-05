166866127c3c08a734b689db9ac72fb996e059bd
Todd Miller <jmiller@stsci.edu>
2017-01-05 14:53:20 -0500

    Added crds.bestrefs package __main__ module Factored header code out of crds.bestrefs.bestrefs Pylinted, switching init of self.old_headers and self.new_headers from None to a bogus abstract class to guide pylint to better checking before the real classes can be determined.

---
+++ crds/bestrefs/__main__.py
@@ -0,0 +1,22 @@
+"""
+This module supports running crds.bestrefs from the command line using -m syntax:
+
+% python -m crds.bestrefs ...
+
+"""
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+import sys
+
+from crds.bestrefs import bestrefs
+
+def main():
+    """Construct and run the bestrefs script,  return 1 if errors occurred, 0 otherwise."""
+    errors = bestrefs.BestrefsScript()()
+    exit_status = int(errors > 0)  # no errors = 0,  errors = 1
+    return exit_status
+
+if __name__ == "__main__":
+    sys.exit(main())
