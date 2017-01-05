bc5be23c08448a217caa4a40857b16fd0036cb69
Todd Miller <jmiller@stsci.edu>
2017-01-05 16:10:49 -0500

    Created crds.certify package and migrated certify module to it.

--- crds/certify/certify.py
+++ crds/certify/certify.py
@@ -1295,11 +1295,3 @@ For more information on the checks being performed,  use --verbose or --verbosit
         return sorted(closure_files)
 
 
-def main():
-    """Construct and run the Certify script,  return 1 if errors occurred, 0 otherwise."""
-    errors = CertifyScript()()
-    exit_status = int(errors > 0)  # no errors = 0,  errors = 1
-    return exit_status
-
-if __name__ == "__main__":
-    sys.exit(CertifyScript()())
