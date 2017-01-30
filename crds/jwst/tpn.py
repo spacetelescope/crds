16de0f004e44959c9413fa44e3d40792df71dbee
Todd Miller <jmiller@stsci.edu>
2017-01-30 14:22:51 -0500

    Reverted change to tpn.py error handling because the absence of many instr_type specifc .tpn files is expected.   This is not really ideal because the ambiguity of "is a .tpn expected for this case?" is colliding with "this .tpn does not load due to syntax errors."

--- crds/jwst/tpn.py
+++ crds/jwst/tpn.py
@@ -95,10 +95,11 @@ def get_classic_tpninfos(*key):
     `filekind` from it's .tpn file.
     """
     where = os.path.join(HERE, "tpns", key[0])
-    with log.verbose_warning_on_exception(
-            "No TPN file exists for", repr(key[0]), verbosity=70):
+    try:
         return _load_tpn(where)
-    return []
+    except Exception as exc:
+        log.verbose_warning("No TPN file exists for", repr(key[0]), verbosity=70)
+        return []
 
 def get_tpninfos(*key):
     """Load the listof TPN info tuples corresponding to `key` from it's .tpn file.
