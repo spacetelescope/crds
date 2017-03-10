68fa1a290bd10905bde2a040512c500e25472e83
Todd Miller <jmiller@stsci.edu>
2017-03-10 11:32:16 -0500

    Added MIRI array constraint files, configured mask,dark,saturation. Updated subarray window checks with -1 offset:  START + SHAPE - 1. Reverted change to io.fits checksum, back to WARN to enable other cert checks.  fitsverify later classifies as ERROR as well.

--- crds/io/abstract.py
+++ crds/io/abstract.py
@@ -42,7 +42,7 @@ def hijack_warnings(func):
             warnings.showwarning = hijacked_showwarning
             warnings.filterwarnings("always", r".*", UserWarning, r".*jwst.*")
             warnings.simplefilter("always", AstropyUserWarning)
-            warnings.filterwarnings("error", r".*sum verification failed.*", AstropyUserWarning, r".*astropy.io.fits.*")
+            # warnings.filterwarnings("error", r".*sum verification failed.*", AstropyUserWarning, r".*astropy.io.fits.*")
             if not config.ALLOW_SCHEMA_VIOLATIONS:
                 warnings.filterwarnings("error", r".*is not valid in keyword.*", UserWarning, r".*jwst.*")
             # warnings.filterwarnings("ignore", r".*unclosed file.*", UserWarning, r".*crds.data_file.*")
