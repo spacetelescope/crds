f2d2c539dc59cf636606ac40f27075959201da84
Todd Miller <jmiller@stsci.edu>
2017-05-20 17:00:19 -0700

    Changes to crds.list and dataset header fetching crds.client API.
    Refactored get_dataset_headers_by_instrument() to factor out header fetching
    for lists of dataset ids instead of "all ids for instrument."
    This core function was dubbed get_dataset_headers_unlimited() and augments
    the JSONRPC primitive that can fail if too many IDs are specified.
    
    Added @-file expansion to command line dataset IDs parameter to avoid
    command line length limitations.
    
    Simplified the output of ID listing routine to omit context when
    only the default context is used.

--- crds/client/api.py
+++ crds/client/api.py
@@ -332,12 +332,20 @@ def get_required_parkeys(context):
 
 def get_dataset_headers_by_instrument(context, instrument, datasets_since=None):
     """Return { dataset_id : { header } } for `instrument`."""
-    max_ids_per_rpc = get_server_info().get("max_headers_per_rpc", 5000)
+    log.verbose("Dumping datasets for", repr(instrument))
     ids = get_dataset_ids(context, instrument, datasets_since)
+    return get_dataset_headers_unlimited(context, ids, datasets_since)
+
+def get_dataset_headers_unlimited(context, ids, datasets_since=None):
+    """Return { dataset_id : { header } } for `ids`,  potentially more
+    ids than can be serviced with a single JSONRPC request, looping 
+    internally.
+    """
+    max_ids_per_rpc = get_server_info().get("max_headers_per_rpc", 5000)
     headers = {}
     for i in range(0, len(ids), max_ids_per_rpc):
+        log.verbose("Dumping dataset headers", i , "of", len(ids), verbosity=20)
         id_slice = ids[i : i + max_ids_per_rpc]
-        log.verbose("Dumping datasets for", repr(instrument), "ids", i , "of", len(ids), verbosity=20)
         header_slice = get_dataset_headers_by_id(context, id_slice)
         headers.update(header_slice)
     return headers
