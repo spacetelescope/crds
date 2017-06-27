4fdc4a672cd4bcec129b7c38d201faa36e16ff6b
Todd Miller <jmiller@stsci.edu>
2017-06-27 15:28:14 -0400

    Added crds_cache_locking.py to wrap lockfile.

---
+++ crds/core/crds_cache_locking.py
@@ -0,0 +1,56 @@
+"""This module defines locking primitives for the CRDS cache used to prevent
+simultaneous writes.  This is principally motivated by the JWST association
+logic.
+"""
+
+# =========================================================================
+
+import contextlib
+import os.path
+
+# =========================================================================
+
+from . import log, config
+
+# =========================================================================
+
+try:
+    import lockfile
+except ImportError:
+    log.verbose_warning("Failed importing 'lockfile' package.  "
+                        "CRDS cannot support cache syncs while multiprocessing.")
+
+# =========================================================================
+
+@contextlib.contextmanager
+def get_fake_crds_lock(lockpath):    
+    """Return an empty lock context manager so that CRDS does not outright crash in
+    all cases because it attempts to support e.g. sync'ing references for multiple
+    association images concurrently.
+    """
+    try:
+        yield
+    finally:
+        pass
+
+# =========================================================================
+
+def get_cache_lock():
+    """Return a file lock context manager to guard the CRDS cache against
+    concurrent writes.
+    """
+    if config.writable_cache_or_warning("cannot sync files or create cache file lock."):
+#         config_dir = config.get_crds_cfgpath(observatory)
+#         lockpath = os.path.join(config_dir, "cache_file_lock")
+        lockpath = os.path.join(os.environ["HOME"], ".crds.cache.lock")
+        try:
+#             utils.ensure_dir_exists(lockpath)            
+            return lockfile.LockFile(lockpath)
+        except Exception as exc:
+            log.warning("Failed creating CRDS cache lock file during cache sync. "
+                        "Cannot support multiprocessing while syncing reference files. "
+                        "export CRDS_READONLY_CACHE=1 to suppress this message.")
+            log.info("Exception was:", str(exc))
+            return get_fake_crds_lock(lockpath)
+    else:
+        return get_fake_crds_lock(lockpath)
