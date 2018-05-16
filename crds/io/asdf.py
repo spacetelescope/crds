94db9af4772df5f43c7262c23e9ad8c5152c8bb2
Todd Miller <jmiller@stsci.edu>
2018-05-16 13:41:26 -0400

    Updated ASDF history reading for Jira CCD-81.   Added unit tests for 3 prior formats.   Still needs more standardization and enforcement,  addition of support for new get_history...() API in ASDF.

--- crds/io/asdf.py
+++ crds/io/asdf.py
@@ -13,7 +13,7 @@ from __future__ import absolute_import
 
 # ============================================================================
 
-from crds.core import timestamp, utils
+from crds.core import timestamp, utils, log
 
 from .abstract import AbstractFile
 
@@ -29,13 +29,17 @@ class AsdfFile(AbstractFile):
         import asdf
         with asdf.AsdfFile.open(self.filepath) as handle:
             header = self.to_simple_types(handle.tree)
-            if "history" in handle.tree:
-                histall = []
-                history = handle.tree["history"]
-                entries = history["entries"] if "entries" in history else history
-                for hist in entries:
-                    histall.append(timestamp.format_date(hist["time"]).split(".")[0] +
-                                   " :: " + hist["description"])
-                header["HISTORY"] = "\n".join(histall)
+            with log.warn_on_exception("Failed reading ASDF history"):
+                if "history" in handle.tree:
+                    histall = []
+                    history = handle.tree["history"]
+                    if "entries" in history or isinstance(history, list):
+                        entries = history["entries"] if "entries" in history else history
+                        for hist in entries:
+                            histall.append(timestamp.format_date(hist["time"]).split(".")[0] +
+                                           " :: " + hist["description"])
+                    else:
+                        histall = [history.get("description", "Unknown history format.")]
+                    header["HISTORY"] = "\n".join(histall)
         return header
 
