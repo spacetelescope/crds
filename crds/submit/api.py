193f32bd7f232e3a6ef7708ce551bb0e33e122ce
Todd Miller <jmiller@stsci.edu>
2019-03-11 14:44:51 -0400

    Added check_sha1sum module and functionality to crds.certify pkg to detect duplication of existing files. Made client.get_default_observatory() public. Added crds.submit.api module for encapsulating new programmatic API for crds.submit

---
+++ crds/submit/api.py
@@ -0,0 +1,19 @@
+"""This module is intended to present a programmatic API to the
+the CRDS submit functionality,  particularly as it pertains to the
+extended form which includes ReDCaT metadata and forward functions.
+"""
+
+def submit():
+    """
+    """
+    # login
+
+    certify_files(abs_paths, context=context, dump_provenance=True,
+                  observatory=observatory, compare_old_reference=True,
+                  run_fitsverify=True, check_rmap=True, check_sha1sums=True)
+
+    # filecopy (web post?)
+
+    # meta-post
+
+    # monitor / wait-for-complete?
