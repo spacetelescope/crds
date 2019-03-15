50099baf6b861e567fdf883d0839e70d86dc9d9d
Todd Miller <jmiller@stsci.edu>
2019-03-15 15:03:14 -0400

    Added example of calling CRDS script (command line program) programmatically. Re-factored ReDCaT script into an API script and a command line submission script.

--- crds/submit/rc_submit.py
+++ crds/submit/rc_submit.py
@@ -2,7 +2,7 @@ import yaml
 
 from .submit import ReferenceSubmissionScript
 
-class RedCatSubmissionScript(ReferenceSubmissionScript):
+class RedCatApiScript(ReferenceSubmissionScript):
     """This script extends the original CRDS command line submission
     system with extra meta-data previously captured by the submit_to_redcat
     script.
@@ -16,6 +16,23 @@ class RedCatSubmissionScript(ReferenceSubmissionScript):
         parameters.update(self._extra_redcat_parameters)
         return parameters
 
+    def get_extra_parameters(self):
+        """Return the form dictionary mapping form variables to value strings for
+        new variables being added by the streamlining project.
+        """
+        # ... get and populate required form dictionary,  careful about
+        # conflicts with baseclass.   See baseclass self.connection for
+        # doing any web i/o.
+        
+    def batch_submit_references(self):
+        """Do a web re-post to the batch submit references web page."""
+        return self._submission("/submission_form/redcat_submit/")
+
+class RedCatSubmissionScript(RedCatApiScript):
+    """This script extends the original CRDS command line submission
+    system with extra meta-data previously captured by the submit_to_redcat
+    script.
+    """
     def add_args(self):
         super(ReferenceSubmissionScript, self).add_args()
         self.add_arg("--redcat-parameters", type="str",
@@ -28,6 +45,17 @@ class RedCatSubmissionScript(ReferenceSubmissionScript):
         with open(self.args.redcat_parameters) as f:
             return yaml.load(f)
         
-    def batch_submit_references(self):
-        """Do a web re-post to the batch submit references web page."""
-        return self._submission("/submission_form/redcat_submit/")
+def submit():
+        # Deliver the files
+    script = RedcatApiScript("crds.submit --files {} "
+                             "--monitor-processing "
+                             "--wait-for-completion "
+                             "--wipe-existing-files "
+                             "--log-time "
+                             "--stats --creator '{} Team' "
+                             "--description '{}'").format(
+            submit_files, instrument, description)
+    # script._extra_redcat_parameters = {}
+    script()
+
+    # What should return value be?
