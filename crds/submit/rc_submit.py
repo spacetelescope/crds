148b6d58e5665ca3c6f9664f57788da437386228
Sean Lockwood <lockwood@stsci.edu>
2019-04-02 21:04:00 -0400

    Adding dictionary-like interaction to RedCatApiScript class.

--- crds/submit/rc_submit.py
+++ crds/submit/rc_submit.py
@@ -3,19 +3,229 @@ to the original commamnd line submission program as part of the file submission
 streamlinin project.
 """
 
+import os
 import sys
-
 import yaml
-
-from crds.core import log
-
+from crds.core import log, config
 from .submit import ReferenceSubmissionScript
+import urllib
+from functools import wraps
+from textwrap import wrap
+
+BASE_URLS = {
+    'dev': {
+        'hst':  'https://hst-crds-dev.stsci.edu/',
+        'jwst': 'https://jwst-crds-dev.stsci.edu/', },
+    'test': {
+        'hst':  'https://hst-crds-test.stsci.edu/',
+        'jwst': 'https://jwst-crds-test.stsci.edu/', },
+    #'production': {
+    #    'hst':  'https://hst-crds.stsci.edu/',
+    #    'jwst': 'https://jwst-crds.stsci.edu/', },
+    }
+
+URL_DESCRIPTION = 'submission_form/redcat_description.yml'
+
+NULL_FIELDTYPES = {
+    'BooleanField'     : bool,
+    'CharField'        : str,
+    'TypedChoiceField' : str, }
+
+HST_INSTRUMENTS  = ['acs', 'cos', 'nicmos', 'stis', 'wfc3', 'wfpc2']
+JWST_INSTRUMENTS = ['fgs', 'miri', 'nircam', 'niriss', 'nirspec', 'system']  # system?
+
+# Preserve order of YAML dicts (from https://stackoverflow.com/a/52621703):
+yaml.add_representer(dict, lambda self, data: yaml.representer.SafeRepresenter.represent_dict(self, data.items()))
+
 
 class RedCatApiScript(ReferenceSubmissionScript):
     """This script extends the original CRDS command line submission
     system with extra meta-data previously captured by the submit_to_redcat
     script.
     """
+    def __init__(self, observatory, string, *args, **keys):
+        observatory, string = observatory.lower(), string.lower()
+        if observatory not in ['hst', 'jwst']:
+            raise ValueError('Observatory "{}" must be either "hst" or "jwst".')
+        if string not in ['dev', 'test', 'production']:
+            raise ValueError('String "{}" must be either "production", "test", or "dev".')
+        
+        self._string = string
+        self._observatory = observatory  # Already set in parent class from shell variable
+        
+        super(RedCatApiScript, self).__init__(*args, **keys)
+        
+        config.base_url = BASE_URLS[string][observatory]  # config.get_server_url(observatory, string)
+        url = urllib.parse.urljoin(config.base_url, URL_DESCRIPTION)
+        try:
+            with urllib.request.urlopen(url) as req:
+                self._form_description = yaml.safe_load(req)
+        except (urllib.error.HTTPError, urllib.error.URLError, ) as e:
+            print ('Check your network connection!')
+            raise e
+        # Convert list describing form to a dictionary (preserves order):
+        self._form_description = {field['key']: field for field in self._form_description}
+        for key in self._form_description:  self._form_description[key].pop('key')
+        
+        # Hard-code in change_level and description for now:
+        self._form_description['change_level'] = {
+            'type': 'TypedChoiceField',
+            'choices': {'TRIVIAL', 'MODERATE', 'SEVERE'},
+            'initial': 'SEVERE',
+            'required': True,
+            'label': 'Degree that new files are expected to impact science results.', }
+        self._form_description['description'] = {
+            'type': 'CharField',
+            'required': True,
+            'label': 'Information about file changes and expected impacts, include '
+                     'instrument and type.  Formatting note: only alphanumeric, '
+                     'periods, commas, dashes, and underscores are allowed', }
+        
+        self._all_keys = set(self._form_description.keys())
+        self._required_keys = {x for x in self._form_description if self._form_description[x]['required']}
+        self._optional_keys = {x for x in self._form_description if not self._form_description[x]['required']}
+        
+        self._fields = dict()  # Users should not modify this directly!
+        
+        for key in self._form_description:
+            # Avoid field validation for initialization by accessing hidden dictionary directly:
+            self._fields[key] = NULL_FIELDTYPES[self._form_description[key]['type']]()
+            try:
+                self[key] = self._form_description[key]['initial']
+            except KeyError:
+                pass
+
+    def __repr__(self):
+        return '<RedCatApiScript Object {}-{}>:\nFields:  {}'.format(
+            self._observatory, self.string, 
+            self._fields.__repr__())
+
+    @property
+    def string(self):
+        ''' Instantiated for production, test, or dev string.
+        '''
+        return self._string
+
+    # ------------------------------------------------------------------------------------
+    # Dictionary methods to handle form field manipulation:
+
+    def __setitem__(self, key, value):
+        ''' Intercept and enforce validation requirements on individual fields.
+        Booleans values map to 'Yes' and 'No' str.
+        '''
+        if key not in self._all_keys:
+            raise ValueError("Key not in submission form template:  '{}'".format(key))
+        if (key in self._required_keys) and (value == ''):  # allow Boolean False
+            raise ValueError("Field '{}' cannot be empty.".format(key))
+        field_type = NULL_FIELDTYPES[self._form_description[key]['type']]
+        
+        # Interpret boolean values in choice fields as 'Yes' and 'No':
+        if isinstance(value, bool) and ('choices' in self._form_description[key]):
+            if value:
+                value = 'Yes'
+            else:
+                value = 'No'
+        if not isinstance(value, field_type):
+            raise ValueError("'{}' must be of type {}".format(key, field_type.__name__))
+        
+        # Check if choice fields have allowed values:
+        if 'choices' in self._form_description[key]:
+            matches = [x for x in self._form_description[key]['choices'] if x.lower() == value.lower()]
+            if len(matches) != 1:
+                raise ValueError("'{}' must be a valid choice: {{{}}}".format(key, 
+                                 ', '.join(self._form_description[key]['choices'])))
+            # Inherit case from matching choice:
+            value = matches[0]
+        
+        # Keep old fields in sync (DOESN'T WORK THE OTHER WAY!):
+        if key == 'change_level':
+            self.args.change_level = value
+        elif key == 'description':
+            self.args.description = value
+        
+        self._fields[key] = value
+
+    def __delitem__(self, key, *args, **kargs):
+        ''' Reset self[key] to its default initialized value.
+        '''
+        if key in self:
+            self._fields[key] = NULL_FIELDTYPES[self._form_description[key]['type']]()
+            try:
+                self[key] = self._form_description[key]['initial']
+            except KeyError:
+                pass
+        else:
+            raise KeyError(key)
+        
+        # Keep old fields in sync:
+        if key == 'change_level':
+            self.args.change_level = 'SEVERE'
+        elif key == 'description':
+            self.args.description = None
+
+    @wraps(dict.__getitem__)
+    def __getitem__(self, key, *args, **kargs):
+        return self._fields.__getitem__(key, *args, **kargs)
+
+    @wraps(dict.__contains__)
+    def __contains__(self, *args, **kargs):
+        return self._fields.__contains__(*args, **kargs)
+
+    @wraps(dict.get)
+    def get(self, *args, **kargs):
+        return self._fields.get(*args, **kargs)
+
+    @wraps(dict.keys)
+    def keys(self, *args, **kargs):
+        return self._fields.keys(*args, **kargs)
+
+    @wraps(dict.values)
+    def values(self, *args, **kargs):
+        return self._fields.values(*args, **kargs)
+
+    @wraps(dict.items)
+    def items(self, *args, **kargs):
+        return self._fields.items(*args, **kargs)
+
+    # ------------------------------------------------------------------------------------
+
+    def help(self):
+        ''' Print help text derived from CRDS instance specified.
+        '''
+        # Can't easily overwrite __doc__ dynamically.
+        for key, field in self._form_description.items():
+            print (key, ' (', NULL_FIELDTYPES[self._form_description[key]['type']].__name__, 
+                ', optional)' if not field.get('required', False) else ')', '\n', '-'*len(key), sep='')
+            print ('\n'.join(wrap(field['label'])))
+            if 'help_text' in field:
+                print ('\n'.join(wrap(field['help_text'])))
+            if 'choices' in field:
+                print ('Valid choices:')
+                print ('  {', ', '.join(["'{}'".format(x) for x in field['choices']]), '}', sep='')
+            print ()
+
+    def validate(self):
+        ''' Validate the object for submission to CRDS.
+        '''
+        if (set(self.keys()) - self._optional_keys) != self._required_keys:
+            raise Exception('Extra/missing keys')
+        
+        # Check for all empty required keys at once to raise one exception:
+        empty_keys = {key for key in self._required_keys if self[key] == ''}  # Don't flag False booleans
+        if empty_keys:
+            raise ValueError('These keywords cannot be empty:\n    ' + '\n    '.join(empty_keys))
+        
+        ## If instrument is locked and "instrument" field is defined, make sure they're the same:
+        #if (self.lock_status != UNLOCKED) and ('instrument' in self) and (self['instrument'] != self.lock_status):
+        #    raise ValueError("Locked instrument is not the one being updated:  "
+        #                     "locked='{}' vs reported='{}'".format(
+        #        self.lock_status, self['instrument']))
+        
+        ## Make sure files were associated with the submission:
+        #if len(self.files) == 0:
+        #    raise Exception('No files have been added to submission.  '
+        #                    'Use the `submission_obj.add_file()` method.')
+
     def get_submission_args(self):
         """Returns the combined form parameter dictionary as a CRDS Struct defining
         the mapping from all form variables to their string values.
@@ -32,6 +242,8 @@ class RedCatApiScript(ReferenceSubmissionScript):
         # ... get and populate required form dictionary,  careful about
         # conflicts with baseclass.   See baseclass self.connection for
         # doing any web i/o.
+        
+        
         
     def batch_submit_references(self):
         """Do a web re-post to the batch submit references web page."""
