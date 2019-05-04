38e97b871669a6789ab9b68cd48305563c841b24
Todd Miller <jmiller@stsci.edu>
2019-05-04 09:57:46 -0400

    Fix remove_checksum() when FITS primary has no data.

--- crds/io/fits.py
+++ crds/io/fits.py
@@ -227,7 +227,8 @@ class FitsFile(AbstractFile):
             for hdu in hdus:
                 hdu.header.pop("CHECKSUM",None)
                 hdu.header.pop("DATASUM", None)
-                fits.append(output, hdu.data, hdu.header, checksum=False)
+                data = hdu.data if hdu.data is not None else np.array([])
+                fits.append(output, data, hdu.header, checksum=False)
         os.remove(self.filepath)
         os.rename(output, self.filepath)
     
