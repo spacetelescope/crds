198699b9b4cfb62b0e6da1e542270e01b288bc01
Todd Miller <jmiller@stsci.edu>
2019-09-18 17:01:18 -0400

    Updated version tag for 7.4.1.1 MIRI RESET release.

--- crds/__init__.py
+++ crds/__init__.py
@@ -10,8 +10,8 @@ warnings.filterwarnings(
 
 # ============================================================================
 
-__version__ = "7.4.1"   # XXXX  see also ../setup.cfg
-__rationale__ = "2019 Q3: Support for S3-backed serverless operation, conversion to pip-based installs and Travis."
+__version__ = "7.4.1.1"   # XXXX  see also ../setup.cfg
+__rationale__ = "2019 Q3: Updated support for MIRI RESET."
 
 __all__ = [
     "getrecommendations",
