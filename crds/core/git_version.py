f1e2c622cb881ce60489ade4bbea0d458f4dbd87
Todd Miller <jmiller@stsci.edu>
2019-10-09 10:21:30 -0400

    Version hash update.

--- crds/core/git_version.py
+++ crds/core/git_version.py
@@ -1,9 +1,9 @@
 
 
-__version__ = 'c0a5f9fb70357fa3a0eedf95dea9a8dee9bae890'
+__version__ = 'cd256f114752310147ceb1fdf1f63bd6be99614c'
 
 __full_version_info__ = '''
 branch: b7.4.1.2
-sha1: c0a5f9fb70357fa3a0eedf95dea9a8dee9bae890
+sha1: cd256f114752310147ceb1fdf1f63bd6be99614c
 '''
     
