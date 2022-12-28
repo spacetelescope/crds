2df6e2ece050d4dfa7e2d52d476070c16c6300ec
Zach Burnett <zburnett@stsci.edu>
2022-12-28 09:44:27 -0500

    use context manager

--- test/conftest.py
+++ test/conftest.py
@@ -6,5 +6,6 @@ from pytest import fixture
 
 @fixture
 def combined_spec(scope='session'):
-    return json.load(
-        open(Path(__file__).parent.parent / "crds" / "roman" / "specs" / "combined_specs.json", 'r'))
+    filename = Path(__file__).parent.parent / "crds" / "roman" / "specs" / "combined_specs.json"
+    with open(filename, 'r') as specs_file:
+        return json.load(specs_file)
