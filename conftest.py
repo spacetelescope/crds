63d4e2c439cf67120ae612b3e80322c38b035468
alphasentaurii <rkein@stsci.edu>
2023-09-22 14:30:29 -0400

    return the config...

--- conftest.py
+++ conftest.py
@@ -142,6 +142,7 @@ def default_shared_state(crds_shared_group_cache):
 def hst_shared_cache_state(crds_shared_group_cache):
     cfg = ConfigState(cache=crds_shared_group_cache, url="https://hst-crds.stsci.edu", observatory="hst")
     cfg.config_setup()
+    return cfg
 
 
 @fixture(scope='function')
