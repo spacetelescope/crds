13188aa6419b72d1bf321d3136eebc80ec5fd3f0
alphasentaurii <rkein@stsci.edu>
2024-10-17 15:29:32 -0400

    revert back to display version

--- documentation/crds_users_guide/source/conf.py
+++ documentation/crds_users_guide/source/conf.py
@@ -122,7 +122,7 @@ html_theme_options = {
     'canonical_url': '',
     # 'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
     'logo_only': False,
-    'version_selector': True,
+    'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
