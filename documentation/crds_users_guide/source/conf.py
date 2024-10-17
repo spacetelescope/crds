adf1837a7573850425360a5cd2027548ec1a18c0
zacharyburnett <zburnett@stsci.edu>
2024-10-17 14:12:25 -0400

    remove default parameter

--- documentation/crds_users_guide/source/conf.py
+++ documentation/crds_users_guide/source/conf.py
@@ -122,7 +122,6 @@ html_theme_options = {
     'canonical_url': '',
     # 'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
     'logo_only': False,
-    'version_selector': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
