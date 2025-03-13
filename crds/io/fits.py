4d658d5ce1579bc08e64fbc97abba9969bbbf71f
alphasentaurii <rkein@stsci.edu>
2025-03-13 13:00:10 -0400

    remove empty array addition, use hdu.data as is

--- crds/io/fits.py
+++ crds/io/fits.py
@@ -214,8 +214,7 @@ class FitsFile(AbstractFile):
         output = "crds-" + str(uuid.uuid4()) + ".fits"
         with fits_open(self.filepath, do_not_scale_image_data=True) as hdus:
             for hdu in hdus:
-                data = hdu.data if hdu.data is not None else np.array([])
-                fits.append(output, data, hdu.header, checksum=True)
+                fits.append(output, hdu.data, hdu.header, checksum=True)
         os.remove(self.filepath)
         os.rename(output, self.filepath)
 
