3837e40e5c3a40e98ce8796009fa83b10972cb95
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2026-04-03 10:32:57 -0400

    update build6 tests

--- test/test_build6.py
+++ test/test_build6.py
@@ -18,8 +18,8 @@ def test_get_context_history(jwst_no_cache_state):
     """
     history = api.get_context_history("jwst")
 
-    assert history[0] == ('2012-09-06 00:00:00', 'jwst.pmap', 'Bootstrap mappings')
-    assert history[32] == ('2015-11-18 12:58:13', 'jwst_0105.pmap', 'Declared various EXP_TYPE as N/A for 15 WCS types for MIRI, NIRCAM, NIRSPEC. Replacement MIRI distortion references for CRDS #238.')
+    assert history[0] == ('2012-09-06 00:00:00+00:00', 'jwst.pmap', 'Bootstrap mappings')
+    assert history[32] == ('2015-11-18 12:58:13+00:00', 'jwst_0105.pmap', 'Declared various EXP_TYPE as N/A for 15 WCS types for MIRI, NIRCAM, NIRSPEC. Replacement MIRI distortion references for CRDS #238.')
 
 
 @mark.jwst
@@ -35,17 +35,19 @@ def test_query_affected_datasets_list(jwst_no_cache_state, capsys):
     out = capsys.readouterr().out
 
     expected = """
-    (0, '2012-09-06 00:00:00', 'jwst.pmap', 'Bootstrap mappings')
-    (1, '2012-09-27 00:00:00', 'jwst_0000.pmap', 'First rules and references from jwst_gentools stub development cloning.')
-    (2, '2013-04-13 00:00:00', 'jwst_0001.pmap', 'Linearity and dark files.')
-    (3, '2013-07-31 00:00:00', 'jwst_0002.pmap', 'Dark and Mask files.')
-    (4, '2013-09-04 00:00:00', 'jwst_0003.pmap', 'Absolute Calibration (photom) additions and replacements.')
-    (5, '2013-11-25 09:00:03', 'jwst_0005.pmap', 'set by system')
-    (6, '2014-03-19 10:51:19', 'jwst_0012.pmap', 'Updated for META.INSTRUMENT.TYPE switch to META.INSTRUMENT.NAME\\r\\nNew linearity files for all instruments\\r\\nNew saturation files and rmaps for all instruments')
-    (7, '2014-03-26 08:49:12', 'jwst_0013.pmap', 'New context jwst_0013.pmap adds Interpixel Capacitance references for all instruments.')
-    (8, '2014-07-08 16:09:39', 'jwst_0022.pmap', 'Multiple MIRI FLAT changes, new references, added SUBARRAY as matching parameter.  See diffs.')
-    (9, '2014-07-18 11:34:48', 'jwst_0023.pmap', 'First MIRI PHOTOM reference for MIRIMAGE detector with real data.')
-    (10, '2014-07-24 18:49:03', 'jwst_0025.pmap', 'Replaced PHOTOM references for NIRSPEC and NIRISS.')
+    (0, '2012-09-06 00:00:00+00:00', 'jwst.pmap', 'Bootstrap mappings')
+    (1, '2012-09-27 00:00:00+00:00', 'jwst_0000.pmap', 'First rules and references from jwst_gentools stub development cloning.')
+    (2, '2013-04-13 00:00:00+00:00', 'jwst_0001.pmap', 'Linearity and dark files.')
+    (3, '2013-07-31 00:00:00+00:00', 'jwst_0002.pmap', 'Dark and Mask files.')
+    (4, '2013-09-04 00:00:00+00:00', 'jwst_0003.pmap', 'Absolute Calibration (photom) additions and replacements.')
+    (5, '2013-11-25 09:00:03+00:00', 'jwst_0005.pmap', 'set by system')
+    (6, '2014-03-19 10:51:19+00:00', 'jwst_0012.pmap', 'Updated for META.INSTRUMENT.TYPE switch to META.INSTRUMENT.NAME
+    New linearity files for all instruments
+    New saturation files and rmaps for all instruments')
+    (7, '2014-03-26 08:49:12+00:00', 'jwst_0013.pmap', 'New context jwst_0013.pmap adds Interpixel Capacitance references for all instruments.')
+    (8, '2014-07-08 16:09:39+00:00', 'jwst_0022.pmap', 'Multiple MIRI FLAT changes, new references, added SUBARRAY as matching parameter.  See diffs.')
+    (9, '2014-07-18 11:34:48+00:00', 'jwst_0023.pmap', 'First MIRI PHOTOM reference for MIRIMAGE detector with real data.')
+    (10, '2014-07-24 18:49:03+00:00', 'jwst_0025.pmap', 'Replaced PHOTOM references for NIRSPEC and NIRISS.')
     """
     for msg in expected.splitlines():
         assert msg.strip() in out
